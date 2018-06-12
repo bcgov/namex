5ef1bb89aadb653c311d55d1c9b79f0cc56b0abb
Thor Wolpert <thor@wolpert.ca>
2018-06-12 16:43:42 -0700

    adding more OS gunicorn configs

--- nro-extractor/gunicorn_config.py
+++ nro-extractor/gunicorn_config.py
@@ -1,6 +1,7 @@
 import os
 
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
+worker_class = os.environ.get('GUNICORN_WORKER_CLASS', 'sync')
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 timeout = int(os.environ.get('GUNICORN_TIMEOUT', '30'))
 keepalive = int(os.environ.get('GUNICORN_KEEPALIVE', '2'))
