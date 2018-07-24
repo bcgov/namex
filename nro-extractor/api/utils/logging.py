a30fd586e0feb31c990849b2e4bde4a8c7f71d72
thor <thor@wolpert.ca>
2018-07-24 14:52:02 -0700

    Namex 108 (#111)
    * Make the API installable #107
    
    * fixed merge conflicts
    
    * adding state const to Name

--- nro-extractor/api/utils/logging.py
+++
@@ -1,9 +0,0 @@
-# setup logging - important to set it up first
-import logging.config
-from os import path
-
-
-def setup_logging(conf='../../logging.conf'):
-    log_file_path = path.join(path.dirname(path.abspath(__file__)), conf)
-    logging.config.fileConfig(log_file_path)
-    # log = logging.getLogger(__name__)
