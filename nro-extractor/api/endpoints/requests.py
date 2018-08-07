c5b487489763d0ed74213934c1020ecb57cc2a1b
thor <thor@wolpert.ca>
2018-08-07 11:11:20 -0700

    557 nro status (#158)
    * completed first pass for 557 - update NRO to Hold while work is moved to NameX

--- nro-extractor/api/endpoints/requests.py
+++ nro-extractor/api/endpoints/requests.py
@@ -24,6 +24,31 @@ class NRORequest(Resource):
 
     @api.expect(a_request)
     def post(self, *args, **kwargs):
+        """this creates a new Request in NameX from data in the NRO system
+        calls: transfer_NRO_records_to_NameX with UPDATE=FALSE
+        :returns: (json): the NR of the request if 200
+        :returns: 200, 403, 409, 500
+        """
+        return self.transfer_NRO_records_to_NameX(update=False, *args, **kwargs)
+
+    @api.expect(a_request)
+    def put(self, *args, **kwargs):
+        """this updates an existing Request in NameX from data in the NRO system
+        calls: transfer_NRO_records_to_NameX with UPDATE=TRUE
+        :returns: (json): the NR of the request if 200
+        :returns: 200, 403, 409, 500
+        """
+        return self.transfer_nro_records_to_namex(update=True, *args, **kwargs)
+
+    def transfer_nro_records_to_namex(self, update=False, *args, **kwargs):
+        """
+
+        :param update: (bool)
+        :param args: __future__
+        :param kwargs:  __future__
+        :returns: (json): the NR of the request if 200
+        :returns: 200, 403, 409, 500
+        """
         json_input = request.get_json()
         if not json_input:
             return {"message": "No input data provided"}, 400
@@ -33,19 +58,19 @@ class NRORequest(Resource):
         if not validNRFormat(nr_num):
             return {"message": "Valid NR format required - 'NR 9999999'"}, 400
 
+        # open a connection to the NRO bind
         nro_session = db.get_engine(bind='nro').connect()
         g.db_nro_session = nro_session
 
         try:
-
             existing_nr = Request.find_by_nr(nr_num)
-            if existing_nr:
+            if existing_nr and not update:
                 return {"message": "{nr} already exists in namex, unable to create a duplicate".format(nr=nr_num)}, 409
 
             # get the header from NRO, if it's not there, bail
             nr_header = get_nr_header(nro_session, nr_num)
             if not nr_header:
-                return {"message": "{nr} not found, unable to complete extraction to new system".format(nr=nr_num)}, 404
+                return {"message": "{nr} not found in NRO, unable to complete extraction to new system".format(nr=nr_num)}, 404
             current_app.logger.debug('nr_header: {}'.format(nr_header))
 
             # get all the request segments from NRO
@@ -58,16 +83,20 @@ class NRORequest(Resource):
             # get the service account user to save BRO Requests
             user = User.find_by_username(current_app.config['NRO_SERVICE_ACCOUNT'])
 
-            #Create NR
-            new_nr = Request()
-            add_nr_header(new_nr, nr_header, nr_submitter, user)
-            add_applicant(new_nr, nr_applicant)
-            add_comments(new_nr, nr_ex_comments)
-            add_nwpta(new_nr, nr_nwpat)
-            add_names(new_nr, nr_names)
+            # Create or Update the NR
+            if existing_nr and update:
+                nr = existing_nr
+            else:
+                nr = Request()
 
-            current_app.logger.debug('saving the {} graph to the database'.format(new_nr.nrNum))
-            db.session.add(new_nr)
+            add_nr_header(nr, nr_header, nr_submitter, user, update)
+            add_applicant(nr, nr_applicant, update)
+            add_comments(nr, nr_ex_comments, update)
+            add_nwpta(nr, nr_nwpat, update)
+            add_names(nr, nr_names, update)
+
+            current_app.logger.debug('saving the {} graph to the database, updating?:{}'.format(nr.nrNum, update))
+            db.session.add(nr)
             db.session.commit()
 
         except Exception as err:
@@ -79,8 +108,9 @@ class NRORequest(Resource):
             if nro_session:
                 nro_session.close()
 
-        return {"message": "{nr} has been successfully copied".format(nr=nr_num)}, 200
+        return {"message": "{nr_num} has been successfully copied".format(nr_num=nr_num)}, 200
 
+# ### UTILITY FUNCTIONS #########################################
 
 def row_to_dict(row):
     return {key: value for (key, value) in row.items()}
@@ -102,7 +132,7 @@ def validNRFormat(nr):
 
 
 # ######## save stuff
-def add_nr_header(new_nr, nr_header, nr_submitter, user):
+def add_nr_header(nr, nr_header, nr_submitter, user, update=False):
 
     NR_STATE={
         'HISTORICAL': 'HISTORICAL',
@@ -116,36 +146,51 @@ def add_nr_header(new_nr, nr_header, nr_submitter, user):
     if nr_submitter:
         submitter = User.find_by_username(nr_submitter['submitter'])
 
-    new_nr.userId = user.id
-    new_nr.stateCd = State.DRAFT if nr_header['state_type_cd'] is None else NR_STATE[nr_header['state_type_cd']]
-    new_nr.nrNum = nr_header['nr_num']
-    new_nr.requestId = nr_header['request_id']
-    new_nr.previousRequestId = nr_header['previous_request_id']
-    new_nr.submitCount = nr_header['submit_count']
-    new_nr.requestTypeCd = nr_header['request_type_cd']
-    new_nr.expirationDate = nr_header['expiration_date']
-    new_nr.additionalInfo = nr_header['additional_info']
-    new_nr.natureBusinessInfo = nr_header['nature_business_info']
-    new_nr.xproJurisdiction = nr_header['xpro_jurisdiction']
-    new_nr.submittedDate = nr_submitter['submitted_date']
-    new_nr.submitter_userid = None if (submitter is None) else submitter.id
-    new_nr.nroLastUpdate = nr_header['last_update']
-    if nr_header['priority_cd'] is 'PQ':
-        new_nr.priorityCd = 'Y'
-    else:
-        new_nr.priorityCd = 'N'
+    nr.userId = user.id
+    nr.stateCd = State.DRAFT if nr_header['state_type_cd'] is None else NR_STATE[nr_header['state_type_cd']]
+    nr.nrNum = nr_header['nr_num']
+    nr.requestId = nr_header['request_id']
+    nr.previousRequestId = nr_header['previous_request_id']
+    nr.submitCount = nr_header['submit_count']
+    nr.requestTypeCd = nr_header['request_type_cd']
+    nr.expirationDate = nr_header['expiration_date']
+    nr.additionalInfo = nr_header['additional_info']
+    nr.natureBusinessInfo = nr_header['nature_business_info']
+    nr.xproJurisdiction = nr_header['xpro_jurisdiction']
+    nr.submittedDate = nr_submitter['submitted_date']
+    nr.submitter_userid = None if (submitter is None) else submitter.id
+    nr.nroLastUpdate = nr_header['last_update']
 
-def add_comments(new_nr, comments):
+    if nr_header['priority_cd'] is 'PQ':
+        nr.priorityCd = 'Y'
+    else:
+        nr.priorityCd = 'N'
+
+
+def add_comments(nr, comments, update=False):
     # TODO: link to examiner IDs somehow - examiner_IDIR
+    # TODO: append new comments, rather than flushing out all the old ones first
+    if update:
+        for com in nr.comments.all():
+            nr.comments.remove(com)
+
     if comments:
         for c in comments:
             comm = Comment()
             comm.comment = c['examiner_comment']
             comm.timestamp = c['event_timestamp']
 
-            new_nr.comments.append(comm)
+            nr.comments.append(comm)
 
-def add_nwpta(new_nr, nr_nwpat):
+
+def add_nwpta(nr, nr_nwpat, update=False):
+
+    # naive approach, if updating remove all the old PNS'es
+    # TODO change to an update / delete / insert flow
+    if update:
+        for pn in nr.partnerNS.all():
+            nr.partnerNS.remove(pn)
+
     if nr_nwpat:
         for p in nr_nwpat:
             pns = PartnerNameSystem()
@@ -155,31 +200,42 @@ def add_nwpta(new_nr, nr_nwpat):
             pns.partnerNameDate = p['partner_name_date']
             pns.partnerName = p['partner_name']
 
-            new_nr.partnerNS.append(pns)
+            nr.partnerNS.append(pns)
 
 
-def add_names(new_nr, nr_names):
+def add_names(nr, nr_names, update=False):
     NAME_STATE={
         'NE': Name.NOT_EXAMINED,
         'A': Name.APPROVED,
         'R': Name.REJECTED,
-        'C': Name.CANCELED
+        'C': Name.CONDITION
     }
-    last_choice=0
+    # naive approach
+    # TODO change to an update / delete / insert flow
+    if update:
+        # remove all names
+        for ne in nr.names.all():
+            nr.names.remove(ne)
+
+    # add in the names from this request
     for n in nr_names:
-        if last_choice != n['choice_number']: # TODO remove this when the view is fixed
-            last_choice = n['choice_number']
-            name = Name()
-            name.state = Name.NOT_EXAMINED if n['name_state_type_cd'] is None else NAME_STATE[n['name_state_type_cd']]
-            name.choice = n['choice_number']
-            name.name = n['name']
-            name.designation = n['designation']
+        name = Name()
+        name.state = Name.NOT_EXAMINED if n['name_state_type_cd'] is None else NAME_STATE[n['name_state_type_cd']]
+        name.choice = n['choice_number']
+        name.name = n['name']
+        name.designation = n['designation']
 
-            new_nr.names.append(name)
+        nr.names.append(name)
 
 
-def add_applicant(new_nr, nr_applicant):
-    applicant = Applicant()
+def add_applicant(nr, nr_applicant, update=False):
+
+    if update:
+        applicant = nr.applicants
+        # TODO: can an existing NR ever not have applicants?
+    else:
+        applicant = Applicant()
+
     applicant.lastName = nr_applicant['last_name']
     applicant.firstName = nr_applicant['first_name']
     applicant.middleName = nr_applicant['middle_name']
@@ -198,12 +254,13 @@ def add_applicant(new_nr, nr_applicant):
     applicant.stateProvinceCd = nr_applicant['state_province_cd']
     applicant.countryTypeCd = nr_applicant['country_type_cd']
 
-    new_nr.applicants.append(applicant)
+    if not update:
+        nr.applicants.append(applicant)
 
 
-######### get stuff
+# ######## Fetch Current NRO Information ##########################################
 
-def get_nr_header(engine, nr_num):
+def get_nr_header(session, nr_num):
     # get the NR Header
     #############################
     sql_nr = text(
@@ -234,7 +291,7 @@ def get_nr_header(engine, nr_num):
         ' where rs.request_id = :req_id'
         ' and rs.end_event_id IS NULL'
     )
-    result = engine.execute(sql_nr.params(nr=nr_num), multi=True)
+    result = session.execute(sql_nr.params(nr=nr_num), multi=True)
     row = result.fetchone()
     result.close()
 
@@ -243,13 +300,13 @@ def get_nr_header(engine, nr_num):
         nr = row_to_dict(row)
 
         # get last_updated
-        result = engine.execute(sql_lu.params(id=nr['request_id']), multi=True)
+        result = session.execute(sql_lu.params(id=nr['request_id']), multi=True)
         row = result.fetchone()
         if row:
             nr = {**nr, **(row_to_dict(row))}
 
         # get state
-        result = engine.execute(sql_state.params(req_id=nr['request_id']), multi=True)
+        result = session.execute(sql_state.params(req_id=nr['request_id']), multi=True)
         row = result.fetchone()
         if row:
             nr = {**nr, **(row_to_dict(row))}
@@ -258,7 +315,7 @@ def get_nr_header(engine, nr_num):
 
     return None
 
-def get_nr_submitter(engine, request_id):
+def get_nr_submitter(session, request_id):
 
     # get the NR Submitter
     #############################
@@ -269,14 +326,14 @@ def get_nr_submitter(engine, request_id):
         ' from namex_submitter_vw'
         ' where request_id = :req_id'
     )
-    result = engine.execute(sql.params(req_id=request_id), multi=True)
+    result = session.execute(sql.params(req_id=request_id), multi=True)
     row = result.fetchone()
     if row:
         current_app.logger.debug(row_to_dict(row))
         return row_to_dict(row)
     return None
 
-def get_nr_requester(engine, request_id):
+def get_nr_requester(session, request_id):
 
     # get the NR Requester
     #############################
@@ -303,14 +360,14 @@ def get_nr_requester(engine, request_id):
         ' from namex_request_party_vw'
         ' where request_id = :req_id'
     )
-    result = engine.execute(sql.params(req_id=request_id), multi=True)
+    result = session.execute(sql.params(req_id=request_id), multi=True)
     row = result.fetchone()
     if row:
         current_app.logger.debug(row_to_dict(row))
         return row_to_dict(row)
     return None
 
-def get_exam_comments(engine, request_id):
+def get_exam_comments(session, request_id):
     # get the NR Requester
     #############################
     sql = text(
@@ -322,7 +379,7 @@ def get_exam_comments(engine, request_id):
         ' from namex_examiner_comments_vw'
         ' where request_id= :req_id'
     )
-    result = engine.execute(sql.params(req_id=request_id), multi=True)
+    result = session.execute(sql.params(req_id=request_id), multi=True)
     ex_comments = []
     for row in result:
         if row['examiner_comment'] or row['state_comment']:
@@ -332,7 +389,7 @@ def get_exam_comments(engine, request_id):
         return None
     return ex_comments
 
-def get_nwpta(engine, request_id):
+def get_nwpta(session, request_id):
     # get the NR NWPTA Partner information
     #############################
     sql = text(
@@ -347,7 +404,7 @@ def get_nwpta(engine, request_id):
         ' where end_event_id IS NULL'
         ' and pns.request_id= :req_id'
     )
-    result = engine.execute(sql.params(req_id=request_id), multi=True)
+    result = session.execute(sql.params(req_id=request_id), multi=True)
     nwpta = []
     for row in result:
         current_app.logger.debug(row_to_dict(row))
@@ -356,7 +413,7 @@ def get_nwpta(engine, request_id):
         return None
     return nwpta
 
-def get_names(engine, request_id):
+def get_names(session, request_id):
     # get the NR Names
     #############################
     sql = text(
@@ -368,7 +425,7 @@ def get_names(engine, request_id):
         ' from namex_names_vw'
         ' where request_id = :req_id'
     )
-    result = engine.execute(sql.params(req_id=request_id), multi=True)
+    result = session.execute(sql.params(req_id=request_id), multi=True)
     names = []
     for row in result:
         current_app.logger.debug(row_to_dict(row))
