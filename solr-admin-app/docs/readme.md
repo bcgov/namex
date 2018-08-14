31972c3dd578bb580bda6cd965afa281e1801816
WalterMoar <Walter.Moar@gov.bc.ca>
2018-08-14 10:45:49 -0700

    Added admin app for Solr synonyms #590

---
+++ solr-admin-app/docs/readme.md
@@ -0,0 +1,33 @@
+In PyCharm choos enew prject and selected flask (but then mo).
+
+Copied in flask-admin sample, but complained about SQL Alchemy.
+
+File > Settings; Project: [projectname] > Project Interpreter. Clicked + and installed Flask Admin
+
+File > Settings; Project: [projectname] > Project Interpreter. Clicked + and installed Flask-SQLAlchemy
+
+Also:
+psycopg2
+
+#### DONE:
+* SQLAlchemy secret key
+* Better duplicate error message
+* DB Configuration
+* Singularize table name
+* Sorting warning on create box
+* Notice of version for template files plus documentation
+* Put into NAMEX
+* Redeploy Solr with new config files ("enabled")
+* What container? Python
+
+#### TODO:
+* Config for different environments
+* SSO
+* Reload cores: auto or button
+* Stop words?
+* Performance
+* NAMEX-API build on commit
+* Dev DB reconfiguration and reload
+* Move templates to better place / do jinja properly
+* Where to deploy? Test and push to dev/prod?
+* Documentation on data loading, project, DB schemas
---
+++ solr-admin-app/solr_admin/views/synonym_view.py
@@ -0,0 +1,43 @@
+
+from flask_admin.contrib.sqla import ModelView
+
+
+# The customized ModelView that is used for working with the synonyms.
+class SynonymView(ModelView):
+    # We're unlikely to do multiple deletes, so just get rid of the checkboxes and the drop down for delete.
+    action_disallowed_list = ["delete"]
+
+    # Allow the user to change the page size.
+    can_set_page_size = True
+
+    # Allow export as a CSV file.
+    can_export = True
+
+    # Keep everything sorted, although realistically also we need to sort the values within a row before it is saved.
+    column_default_sort = "synonyms_text"
+
+    # Search within the synonyms_text.
+    column_searchable_list = ["synonyms_text"]
+
+    # Use a custom create.html that warns the user about sorting what they enter.
+    create_template = "synonyms_create.html"
+
+    # Use a custom edit.html that warns the user about sorting what they enter.
+    edit_template = "synonyms_edit.html"
+
+    # Use a custom list.html that provides a page size drop down with extra choices.
+    list_template = "synonyms_list.html"
+
+    # When the user goes to save the data, trim whitespace and put the list back into alphabetical order.
+    def on_model_change(self, form, model, is_created):
+        if model.synonyms_text is not None:
+            # Split into comma-separated words.
+            synonyms = model.synonyms_text.split(",")
+
+            # Strip leading and trailing spaces.
+            synonyms = list(map(str.strip, synonyms))
+
+            # Sort alphabetically within the synonyms.
+            synonyms.sort()
+
+            model.synonyms_text = ",".join(synonyms)
