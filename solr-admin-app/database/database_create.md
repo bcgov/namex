71008d57d1e2689835dbfd0d553ba5dce2d92116
WalterMoar <Walter.Moar@gov.bc.ca>
2018-08-15 11:08:27 -0700

    Added admin app for Solr synonyms #590

---
+++ solr-admin-app/database/database_create.md
@@ -0,0 +1,49 @@
+
+# Scripts for creating databases.
+
+The SQL in database_create.sql is used to create the database objects needed for Solr configuration. These are a "least
+effort" way of creating the seldom-changing objects. This process would be much easier if there was a way to run the
+script from pgadmin, but that does not seem to work.
+
+This documentation assumes that `oc.exe` from OpenShift Origin Client Tools has been installed and that the user is
+either running Minishift locally or has an account on the Pathfinder OpenShift cluster.
+
+## Port Forward to the OpenShift Database
+
+Log into the `OpenShift Web Console`. Click the drop-down for your username in the upper-right corner, and select
+`Copy Login Command`. Paste the command into your shell:
+
+```
+C:\> oc login https://console.pathfinder.gov.bc.ca:8443 --token=<blahblahblah>
+Logged into "https://console.pathfinder.gov.bc.ca:8443" as "<username>" using the token provided.
+
+You have access to the following projects and can switch between them with 'oc project <projectname>':
+
+  * servicebc-ne-dev
+    servicebc-ne-test
+    servicebc-ne-tools
+
+Using project "servicebc-ne-dev".
+```
+
+(Note that you could also run `oc login` and enter the same credentials).
+
+If your current project isn't the one of the environment you're working with, change it. For example for dev:
+
+```
+C:\> oc project servicebc-ne-dev
+```
+
+Start the port forwarding:
+
+```
+C:\> oc port-forward postgresql-solr-<pod-id> 54321:5432
+```
+
+## Run the Script
+
+Next connect to the database and run the script:
+
+```
+C:\> "C:\Program Files\PostgreSQL\10\bin\psql" -h localhost -p 54321 -f C:\<path>\database_create.sql solr <username>
+```
--- solr-admin-app/solr_admin/__init__.py
+++ solr-admin-app/solr_admin/__init__.py
@@ -5,52 +5,27 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 
+import config
+import solr_admin.models
 from solr_admin.models.synonym import Synonym
 from solr_admin.views.synonym_view import SynonymView
 
 
-# Create application
-app = Flask(__name__)
-
-# Create secret key so we can use sessions.
-app.config["SECRET_KEY"] = os.urandom(24)
-
-# Turn this off to get rid of warning messages. In future versions of SQLAlchemy, false will be the default and this
-# can be removed.
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-# Get the environment variables that define the database connection.
-DB_USER = os.getenv("DATABASE_USER", "")
-DB_PASSWORD = os.getenv("DATABASE_PASSWORD", "")
-DB_HOST = os.getenv("DATABASE_HOST", "")
-DB_PORT = os.getenv("DATABASE_PORT", "5432")
-DB_NAME = os.getenv("DATABASE_NAME", "")
-SQLALCHEMY_DATABASE_URI = "postgresql://{user}:{password}@{host}:{port}/{name}".format(
-    user=DB_USER,
-    password=DB_PASSWORD,
-    host=DB_HOST,
-    port=int(DB_PORT),
-    name=DB_NAME,
-)
-
-# For testing.
-SQLALCHEMY_DATABASE_URI = "postgres://wamoar:postpass@localhost:5432/namex"
-
-# Create the connection to the database.
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_ECHO"] = True
-db = SQLAlchemy(app)
-
-
-# Flask views
-@app.route("/")
-def index():
-    return "<a href=\"/admin/synonym\">Click me to get to Synonyms!</a>"
-
-
 # Create admin
-def create_app():
-    solr_admin = Admin(app, name="Solr Configuration", template_mode="bootstrap3")
-    solr_admin.add_view(SynonymView(Synonym, db.session))
+def create_app(run_mode=os.getenv('FLASK_ENV', 'production')):
+    # Create application
+    app = Flask(__name__)
+    app.config.from_object(config.CONFIGURATION[run_mode])
+
+    # Create the connection to the database.
+    models.db = SQLAlchemy(app)
+
+    # Flask views
+    @app.route("/")
+    def index():
+        return "<a href=\"/admin/synonym\">Click me to get to Synonyms!</a>"
+
+    solr_admin_app = Admin(app, name="Solr Configuration", template_mode="bootstrap3")
+    solr_admin_app.add_view(SynonymView(Synonym, models.db.session))
 
     return app
