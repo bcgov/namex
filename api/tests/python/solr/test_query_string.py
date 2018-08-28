28a0ab81526b2f323ea8975660818d8822c0e87c
Walter Moar <35532993+WalterMoar@users.noreply.github.com>
2018-08-28 16:14:56 -0700

    Change Solr API to allow not synonymizing #728 (#224)

---
+++ api/tests/python/solr/test_query_string.py
@@ -0,0 +1,70 @@
+#
+# I desperately needed tests but had no time to figure out pytest in Pycharm. It's hacked together for now, but the
+# tests at least are written.
+#
+
+from namex.analytics.solr import NO_SYNONYMS_INDICATOR, NO_SYNONYMS_PREFIX, SolrQueries
+
+
+def test_plain_search():
+    response = SolrQueries()._get_name_copy_clause('waffle corp')
+    print(response)
+    assert response == ''
+
+
+def test_one_term_first():
+    response = SolrQueries()._get_name_copy_clause(NO_SYNONYMS_INDICATOR + 'waffle corp')
+
+    assert response == NO_SYNONYMS_PREFIX + 'waffle'
+
+
+def test_one_term_middle():
+    response = SolrQueries()._get_name_copy_clause('big ' + NO_SYNONYMS_INDICATOR + 'waffle corp')
+
+    assert response == NO_SYNONYMS_PREFIX + 'waffle'
+
+
+def test_one_term_last():
+    response = SolrQueries()._get_name_copy_clause('big corp for ' + NO_SYNONYMS_INDICATOR + 'waffle')
+
+    assert response == NO_SYNONYMS_PREFIX + 'waffle'
+
+
+def test_quoted():
+    response = SolrQueries()._get_name_copy_clause('my "happy waffle"')
+
+    assert response == ''
+
+
+def test_quoted_ignore_embedded_indicator():
+    response = SolrQueries()._get_name_copy_clause('my "happy ' + NO_SYNONYMS_INDICATOR + 'waffle"')
+
+    assert response == ''
+
+
+def test_indicatored_quoted():
+    response = SolrQueries()._get_name_copy_clause('my ' + NO_SYNONYMS_INDICATOR + '"happy waffle"')
+
+    assert response == NO_SYNONYMS_PREFIX + '"happy waffle"'
+
+
+def test_quoted_ignore_embedded_indicator2():
+    response = SolrQueries()._get_name_copy_clause('my ' + NO_SYNONYMS_INDICATOR + '"happy ' + NO_SYNONYMS_INDICATOR +
+                                                   'waffle"')
+
+    assert response == NO_SYNONYMS_PREFIX + '"happy waffle"'
+
+
+# No idea how to get the tests running, so do it manually for now.
+try:
+    test_plain_search()
+    test_one_term_first()
+    test_one_term_middle()
+    test_one_term_last()
+    test_quoted()
+    test_quoted_ignore_embedded_indicator()
+    test_indicatored_quoted()
+    test_quoted_ignore_embedded_indicator2()
+except AssertionError as exception:
+    pass  # (for a breakpoint)
+    raise exception
