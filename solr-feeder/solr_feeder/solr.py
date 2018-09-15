ec072f96df767567c1c525b9eebff23afde4fb71
Walter Moar <35532993+WalterMoar@users.noreply.github.com>
2018-09-15 09:24:24 -0700

    Added the Solr feeder project. #527 (#283)

--- solr-feeder/solr_feeder/solr.py
+++ solr-feeder/solr_feeder/solr.py
@@ -13,11 +13,13 @@ _SOLR_URL = _SOLR_INSTANCE + '/{}/update/json'
 
 
 # Update the core with the given data.
-def update_core(core_name, json):
-    logging.debug('json Solr command: {}'.format(json))
+def update_core(core_name: str, json_string: str):
+    logging.debug('json Solr command: {}'.format(json_string))
 
-    response = requests.post(_SOLR_URL.format(core_name), json=json)
+    response = requests.post(_SOLR_URL.format(core_name), data=json_string)
 
+    # By the way, if your request is mangled, Solr will sometimes happily return a 200 with a responseHeader['status']
+    # value of 0 (meaning all is good).
     if response.status_code != 200:
         logging.error('{} core: {}'.format(core_name, response.json()))
 
