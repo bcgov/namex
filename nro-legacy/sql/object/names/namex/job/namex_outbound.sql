a9279b44e0da50fcbe60d144415afcf3b9a091f3
Thor Wolpert <thor@wolpert.ca>
2018-09-17 11:38:16 -0700

    adding trigger/package to the Oracle NamesDB to send changes and updates to NameX

---
+++ nro-legacy/sql/object/names/namex/job/namex_outbound.sql
@@ -0,0 +1,12 @@
+begin
+      DBMS_SCHEDULER.create_job (
+      job_name => 'NAMEX_OUTBOUND',
+      job_type => 'STORED_PROCEDURE',
+      job_action => 'namex_feeder.feed_namex',
+      start_date => sysdate,
+      repeat_interval => 'freq=MINUTELY; INTERVAL=1',
+      end_date => NULL,
+      enabled => TRUE,
+      comments => 'Send a NR to NameX');
+end;
+/
