595482bd44895cbeef77abbe3b73cb4314e9436f
Walter Moar <35532993+WalterMoar@users.noreply.github.com>
2018-09-18 14:35:54 -0700

    Preparation for production deployment. #875 (#302)

--- nro-legacy/sql/release/20180918_namex/registry/colin_mgr/create.sql
+++ nro-legacy/sql/release/20180918_namex/registry/colin_mgr/create.sql
@@ -1,4 +1,4 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 
-@ ../../../../object/registry/namesdb/trigger/trigger_handler_pks.sql
-@ ../../../../object/registry/namesdb/trigger/trigger_handler_pkb.sql
+@ ../../../../object/registry/colin_mgr/trigger/namex_corp_name_qmsg.sql
+@ ../../../../object/registry/colin_mgr/trigger/namex_corp_state_qmsg.sql
