65aab0c2e07c894b6a45bdf660a607a7054ce2ce
WalterMoar <Walter.Moar@gov.bc.ca>
2018-10-02 11:56:21 -0700

    Solr: fix problems with @ metacharacters. #960

--- api/tests/python/solr/test_query_string.py
+++ api/tests/python/solr/test_query_string.py
@@ -8,50 +8,68 @@ from namex.analytics.solr import NO_SYNONYMS_INDICATOR, NO_SYNONYMS_PREFIX, Solr
 
 def test_plain_search():
     response = SolrQueries()._get_name_copy_clause('waffle corp')
-    print(response)
+
     assert response == ''
 
 
 def test_empty_term():
     response = SolrQueries()._get_name_copy_clause('waffle ' + NO_SYNONYMS_INDICATOR + ' corp')
-    print(response)
+
     assert response == ''
 
 
 def test_empty_term_end():
     response = SolrQueries()._get_name_copy_clause('waffle corp ' + NO_SYNONYMS_INDICATOR)
-    print(response)
+
     assert response == ''
 
 
 def test_trailing_indicator():
     response = SolrQueries()._get_name_copy_clause('waffle' + NO_SYNONYMS_INDICATOR + ' corp')
-    print(response)
+
     assert response == ''
 
 
 def test_trailing_indicator_end():
     response = SolrQueries()._get_name_copy_clause('waffle corp' + NO_SYNONYMS_INDICATOR)
-    print(response)
+
     assert response == ''
+
+
+def test_embedded_indicator():
+    response = SolrQueries()._get_name_copy_clause('waffle' + NO_SYNONYMS_INDICATOR + 'corp')
+
+    assert response == NO_SYNONYMS_PREFIX + '(corp)'
+
+
+def test_embedded_indicator_end():
+    response = SolrQueries()._get_name_copy_clause('the waffle' + NO_SYNONYMS_INDICATOR + 'corp')
+
+    assert response == NO_SYNONYMS_PREFIX + '(corp)'
+
+
+def test_special_character():
+    response = SolrQueries()._get_name_copy_clause(NO_SYNONYMS_INDICATOR + '^corp!')
+
+    assert response == NO_SYNONYMS_PREFIX + '(%5Ecorp%21)'
 
 
 def test_one_term_first():
     response = SolrQueries()._get_name_copy_clause(NO_SYNONYMS_INDICATOR + 'waffle corp')
 
-    assert response == NO_SYNONYMS_PREFIX + 'waffle'
+    assert response == NO_SYNONYMS_PREFIX + '(waffle)'
 
 
 def test_one_term_middle():
     response = SolrQueries()._get_name_copy_clause('big ' + NO_SYNONYMS_INDICATOR + 'waffle corp')
 
-    assert response == NO_SYNONYMS_PREFIX + 'waffle'
+    assert response == NO_SYNONYMS_PREFIX + '(waffle)'
 
 
 def test_one_term_last():
     response = SolrQueries()._get_name_copy_clause('big corp for ' + NO_SYNONYMS_INDICATOR + 'waffle')
 
-    assert response == NO_SYNONYMS_PREFIX + 'waffle'
+    assert response == NO_SYNONYMS_PREFIX + '(waffle)'
 
 
 def test_quoted():
@@ -69,14 +87,14 @@ def test_quoted_ignore_embedded_indicator():
 def test_indicatored_quoted():
     response = SolrQueries()._get_name_copy_clause('my ' + NO_SYNONYMS_INDICATOR + '"happy waffle"')
 
-    assert response == NO_SYNONYMS_PREFIX + '%22happy%20waffle%22'
+    assert response == NO_SYNONYMS_PREFIX + '(%22happy%20waffle%22)'
 
 
 def test_quoted_ignore_embedded_indicator2():
     response = SolrQueries()._get_name_copy_clause('my ' + NO_SYNONYMS_INDICATOR + '"happy ' + NO_SYNONYMS_INDICATOR +
                                                    'waffle"')
 
-    assert response == NO_SYNONYMS_PREFIX + '%22happy%20waffle%22'
+    assert response == NO_SYNONYMS_PREFIX + '(%22happy%20waffle%22)'
 
 
 # No idea how to get the tests running, so do it manually for now.
@@ -86,6 +104,9 @@ try:
     test_empty_term_end()
     test_trailing_indicator()
     test_trailing_indicator_end()
+    test_embedded_indicator()
+    test_embedded_indicator_end()
+    test_special_character()
     test_one_term_first()
     test_one_term_middle()
     test_one_term_last()
