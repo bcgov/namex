6669c4dc1f21c8bf7b286805bd93109d56750682
WalterMoar <Walter.Moar@gov.bc.ca>
2018-10-21 18:18:06 -0700

    Solr: API query with synonyms. #1039

--- solr-synonyms-api/synonyms/endpoints/synonyms.py
+++ solr-synonyms-api/synonyms/endpoints/synonyms.py
@@ -1,7 +1,6 @@
 
 import logging
 
-import flask
 import flask_restplus
 
 import synonyms.models.synonym as synonym
@@ -17,7 +16,8 @@ api = flask_restplus.Namespace('Synonyms', description='Work with synonyms used 
 class _Synonyms(flask_restplus.Resource):
     @staticmethod
     def get(term):
-        logging.debug('synonyms get: {}'.format(flask.request.data))
+        term = term.strip()
+        logging.debug('Doing synonym search for "{}"'.format(term))
 
         results = synonym.Synonym.find(term)
 
