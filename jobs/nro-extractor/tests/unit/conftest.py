071beefb276c5fa0d135961e9263a4a951d17600
Thor Wolpert <thor@wolpert.ca>
2018-12-01 20:48:07 -0800

    modified nro-extractor from a https service to a cronjob, move core functionality out to the namex api
    Signed-off-by: Thor Wolpert <thor@wolpert.ca>

--- jobs/nro-extractor/tests/unit/conftest.py
+++ jobs/nro-extractor/tests/unit/conftest.py
@@ -1,14 +1,30 @@
 import os
 import logging
 import json
+import datetime
 
+import cx_Oracle
 import pytest
 from flask import Flask, Blueprint
 from flask.testing import FlaskClient
 from sqlalchemy import event, text
 
-from api import create_app, db as _db
+from extractor.app import create_app, db as _db, nro as _nro
 from config import TestConfig
+
+from . import FROZEN_DATETIME, EPOCH_DATETIME
+
+
+# fixture to freeze utcnow to a fixed date-time
+@pytest.fixture
+def patch_datetime_utcnow(monkeypatch):
+
+    class _Datetime:
+        @classmethod
+        def utcnow(cls):
+            return FROZEN_DATETIME
+
+    monkeypatch.setattr(datetime, 'datetime', _Datetime)
 
 
 @pytest.fixture(scope="session")
@@ -17,10 +33,18 @@ def app(request):
     Returns session-wide application.
     """
     logging.log(logging.INFO, TestConfig().SQLALCHEMY_DATABASE_URI)
-    app = app = create_app(TestConfig())
+    app = create_app(TestConfig())
 
     return app
 
+@pytest.fixture(scope="session")
+def nro_connection(app):
+    """
+    so that we can use just one of the connections form the pool
+    :param app: the app fixture
+    :return: an cx_Oracle connection from namex.NROServices
+    """
+    return _nro.connection
 
 @pytest.fixture
 def client(app):
@@ -30,7 +54,7 @@ def client(app):
     return client
 
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope="function")
 def session(app, request):
     """
     Returns function-scoped session.
@@ -66,3 +90,36 @@ def session(app, request):
         # This instruction rollsback any commit that were executed in the tests.
         txn.rollback()
         conn.close()
+
+
+@pytest.fixture(scope="function")
+def namex_feeder(app, nro_connection):
+
+    schema = """
+    DROP TABLE NAMEX.NAMEX_FEEDER CASCADE CONSTRAINTS;
+
+    CREATE TABLE NAMEX.NAMEX_FEEDER
+    (
+      ID              NUMBER(10)                    NOT NULL,
+      TRANSACTION_ID  NUMBER(10)                    NOT NULL,
+      STATUS          CHAR(1 BYTE)                  DEFAULT 'P'                   NOT NULL,
+      NR_NUM          VARCHAR2(10 BYTE),
+      ACTION          CHAR(1 BYTE),
+      SEND_COUNT      NUMBER(10)                    DEFAULT 0,
+      SEND_TIME       TIMESTAMP(6),
+      ERROR_MSG       VARCHAR2(4000 BYTE)
+);
+"""
+    cursor = nro_connection.cursor()
+
+    print('creating namex feeder')
+
+    # create schema
+    schema_sql = [sql for sql in [x.strip() for x in schema.replace('\n','').replace('\t','').split(';')] if len(sql)>0]
+    for sql in schema_sql:
+        try:
+            cursor.execute(sql)
+        except cx_Oracle.DatabaseError as e:
+            error, = e.args
+            if error.code != 942: # eat it, if it's a table not found in a drop call
+                raise e
