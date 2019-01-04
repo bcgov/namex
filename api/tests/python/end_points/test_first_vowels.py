c6623f68aeb039f97cc9854483819ccdf568929b
Kial Jinnah <kialj876@gmail.com>
2019-01-04 11:27:00 -0800

    merged in phonetic bucket with cobrs phonetic
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- api/tests/python/end_points/test_first_vowels.py
+++ api/tests/python/end_points/test_first_vowels.py
@@ -10,10 +10,6 @@ def test_can_extract_ou():
     assert_that(first_vowels('CLOUDSIDE'), equal_to('OU'))
 
 
-def test_can_extract_aaa():
-    assert_that(first_vowels('BAAAA'), equal_to('AAAA'))
-
-
 def test_resists_no_vowel():
     assert_that(first_vowels('KCH'), equal_to(''))
 
