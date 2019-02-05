2223ef956989b5134ada11d8b764fb0ce07d6269
Richard <rarmitag@gmail.com>
2019-02-05 09:55:33 -0800

    refactor test to use page object pattern

--- solr-admin-app/tests/external/test_word_condition_audit_list.py
+++ solr-admin-app/tests/external/test_word_condition_audit_list.py
@@ -2,6 +2,8 @@ from hamcrest import *
 from solr_admin.models.restricted_condition_audit import RestrictedConditionAudit
 from tests.external.pages.word_condition_creation_page import WordConditionCreationPage
 
+from tests.external.pages.restricted_word_condition_audit_list_page import RestrictedWordConditionAuditListPage
+
 def test_word_conditon_audit_creation(browser, base_url, db):
 
     page = WordConditionCreationPage(browser, base_url)
@@ -11,24 +13,12 @@ def test_word_conditon_audit_creation(browser, base_url, db):
     page.fill('rc_condition_text', 'This means something')
     page.save()
 
+    page = RestrictedWordConditionAuditListPage(browser, base_url)
+    assert_that(page.list_size(), equal_to(1))
 
-    browser.get(base_url + '/admin/restrictedconditionaudit')
-    browser.find_element_by_link_text('Restricted Condition Audit').click()
-    selection = browser.find_elements_by_css_selector('li.active')
+    assert_that(page.consenting_body_of_row(1).text, equal_to('me'))
+    assert_that(page.words_of_row(1).text, equal_to('needs, BC, approval'))
+    assert_that(page.instructions_of_row(1).text, equal_to('needs approval'))
+    assert_that(page.condition_text_of_row(1).text, equal_to('This means something'))
 
-    assert_that(selection[0].text, equal_to('Restricted Condition Audit'))
-    assert_that(selection[1].text, equal_to('List (1)'))
-
-    cell = browser.find_element_by_css_selector('table.model-list tbody tr:nth-child(1) td.col-username')
-    assert_that(cell.text, equal_to('names-with-admin-access'))
-    cell = browser.find_element_by_css_selector('table.model-list tbody tr:nth-child(1) td.col-action')
-    assert_that(cell.text, equal_to('CREATE'))
-    cell = browser.find_element_by_css_selector('table.model-list tbody tr:nth-child(1) td.col-cnd_text')
-    assert_that(cell.text, equal_to('This means something'))
-    cell = browser.find_element_by_css_selector('table.model-list tbody tr:nth-child(1) td.col-words')
-    assert_that(cell.text, equal_to('needs, BC, approval'))
-    cell = browser.find_element_by_css_selector('table.model-list tbody tr:nth-child(1) td.col-consenting_body')
-    assert_that(cell.text, equal_to('me'))
-    cell = browser.find_element_by_css_selector('table.model-list tbody tr:nth-child(1) td.col-instructions')
-    assert_that(cell.text, equal_to('needs approval'))
 
