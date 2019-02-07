ab285507d8480f4b495c99ca3ca731f60c25b6dc
Susan <suziep1@me.com>
2019-02-07 11:44:39 -0800

    Allow a name to be removed from nro name_instance table (#512)
    * Allow a name to be removed from nro name_instance table
    
    * Also delete the missing name from  Namex npostgres db
    
    * remove unnecessary change
    
    * Adjust where names are deleted

--- api/tests/python/nro_services/test_update_request.py
+++ api/tests/python/nro_services/test_update_request.py
@@ -3,6 +3,7 @@ from namex import nro
 from namex.models import User
 from tests.python import integration_oracle_namesdb, integration_oracle_local_namesdb
 from namex.services.nro.change_nr import \
+    _update_nro_names, \
     _update_request, \
     _get_event_id, \
     _create_nro_transaction, \
@@ -16,9 +17,25 @@ def test_nro_connection(app):
     assert type(conn) is cx_Oracle.Connection
 
 
+class NamesList:
+    mynames = []
+    def addNames(self, names):
+        self.mynames = names
+
+    def all(self):
+        return iter(self.mynames)
+
+
 class FakeRequest:
     requestId = '42'
     previousRequestId = '15'
+    names = NamesList();
+
+
+class FakeName:
+    nameId = '42'
+    name = ''
+    choice = 1
 
 
 @integration_oracle_namesdb
@@ -137,3 +154,130 @@ def test_update_nro_request_state_to_approved(app):
     assert resultset is None
 
 
+@integration_oracle_namesdb
+def test_update_nro_change_and_remove_name_choices(app):
+    """
+    Ensure name can be changed, or removed.
+    """
+    con = nro.connection
+    cursor = con.cursor()
+
+    eid = _get_event_id(cursor)
+
+    user = User('idir/bob', 'bob', 'last', 'idir', 'localhost')
+
+    fake_request = FakeRequest()
+    fake_name1 = FakeName()
+    fake_name2 = FakeName()
+    fake_name3 = FakeName()
+
+    fake_name1.choice = 1
+    fake_name2.choice = 2
+    fake_name3.choice = 3
+    # Update the second name only, and remove the third:
+    fake_name1.name = "Fake name"
+    fake_name2.name = 'Second fake name'
+    fake_name3.name = ''
+    names = NamesList()
+    names.addNames([fake_name1, fake_name2, fake_name3])
+    fake_request.names = names
+
+    fake_request.requestId = 142729
+    fake_request.stateCd = 'INPROGRESS'
+    fake_request.activeUser = user
+
+    change_flags = {
+        'is_changed__name1': False,
+        'is_changed__name2': True,
+        'is_changed__name3': True,
+    }
+
+    # Fail if our test data is not still valid:
+    cursor.execute("""
+        select ni.name
+        from name n, name_instance ni
+        where  ni.name_id = n.name_id 
+        and n.request_id = {} 
+        and ni.end_event_id is null """
+                   .format(fake_request.requestId))
+    result = list(cursor.fetchall())
+    assert len(result) == 3
+    assert (result[1][0] != 'Second fake name')
+
+    _update_nro_names(cursor, fake_request, eid, change_flags)
+
+    cursor.execute("""
+        select ni.name
+        from name n, name_instance ni
+        where  ni.name_id = n.name_id 
+        and n.request_id = {} 
+        and ni.end_event_id is null """
+                   .format(fake_request.requestId))
+    result = list(cursor.fetchall())
+
+    assert result
+    assert len(result) == 2
+    assert result[0][0] != 'Fake name'
+    assert result[1][0] == 'Second fake name'
+
+
+@integration_oracle_namesdb
+def test_update_nro_add_new_name_choice(app):
+    """
+    Ensure name can be changed, or removed.
+    """
+    con = nro.connection
+    cursor = con.cursor()
+
+    eid = _get_event_id(cursor)
+
+    user = User('idir/bob', 'bob', 'last', 'idir', 'localhost')
+
+    fake_request = FakeRequest()
+    fake_name1 = FakeName()
+    fake_name2 = FakeName()
+
+    fake_name1.choice = 1
+    fake_name2.choice = 2
+    # Add a second name choice:
+    fake_name1.name = "Fake name"
+    fake_name2.name = 'Second fake name'
+    names = NamesList()
+    names.addNames([fake_name1, fake_name2])
+    fake_request.names = names
+
+    fake_request.requestId = 884047
+    fake_request.stateCd = 'INPROGRESS'
+    fake_request.activeUser = user
+
+    change_flags = {
+        'is_changed__name1': False,
+        'is_changed__name2': True,
+        'is_changed__name3': False,
+    }
+
+    # Fail if our test data is not still valid:
+    cursor.execute("""
+        select ni.name
+        from name n, name_instance ni
+        where  ni.name_id = n.name_id 
+        and n.request_id = {} 
+        and ni.end_event_id is null """
+                   .format(fake_request.requestId))
+    result = list(cursor.fetchall())
+    assert len(result) == 1
+
+    _update_nro_names(cursor, fake_request, eid, change_flags)
+
+    cursor.execute("""
+        select ni.name
+        from name n, name_instance ni
+        where  ni.name_id = n.name_id 
+        and n.request_id = {} 
+        and ni.end_event_id is null """
+                   .format(fake_request.requestId))
+    result = list(cursor.fetchall())
+
+    assert result
+    assert len(result) == 2
+    assert result[0][0] != 'Fake name'
