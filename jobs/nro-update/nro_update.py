74f120def7d19d4d0779cca43a3f0a80545b8164
PWEI <patrick.wei@gov.bc.ca>
2019-02-20 08:50:09 -0800

    add Jaeger tracing
    Signed-off-by: PWEI <patrick.wei@gov.bc.ca>

--- jobs/nro-update/nro_update.py
+++ jobs/nro-update/nro_update.py
@@ -1,5 +1,7 @@
 import sys
-from datetime import datetime, timedelta
+from datetime import datetime
+import traceback
+import time
 
 import cx_Oracle
 from sqlalchemy.dialects import postgresql
@@ -15,7 +17,32 @@ from nro.nro_datapump import nro_data_pump_update
 from nro.app import create_app, db
 from util.job_tracker import JobTracker
 
+from jaeger_client import Config as JaegerConfig
+from opentracing.ext import tags
+
 setup_logging() # important to do this first
+
+
+def init_tracer(service):
+    config = JaegerConfig(
+        config={  # usually read from some yaml config
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+            'reporter_batch_size': 1,
+        },
+        service_name=service,
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
+
+
+# this call also sets opentracing.tracer
+tracer = init_tracer('nro_update')
+
 
 def get_ops_params():
     try:
@@ -45,77 +72,99 @@ delay, max_rows, expires_days = get_ops_params()
 start_time = datetime.utcnow()
 row_count = 0
 
-try:
-    job_id = JobTracker.start_job(db, start_time)
+with tracer.start_active_span('job_start') as scope:
+    try:
+        job_id = JobTracker.start_job(db, start_time)
 
-    # get the service account user to save BRO Requests
-    user = User.find_by_username(current_app.config['NRO_SERVICE_ACCOUNT'])
+        # get the service account user to save BRO Requests
+        user = User.find_by_username(current_app.config['NRO_SERVICE_ACCOUNT'])
 
-    ora_con = cx_Oracle.connect(Config.ORA_USER,
-                                Config.ORA_PASSWORD,
-                                "{0}:{1}/{2}".format(Config.ORA_HOST, Config.ORA_PORT, Config.ORA_NAME))
-    ora_con.begin()
-    ora_cursor = ora_con.cursor()
+        ora_con = cx_Oracle.connect(Config.ORA_USER,
+                                    Config.ORA_PASSWORD,
+                                    "{0}:{1}/{2}".format(Config.ORA_HOST, Config.ORA_PORT, Config.ORA_NAME))
+        ora_con.begin()
+        ora_cursor = ora_con.cursor()
 
-    # A more generic way of setting time
-    # but it doens't print / log well from the Postgres Dialect
-    # so just leaving it here for future reference
-    # q = q.filter(Request.lastUpdate < datetime.utcnow()-timedelta(seconds=delay)). \
-    #
+        # A more generic way of setting time
+        # but it doens't print / log well from the Postgres Dialect
+        # so just leaving it here for future reference
+        # q = q.filter(Request.lastUpdate < datetime.utcnow()-timedelta(seconds=delay)). \
+        #
 
-    q = db.session.query(Request).\
-                filter(Request.stateCd.in_([State.APPROVED, State.REJECTED, State.CONDITIONAL])).\
-                filter(Request.furnished != 'Y').\
-                filter(Request.lastUpdate <= text('(now() at time zone \'utc\') - INTERVAL \'{delay} SECONDS\''.format(delay=delay))).\
-                order_by(Request.lastUpdate.asc()). \
-                limit(max_rows). \
-                with_for_update()
+        with tracer.start_active_span('pickup_nrs', child_of=scope.span) as scope_pickup:
+            q = db.session.query(Request).\
+                        filter(Request.stateCd.in_([State.APPROVED, State.REJECTED, State.CONDITIONAL])).\
+                        filter(Request.furnished != 'Y').\
+                        filter(Request.lastUpdate <= text('(now() at time zone \'utc\') - INTERVAL \'{delay} SECONDS\''.format(delay=delay))).\
+                        order_by(Request.lastUpdate.asc()). \
+                        limit(max_rows). \
+                        with_for_update()
 
-    # leaving this debug statement here as there were some translation and image caching issues
-    # that are easier to see from the raw SQL in the log
-    #
-    current_app.logger.debug(str(q.statement.compile(
-        dialect=postgresql.dialect(),
-        compile_kwargs={"literal_binds": True}))
-    )
+            scope_pickup.span.set_tag(tags.DATABASE_TYPE, 'postgresql')
+            scope_pickup.span.set_tag(tags.DATABASE_STATEMENT,
+                                      str(q.statement.compile(dialect=postgresql.dialect(),
+                                                              compile_kwargs={"literal_binds": True})))
+            # leaving this debug statement here as there were some translation and image caching issues
+            # that are easier to see from the raw SQL in the log
+            #
+            current_app.logger.debug(str(q.statement.compile(
+                dialect=postgresql.dialect(),
+                compile_kwargs={"literal_binds": True}))
+            )
 
-    for r in q.all():
-        row_count += 1
+            for r in q.all():
+                row_count += 1
 
-        current_app.logger.debug('processing: {}'.format(r.nrNum))
+                with tracer.start_active_span('processing_nr', child_of=scope_pickup.span) as scope_process:
+                    current_app.logger.debug('processing: {}'.format(r.nrNum))
+                    scope_process.span.set_tag('nr.number', '{}'.format(r.nrNum))
 
-        try:
+                    try:
+                        nro_data_pump_update(r, ora_cursor, expires_days)
+                        db.session.add(r)
+                        EventRecorder.record(user, Event.NRO_UPDATE, r, {}, save_to_session=True)
 
-            nro_data_pump_update(r, ora_cursor, expires_days)
-            db.session.add(r)
-            EventRecorder.record(user, Event.NRO_UPDATE, r, {}, save_to_session=True)
+                        ora_con.commit()
+                        db.session.commit()
+                        JobTracker.job_detail(db, job_id, r.nrNum)
 
-            ora_con.commit()
-            db.session.commit()
-            JobTracker.job_detail(db, job_id, r.nrNum)
+                    except Exception as err:
+                        scope_process.span.set_tag(tags.ERROR, 'true')
+                        tb = traceback.format_exc()
+                        scope_process.span.log_kv({'event': 'error',
+                                                   'error.kind': str(type(err)),
+                                                   'error.message': err.with_traceback(None),
+                                                   'error.object': tb})
+                        current_app.logger.error(err)
+                        current_app.logger.error('ERROR: {}'.format(r.nrNum))
+                        db.session.rollback()
+                        ora_con.rollback()
+                        JobTracker.job_detail_error(db, job_id, r.nrNum, str(err))
 
-        except Exception as err:
-            current_app.logger.error(err)
-            current_app.logger.error('ERROR: {}'.format(r.nrNum))
-            db.session.rollback()
+            JobTracker.end_job(db, job_id, datetime.utcnow(), 'success')
+
+    except Exception as err:
+        scope.span.set_tag(tags.ERROR, 'true')
+        tb = traceback.format_exc()
+        scope.span.log_kv({'event': 'error',
+                           'error.kind': str(type(err)),
+                           'error.message': err.with_traceback(None),
+                           'error.object': tb})
+        db.session.rollback()
+        if 'ora_con' in locals() and ora_con:
             ora_con.rollback()
-            JobTracker.job_detail_error(db, job_id, r.nrNum, str(err))
+        print('NRO Update Failed:', err, err.with_traceback(None), file=sys.stderr)
+        JobTracker.end_job(db, job_id, datetime.utcnow(), 'fail')
+        exit(1)
 
-    JobTracker.end_job(db, job_id, datetime.utcnow(), 'success')
-
-except Exception as err:
-    db.session.rollback()
-    if 'ora_con' in locals() and ora_con:
-        ora_con.rollback()
-    print('NRO Update Failed:', err, err.with_traceback(None), file=sys.stderr)
-    JobTracker.end_job(db, job_id, datetime.utcnow(), 'fail')
-    exit(1)
-
-finally:
-    if 'ora_con' in locals() and ora_con:
-        ora_con.close()
+    finally:
+        if 'ora_con' in locals() and ora_con:
+            ora_con.close()
 
 app.do_teardown_appcontext()
 end_time = datetime.utcnow()
 print("job - requests processed: {0} completed in:{1}".format(row_count, end_time-start_time))
+# yield to IOLoop to flush the spans
+time.sleep(2)
+tracer.close()
 exit(0)
--- jobs/nro-update/util/job_tracker.py
+++ jobs/nro-update/util/job_tracker.py
@@ -1,34 +1,57 @@
 from sqlalchemy import text
 from datetime import datetime
 
+import opentracing
+from opentracing.ext import tags
+
+tracer = opentracing.tracer
+
 
 class JobTracker(object):
 
     @staticmethod
     def start_job(db, start_time):
-        state = 'running'
-        sql = text('insert into nro_names_sync_job (status_cd, start_time) values (:state, :start_time) returning id')
+        parent_span = tracer.active_span
+        with tracer.start_active_span('start_job', child_of=parent_span) as scope:
+            state = 'running'
+            sql = text('insert into nro_names_sync_job (status_cd, start_time) values (:state, :start_time) returning id')
+            scope.span.set_tag(tags.DATABASE_TYPE, 'postgresql')
+            scope.span.set_tag(tags.DATABASE_STATEMENT, sql)
+            scope.span.set_tag('db.statement.parameters', "{}, {}".format(state, start_time))
 
-        result = db.engine.execute(sql.params(state=state, start_time=start_time))
-        row = result.fetchone()
-        id = int(row['id'])
+            result = db.engine.execute(sql.params(state=state, start_time=start_time))
+            row = result.fetchone()
+            scope.span.set_tag('db.answer', row)
+            id = int(row['id'])
 
-        return id
+            return id
 
     @staticmethod
     def job_detail(db, job_id, nr_num):
-        sql = text('insert into nro_names_sync_job_detail (job_id, nr_num, time, success) values (:job_id, :nr_num, :event_time, true)')
-
-        db.engine.execute(sql.params(job_id=job_id, nr_num=nr_num, event_time=datetime.utcnow()))
+        parent_span = tracer.active_span
+        with tracer.start_active_span('start_job', child_of=parent_span) as scope:
+            sql = text('insert into nro_names_sync_job_detail (job_id, nr_num, time, success) values (:job_id, :nr_num, :event_time, true)')
+            scope.span.set_tag(tags.DATABASE_TYPE, 'postgresql')
+            scope.span.set_tag(tags.DATABASE_STATEMENT, sql)
+            scope.span.set_tag('db.statement.parameters', "{}, {}".format(job_id, nr_num))
+            db.engine.execute(sql.params(job_id=job_id, nr_num=nr_num, event_time=datetime.utcnow()))
 
     @staticmethod
     def job_detail_error(db, job_id, nr_num, errMsg):
-        sql = text('insert into nro_names_sync_job_detail (job_id, nr_num, time, success, error_msg) values (:job_id, :nr_num, :event_time, false, :errMsg)')
-
-        db.engine.execute(sql.params(job_id=job_id, nr_num=nr_num, event_time=datetime.utcnow(), errMsg=errMsg))
+        parent_span = tracer.active_span
+        with tracer.start_active_span('start_job', child_of=parent_span) as scope:
+            sql = text('insert into nro_names_sync_job_detail (job_id, nr_num, time, success, error_msg) values (:job_id, :nr_num, :event_time, false, :errMsg)')
+            scope.span.set_tag(tags.DATABASE_TYPE, 'postgresql')
+            scope.span.set_tag(tags.DATABASE_STATEMENT, sql)
+            scope.span.set_tag('db.statement.parameters', "{}, {}, {}".format(job_id, nr_num, errMsg))
+            db.engine.execute(sql.params(job_id=job_id, nr_num=nr_num, event_time=datetime.utcnow(), errMsg=errMsg))
 
     @staticmethod
     def end_job(db, job_id, end_time, state):
-        sql = text('update nro_names_sync_job set status_cd = :state, end_time = :end_time where id = :job_id')
-
-        db.engine.execute(sql.params(job_id=job_id, state=state, end_time=end_time))
+        parent_span = tracer.active_span
+        with tracer.start_active_span('start_job', child_of=parent_span) as scope:
+            sql = text('update nro_names_sync_job set status_cd = :state, end_time = :end_time where id = :job_id')
+            scope.span.set_tag(tags.DATABASE_TYPE, 'postgresql')
+            scope.span.set_tag(tags.DATABASE_STATEMENT, sql)
+            scope.span.set_tag('db.statement.parameters', "{}, {}, {}".format(job_id, state, end_time))
+            db.engine.execute(sql.params(job_id=job_id, state=state, end_time=end_time))
