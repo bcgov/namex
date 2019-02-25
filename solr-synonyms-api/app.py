dbb4c78450364376fd357994f7de2aeb52cce53b
Katie McGoff <katie@silverbirchsolutions.com>
2019-02-25 13:47:42 -0800

    Update app.py
    Reverted back to 0.0.0.0 from 127.0.0.1

--- solr-synonyms-api/app.py
+++ solr-synonyms-api/app.py
@@ -10,5 +10,5 @@ logging.basicConfig(level=logging.DEBUG)
 # Listen on all interfaces, and the catalog Python container expects the application to be on 8080.
 application = synonyms.create_application()
 if __name__ == '__main__':
-    #application.run(host='0.0.0.0', port=8080, debug=True)
-    application.run(host='127.0.0.1', port=8080, debug=True)
+    application.run(host='0.0.0.0', port=8080, debug=True)
+    
