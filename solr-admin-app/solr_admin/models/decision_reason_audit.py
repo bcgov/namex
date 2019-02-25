5602813887fc747408c9cb9fff9323f7e07b8020
Katie McGoff <katie@silverbirchsolutions.com>
2019-02-25 13:51:39 -0800

    updated audit table timestamp with timezone

--- solr-admin-app/solr_admin/models/decision_reason_audit.py
+++ solr-admin-app/solr_admin/models/decision_reason_audit.py
@@ -16,7 +16,7 @@ class DecisionReasonAudit(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(100))
-    timestamp = db.Column(db.DateTime)
+    timestamp = db.Column('timestamp', db.DateTime(timezone=True))
     action = db.Column(db.String(10))
     dr_id = db.Column(db.Integer)
     name = db.Column(db.VARCHAR(1024))
