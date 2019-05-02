33dbfe23bd90dedc0a303667ee5fcfbde63399ed
Walter Moar <35532993+WalterMoar@users.noreply.github.com>
2019-05-02 10:12:29 -0700

    Fixed bug for held names, cleaned up scripts (#543) (#569)

--- solr/database/solr_names_core_vw.sql
+++ solr/database/solr_names_core_vw.sql
@@ -5,23 +5,19 @@ CREATE OR REPLACE VIEW solr_names_core_vw
     (id, name, state, last_modified)
 AS
 SELECT
-    nr_num || '-' || choice AS id,
-    name,
+    requests.nr_num || '-' || names.choice AS id,
+    names.name,
     CASE WHEN
-        state IN ('APPROVED', 'CONDITION', 'REJECTED')
+        requests.state_cd != 'HOLD' AND
+        names.state IN ('APPROVED', 'CONDITION', 'REJECTED')
     THEN
         'ACTIVE'
     ELSE
         'INACTIVE'
     END AS state,
-    last_update AS last_modified
+    requests.last_update AS last_modified
 FROM
     requests LEFT JOIN names ON requests.id = names.nr_id;
-
-COMMIT;
-
-
--- Do the following as user "postgres":
 
 GRANT SELECT ON solr_names_core_vw TO solr;
 
