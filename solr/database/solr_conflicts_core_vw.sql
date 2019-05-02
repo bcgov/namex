6d09c377b4323f6e2b968f2d4404c3a8d93408a5
WalterMoar <Walter.Moar@gov.bc.ca>
2019-05-02 10:10:29 -0700

    Fixed bug for held names, cleaned up scripts (#543)

--- solr/database/solr_conflicts_core_vw.sql
+++ solr/database/solr_conflicts_core_vw.sql
@@ -1,3 +1,8 @@
+-- Do the following as user "postgres":
+
+CREATE ROLE solr LOGIN PASSWORD '[YOUR PASSWORD HERE]';
+GRANT USAGE ON SCHEMA public TO solr;
+
 
 -- Do this as the normal user for the database:
 
@@ -5,31 +10,24 @@ CREATE OR REPLACE VIEW solr_conflicts_core_vw
     (id, name, state, last_modified)
 AS
 SELECT
-    nr_num AS id,
-    name,
+    requests.nr_num AS id,
+    names.name,
     CASE WHEN
-        state_cd IN ('APPROVED', 'CONDITIONAL') AND
-        expiration_date > NOW() - INTERVAL '1 day' AND
-        consumption_date IS NULL
+        requests.state_cd IN ('APPROVED', 'CONDITIONAL') AND
+        requests.expiration_date > NOW() - INTERVAL '1 day' AND
+        names.consumption_date IS NULL
     THEN
         'ACTIVE'
     ELSE
         'INACTIVE'
     END AS state,
-    last_update AS last_modified
+    requests.last_update AS last_modified
 FROM
     requests LEFT JOIN names ON requests.id = names.nr_id WHERE
-    state IN ('APPROVED', 'CONDITION') AND
-    request_type_cd NOT IN ('CEM', 'CFR', 'CLL', 'CLP', 'FR', 'LIB', 'LL', 'LP', 'NON', 'PAR', 'RLY', 'TMY', 'XCLL',
-                            'XCLP', 'XLL', 'XLP');
+    names.state IN ('APPROVED', 'CONDITION') AND
+    requests.request_type_cd NOT IN ('CEM', 'CFR', 'CLL', 'CLP', 'FR', 'LIB', 'LL', 'LP', 'NON', 'PAR', 'RLY', 'TMY',
+                                     'XCLL', 'XCLP', 'XLL', 'XLP');
 
-COMMIT;
-
-
--- Do the following as user "postgres":
-
-CREATE ROLE solr LOGIN PASSWORD '[YOUR PASSWORD HERE]';
-GRANT USAGE ON SCHEMA public TO solr;
 GRANT SELECT ON solr_conflicts_core_vw TO solr;
 
 COMMIT;
