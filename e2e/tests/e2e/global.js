7f0cc73f90ac2255f41af7f8d57a106066d8ea48
riyazuddinsyed <49958950+riyazuddinsyed@users.noreply.github.com>
2019-05-06 00:45:29 -0700

    Create global.js

---
+++ e2e/tests/e2e/global.js
@@ -0,0 +1,24 @@
+module.exports = {
+	waitForConditionTimeout : 5000,
+	
+	throwOnMultipleElementsReturned : true,
+	
+    smokeTestNR: {
+        NR_num: '',
+        name_choice1: 'ZZZZZZZ 1 TEST NAME DO NOT EXAMINE'
+    },
+	
+    searchScreenTopHold:{
+        NR_num: ''
+    },
+
+    extractorTimeOut : 1000,
+
+    exactMatch : 'AIR PACIFIC LIMITED',
+
+    conditionExample : 'INSURANCE',
+
+    trademarkResult : 'Litigation Life Benefit Insurance',
+
+    historyExample : 'GITSELASU FLORISTS LIMITED'
+};
