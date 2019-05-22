93ec205813b4ac4ed840268128b4a5f3fa8e9d50
john_lane <john.a.m.lane@gov.bc.ca>
2019-05-22 16:24:31 -0700

    stopped using the page object within perform

--- e2e/page-objects/namexSearch.js
+++ e2e/page-objects/namexSearch.js
@@ -14,7 +14,7 @@ var searchCommands = {
             })
             .perform(function () {
                 if (NRColumnValue == '') {
-                    browser.setValue(this.elements.NR_column.selector, NR_num);
+                    browser.setValue('#search-filter-nr-number', NR_num);
                 }
 
             })
