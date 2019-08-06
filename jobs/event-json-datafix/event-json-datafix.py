4dbaf7b1694a45a4e81b2d8cd546c853b686c662
Lorna Trent <lorna.trent@gov.bc.ca>
2019-08-06 08:26:38 -0700

    Updated

--- jobs/event-json-datafix/event-json-datafix.py
+++ jobs/event-json-datafix/event-json-datafix.py
@@ -1,15 +1,12 @@
-import sys
-
-from sqlalchemy import text
+import sys, os
 from datetime import datetime, timedelta
-
 from flask import Flask, g, current_app
-
 from namex import db
 from namex.utils.logging import setup_logging
 
 from config import Config
-import bz2
+import zlib, json
+
 
 setup_logging() ## important to do this first
 
@@ -27,20 +24,32 @@ def create_app(config=Config):
 app = create_app(Config)
 start_time = datetime.utcnow()
 row_count = 0
+MAX_ROW_LIMIT = os.getenv('MAX_ROWS', '100')
 
 try:
-    sql = "select id,json_zip " \
-          "from events "
+
+    sql = "select id,substring(json_zip,3) " \
+          "from events where event_json is null limit " + MAX_ROW_LIMIT
 
     events = db.session.execute(sql)
     for event_id, event_compressed_json in events:
         current_app.logger.debug('processing id: {}'.format(event_id))
+        x = bytearray.fromhex(event_compressed_json)
+        z = zlib.decompress(x)
+        json_str =  z.decode('utf8')
+        escaped_json_str = json_str.replace("'", "''")
+        new_text = escaped_json_str.replace(":Final ",". Final ")
+        new_text1 = new_text.replace("Fax #:","Fax ")
 
-        decompressed_json = bz2.decompress(event_compressed_json)
+        json_list = json.loads(new_text1)
 
-        update_sql = "update event " \
-                     "set event_json = \'{decompressed_json}\' " \
-                     "where id={id}".format(id=event_id)
+        formatted_json = json.dumps(json_list)
+
+        update_sql = "update events " \
+                     "set event_json='{json_input}'::jsonb "  \
+                     "where id={id}".format(id=event_id, json_input=formatted_json)
+
+        print(update_sql)
         db.session.execute(update_sql)
         db.session.commit()
         row_count += 1
