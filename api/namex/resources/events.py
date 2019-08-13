a7415ddce6d19f4200a7a14dec37e3fd87c962ae
Lorna Trent <lorna.trent@gov.bc.ca>
2019-08-13 14:12:40 -0700

    added business logic to determine and capture the user action
    Signed-off-by: Lorna Trent <lorna.trent@gov.bc.ca>

--- api/namex/resources/events.py
+++ api/namex/resources/events.py
@@ -38,7 +38,61 @@ class Events(Resource):
             request_id = nrd["id"]
         if not request_id:
             return jsonify({"message": "Request NR:{} not found".format(nr)}), 404
-        #return jsonify({"message": "id type:{}".format(type(request_id))}), 200
-        event_results = EventDAO.query.filter_by(nrId = request_id).first_or_404()
-        return jsonify(event_results.json()), 200
 
+        event = EventDAO.query.filter_by(nrId = request_id).order_by("id").first_or_404().json()
+        if not "id" in event:
+            return jsonify({"message": "No events for NR:{} not found".format(nr)}), 404
+
+        event_results = EventDAO.query.filter_by(nrId=request_id).order_by("id").all()
+
+        e_dict_previous = dict()
+        e_txn_history = dict()
+        i=0
+
+        for e in event_results:
+
+            e_dict = e.json()
+            user_action = ""
+
+            if e_dict["action"] == "update_from_nro":
+                user_action = "Select from Draft Queue"
+            if e_dict["action"] == "patch" and  e_dict["stateCd"] == "INPROGRESS":
+                user_action = "Load NR"
+            if e_dict["action"] == "patch" and e_dict["stateCd"] == "HOLD":
+                user_action = "Hold Request"
+            if e_dict["action"] == "marked_on_hold" and e_dict["stateCd"] == "HOLD":
+                user_action = "Marked on Hold"
+            if e_dict["action"] == "put" and e_dict["stateCd"] == "INPROGRESS" and  "additional" in e_dict["jsonData"]:
+                if not e_dict_previous or (e_dict_previous["stateCd"] == "HOLD" or e_dict_previous["stateCd"] == "DRAFT" or e_dict_previous["stateCd"] == "INPROGRESS"):
+                    user_action = "Edit NR Details"
+                if e_dict_previous and e_dict_previous["stateCd"] == "APPROVED" or e_dict_previous["stateCd"]=="REJECTED" or e_dict_previous["stateCd"] == "CONDITIONAL":
+                    if '"furnished": "Y"' in e_dict["jsonData"]:
+                        user_action = "Reset"
+                    else:
+                        user_action = "Re-Open"
+            if e_dict["action"] == "put" and (e_dict["stateCd"] == "APPROVED" or e_dict["stateCd"] == "REJECTED" or e_dict["stateCd"] == "CONDITIONAL"):
+                user_action = "Edit NR Details after Completion"
+
+            if e_dict["action"] == "put" and e_dict["stateCd"] == "INPROGRESS" and  "additional" not in e_dict["jsonData"] and '"state": "NE"' not in  e_dict["jsonData"]:
+                user_action = "Complete the Name Choice"
+            if e_dict["action"] == "patch" and (e_dict["stateCd"]== "APPROVED" or e_dict["stateCd"] == "REJECTED" or e_dict["stateCd"] == "CONDITIONAL"):
+                user_action = "Decision"
+            if e_dict["action"] == "put" and e_dict["stateCd"] == "INPROGRESS" and  "additional" not in e_dict["jsonData"] and  '"state": "NE"' in  e_dict["jsonData"]:
+                user_action = "Undo Decision"
+            if e_dict["action"] == "nro_update" and (e_dict["stateCd"]== "APPROVED" or e_dict["stateCd"] == "REJECTED" or e_dict["stateCd"] == "CONDITIONAL"):
+                user_action = "Updated NRO"
+            if e_dict["action"] == "post" and "comment" in e_dict["jsonData"]:
+                user_action = "Staff Comment"
+            #store the previous row
+            e_dict_previous = e_dict
+            e_dict["user_action"] = user_action
+
+            i=i+1
+            e_txn_history[i] = {}
+            e_txn_history[i] = e_dict
+
+        rep = {'response': {'count':i},
+               'transactions': e_txn_history
+              }
+
+        return jsonify(rep), 200
