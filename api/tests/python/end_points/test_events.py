72c2052d584f1560a624a175858f6193726f477b
Lorna Trent <lorna.trent@gov.bc.ca>
2019-08-16 14:23:42 -0700

    updated tests

--- api/tests/python/end_points/test_events.py
+++ api/tests/python/end_points/test_events.py
@@ -374,3 +374,70 @@ def test_edit_event_history(client, jwt, app):
 
     assert b'"user_action": "Edit NR Details"' in rv.data
 
+def test_reopen_event_history(client, jwt, app):
+    from namex.models import Request as RequestDAO, State, Name as NameDAO, User, Event
+    from namex.services import EventRecorder
+
+    # add a user for the comment
+    user = User('test-user', '', '', '43e6a245-0bf7-4ccf-9bd0-e7fb85fd18cc',
+                'https://sso-dev.pathfinder.gov.bc.ca/auth/realms/sbc')
+    user.save_to_db()
+
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    nr = RequestDAO()
+    nr.nrNum = 'NR 0000002'
+    nr.stateCd = State.REJECTED
+    nr.requestId = 1460775
+    name1 = NameDAO()
+    name1.choice = 1
+    name1.name = 'TEST NAME ONE'
+    nr.names = [name1]
+    nr.save_to_db()
+
+    EventRecorder.record(user, Event.PATCH, nr, {})
+
+    nr.stateCd = State.INPROGRESS
+    EventRecorder.record(user, Event.PUT, nr, {"additional": "additional","furnished": "N"})
+
+    # get the resource (this is the test)
+    rv = client.get('/api/v1/events/NR%200000002', headers=headers)
+    assert rv.status_code == 200
+
+    assert b'"user_action": "Re-Open"' in rv.data
+
+def test_edit_inprogress_event_history(client, jwt, app):
+    from namex.models import Request as RequestDAO, State, Name as NameDAO, User, Event
+    from namex.services import EventRecorder
+
+    # add a user for the comment
+    user = User('test-user', '', '', '43e6a245-0bf7-4ccf-9bd0-e7fb85fd18cc',
+                'https://sso-dev.pathfinder.gov.bc.ca/auth/realms/sbc')
+    user.save_to_db()
+
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    nr = RequestDAO()
+    nr.nrNum = 'NR 0000002'
+    nr.stateCd = State.HOLD
+    nr.requestId = 1460775
+    name1 = NameDAO()
+    name1.choice = 1
+    name1.name = 'TEST NAME ONE'
+    nr.names = [name1]
+    nr.save_to_db()
+
+    EventRecorder.record(user, Event.PATCH, nr, {})
+
+    nr.stateCd = State.INPROGRESS
+    EventRecorder.record(user, Event.PUT, nr, {"additional": "additional","furnished": "N"})
+
+    # get the resource (this is the test)
+    rv = client.get('/api/v1/events/NR%200000002', headers=headers)
+    assert rv.status_code == 200
+
+    assert b'"user_action": "Edit NR Details"' in rv.data
