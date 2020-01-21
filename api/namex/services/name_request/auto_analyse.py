9e1eebb13f95a66521bcb59d4814dfda3ede573d
Lucas <tech@firebrandwebsolutions.com>
2020-01-20 16:20:17 -0800

    Change camelCase method names to snake_case in auto_analyse service.

--- api/namex/services/name_request/auto_analyse.py
+++ api/namex/services/name_request/auto_analyse.py
@@ -6,29 +6,29 @@ class AutoAnalyseService:
     def __init__(self):
         pass
 
-    def executeAnalysis(self):
-        self.cleanName()
-        self.validateName()
+    def execute_analysis(self):
+        self.clean_name()
+        self.validate_name()
         # TODO: Loop over words, call getWordClassification
         pass
 
     # Sanitize input and clean the provided name
-    def cleanName(self):
+    def clean_name(self):
         pass
 
     # Get the classification for a specific word
-    def getWordClassification(self):
+    def get_word_classification(self):
         pass
 
     # Get the company's designation if it's in the name
-    def getNameDesignation(self):
+    def get_name_designation(self):
         pass
 
     # Validate the name
-    def validateName(self):
-        self.validateNameIsWellFormed()
+    def validate_name(self):
+        self.validate_name_is_well_formed()
 
     # Check to see if a provided name is valid
-    def validateNameIsWellFormed(self):
+    def validate_name_is_well_formed(self):
         pass
 
