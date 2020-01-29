c878ea8546dd2d0cc77a5b39da804281d3c73d14
Lucas <tech@firebrandwebsolutions.com>
2020-01-29 12:18:54 -0800

    Add a little bit more meat to the stubbed out test_auto_analyse tests.

--- api/tests/python/end_points/test_auto_analyse.py
+++ api/tests/python/end_points/test_auto_analyse.py
@@ -270,6 +270,10 @@ def test_new_bc_dba_valid_response(client, jwt, app):
     if isinstance(payload.issues, list):
         assert_issues_count_is(0, payload.issues)
 
+def test_too_many_words_request_response(client, jwt, app):
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
 
 def test_new_bc_gp_valid_response(client, jwt, app):
     # create JWT & setup header with a Bearer Token using the JWT
