6711156d3568e732f64e7e1ddcaa3f4653744cab
Lucas <tech@firebrandwebsolutions.com>
2020-02-01 20:19:33 -0800

    Add tests for new entity requests of all types. Fix issues with validation. Tests passing.

--- api/tests/python/end_points/test_auto_analyse.py
+++ api/tests/python/end_points/test_auto_analyse.py
@@ -43,6 +43,7 @@ ENDPOINT_PATH = API_BASE_URI + 'name-analysis'
 #   request_type, one of: [‘new’, ‘existing’, ‘continuation’]
 # }
 
+
 # @pytest.mark.xfail(raises=ValueError)
 def test_get_analysis_request_response(client, jwt, app):
     # create JWT & setup header with a Bearer Token using the JWT
@@ -76,14 +77,111 @@ def test_get_analysis_request_response(client, jwt, app):
 # Contains Unclassifiable Word
 # Conflicts with the Corporate Database
 
+# FIRST LET'S TEST AND MAKE SURE ALL VALID REQUESTS ARE WORKING
+
 # @pytest.mark.xfail(raises=ValueError)
-def test_valid_request_response(client, jwt, app):
+def test_new_bc_cr_valid_response(client, jwt, app):
     # create JWT & setup header with a Bearer Token using the JWT
     token = jwt.create_jwt(claims, token_header)
     headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
 
     test_params = {
-        'name': 'MOUNTAIN NEW FOOD GROWERS LTD.',
+        'name': 'MOUNTAIN NEW FOOD GROWERS INC.',
+        'location': 'BC',
+        'entity_type': 'CR',
+        'request_type': 'NEW'
+    }
+
+    query = '&'.join("{!s}={}".format(k, quote_plus(v)) for (k, v) in test_params.items())
+    path = ENDPOINT_PATH + '?' + query
+    print('\n' + 'request: ' + path + '\n')
+    response = client.get(path, headers=headers)
+    print(response)
+
+
+def test_new_bc_ul_valid_response(client, jwt, app):
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    test_params = {
+        'name': 'MOUNTAIN NEW FOOD GROWERS INC.',
+        'location': 'BC',
+        'entity_type': 'UL',
+        'request_type': 'NEW'
+    }
+
+    query = '&'.join("{!s}={}".format(k, quote_plus(v)) for (k, v) in test_params.items())
+    path = ENDPOINT_PATH + '?' + query
+    print('\n' + 'request: ' + path + '\n')
+    response = client.get(path, headers=headers)
+    print(response)
+
+
+def test_new_bc_cp_valid_response(client, jwt, app):
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    test_params = {
+        'name': 'MOUNTAIN NEW FOOD GROWERS INC.',
+        'location': 'BC',
+        'entity_type': 'CP',
+        'request_type': 'NEW'
+    }
+
+    query = '&'.join("{!s}={}".format(k, quote_plus(v)) for (k, v) in test_params.items())
+    path = ENDPOINT_PATH + '?' + query
+    print('\n' + 'request: ' + path + '\n')
+    response = client.get(path, headers=headers)
+    print(response)
+
+
+def test_new_bc_bc_valid_response(client, jwt, app):
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    test_params = {
+        'name': 'MOUNTAIN NEW FOOD GROWERS INC.',
+        'location': 'BC',
+        'entity_type': 'BC',
+        'request_type': 'NEW'
+    }
+
+    query = '&'.join("{!s}={}".format(k, quote_plus(v)) for (k, v) in test_params.items())
+    path = ENDPOINT_PATH + '?' + query
+    print('\n' + 'request: ' + path + '\n')
+    response = client.get(path, headers=headers)
+    print(response)
+
+
+def test_new_bc_cc_valid_response(client, jwt, app):
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    test_params = {
+        'name': 'MOUNTAIN NEW FOOD GROWERS INC.',
+        'location': 'BC',
+        'entity_type': 'CC',
+        'request_type': 'NEW'
+    }
+
+    query = '&'.join("{!s}={}".format(k, quote_plus(v)) for (k, v) in test_params.items())
+    path = ENDPOINT_PATH + '?' + query
+    print('\n' + 'request: ' + path + '\n')
+    response = client.get(path, headers=headers)
+    print(response)
+
+
+def test_new_bc_fr_valid_response(client, jwt, app):
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    test_params = {
+        'name': 'MOUNTAIN NEW FOOD GROWERS INC.',
         'location': 'BC',
         'entity_type': 'FR',
         'request_type': 'NEW'
@@ -94,6 +192,199 @@ def test_valid_request_response(client, jwt, app):
     print('\n' + 'request: ' + path + '\n')
     response = client.get(path, headers=headers)
     print(response)
+
+
+def test_new_bc_dba_valid_response(client, jwt, app):
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    test_params = {
+        'name': 'MOUNTAIN NEW FOOD GROWERS INC.',
+        'location': 'BC',
+        'entity_type': 'DBA',
+        'request_type': 'NEW'
+    }
+
+    query = '&'.join("{!s}={}".format(k, quote_plus(v)) for (k, v) in test_params.items())
+    path = ENDPOINT_PATH + '?' + query
+    print('\n' + 'request: ' + path + '\n')
+    response = client.get(path, headers=headers)
+    print(response)
+
+
+def test_new_bc_gp_valid_response(client, jwt, app):
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    test_params = {
+        'name': 'MOUNTAIN NEW FOOD GROWERS INC.',
+        'location': 'BC',
+        'entity_type': 'GP',
+        'request_type': 'NEW'
+    }
+
+    query = '&'.join("{!s}={}".format(k, quote_plus(v)) for (k, v) in test_params.items())
+    path = ENDPOINT_PATH + '?' + query
+    print('\n' + 'request: ' + path + '\n')
+    response = client.get(path, headers=headers)
+    print(response)
+
+
+def test_new_bc_lp_valid_response(client, jwt, app):
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    test_params = {
+        'name': 'MOUNTAIN NEW FOOD GROWERS INC.',
+        'location': 'BC',
+        'entity_type': 'LP',
+        'request_type': 'NEW'
+    }
+
+    query = '&'.join("{!s}={}".format(k, quote_plus(v)) for (k, v) in test_params.items())
+    path = ENDPOINT_PATH + '?' + query
+    print('\n' + 'request: ' + path + '\n')
+    response = client.get(path, headers=headers)
+    print(response)
+
+
+def test_new_bc_ll_valid_response(client, jwt, app):
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    test_params = {
+        'name': 'MOUNTAIN NEW FOOD GROWERS INC.',
+        'location': 'BC',
+        'entity_type': 'LL',
+        'request_type': 'NEW'
+    }
+
+    query = '&'.join("{!s}={}".format(k, quote_plus(v)) for (k, v) in test_params.items())
+    path = ENDPOINT_PATH + '?' + query
+    print('\n' + 'request: ' + path + '\n')
+    response = client.get(path, headers=headers)
+    print(response)
+
+
+def test_new_xpro_xcr_valid_response(client, jwt, app):
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    test_params = {
+        'name': 'MOUNTAIN NEW FOOD GROWERS INC.',
+        'location': 'CA',
+        'entity_type': 'XCR',
+        'request_type': 'NEW'
+    }
+
+    query = '&'.join("{!s}={}".format(k, quote_plus(v)) for (k, v) in test_params.items())
+    path = ENDPOINT_PATH + '?' + query
+    print('\n' + 'request: ' + path + '\n')
+    response = client.get(path, headers=headers)
+    print(response)
+
+
+def test_new_xpro_xul_valid_response(client, jwt, app):
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    test_params = {
+        'name': 'MOUNTAIN NEW FOOD GROWERS INC.',
+        'location': 'CA',
+        'entity_type': 'XUL',
+        'request_type': 'NEW'
+    }
+
+    query = '&'.join("{!s}={}".format(k, quote_plus(v)) for (k, v) in test_params.items())
+    path = ENDPOINT_PATH + '?' + query
+    print('\n' + 'request: ' + path + '\n')
+    response = client.get(path, headers=headers)
+    print(response)
+
+
+def test_new_xpro_xcp_valid_response(client, jwt, app):
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    test_params = {
+        'name': 'MOUNTAIN NEW FOOD GROWERS INC.',
+        'location': 'CA',
+        'entity_type': 'XCP',
+        'request_type': 'NEW'
+    }
+
+    query = '&'.join("{!s}={}".format(k, quote_plus(v)) for (k, v) in test_params.items())
+    path = ENDPOINT_PATH + '?' + query
+    print('\n' + 'request: ' + path + '\n')
+    response = client.get(path, headers=headers)
+    print(response)
+
+
+def test_new_xpro_xlc_valid_response(client, jwt, app):
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    test_params = {
+        'name': 'MOUNTAIN NEW FOOD GROWERS INC.',
+        'location': 'CA',
+        'entity_type': 'XLC',
+        'request_type': 'NEW'
+    }
+
+    query = '&'.join("{!s}={}".format(k, quote_plus(v)) for (k, v) in test_params.items())
+    path = ENDPOINT_PATH + '?' + query
+    print('\n' + 'request: ' + path + '\n')
+    response = client.get(path, headers=headers)
+    print(response)
+
+
+def test_new_xpro_xlp_valid_response(client, jwt, app):
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    test_params = {
+        'name': 'MOUNTAIN NEW FOOD GROWERS INC.',
+        'location': 'CA',
+        'entity_type': 'XLC',
+        'request_type': 'NEW'
+    }
+
+    query = '&'.join("{!s}={}".format(k, quote_plus(v)) for (k, v) in test_params.items())
+    path = ENDPOINT_PATH + '?' + query
+    print('\n' + 'request: ' + path + '\n')
+    response = client.get(path, headers=headers)
+    print(response)
+
+
+def test_new_xpro_xll_valid_response(client, jwt, app):
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    test_params = {
+        'name': 'MOUNTAIN NEW FOOD GROWERS INC.',
+        'location': 'CA',
+        'entity_type': 'XLL',
+        'request_type': 'NEW'
+    }
+
+    query = '&'.join("{!s}={}".format(k, quote_plus(v)) for (k, v) in test_params.items())
+    path = ENDPOINT_PATH + '?' + query
+    print('\n' + 'request: ' + path + '\n')
+    response = client.get(path, headers=headers)
+    print(response)
+
+# IN THIS SECTION TEST VARIOUS ERROR RESPONSES
+
 
 # @pytest.mark.xfail(raises=ValueError)
 def test_add_distinctive_word_request_response(client, jwt, app):
@@ -114,6 +405,7 @@ def test_add_distinctive_word_request_response(client, jwt, app):
     response = client.get(path, headers=headers)
     print(response)
 
+
 # @pytest.mark.xfail(raises=ValueError)
 def test_add_descriptive_word_request_response(client, jwt, app):
     # create JWT & setup header with a Bearer Token using the JWT
@@ -132,6 +424,7 @@ def test_add_descriptive_word_request_response(client, jwt, app):
     print('\n' + 'request: ' + path + '\n')
     response = client.get(path, headers=headers)
     print(response)
+
 
 # @pytest.mark.xfail(raises=ValueError)
 def test_contains_words_to_avoid_request_response(client, jwt, app):
@@ -152,6 +445,7 @@ def test_contains_words_to_avoid_request_response(client, jwt, app):
     response = client.get(path, headers=headers)
     print(response)
 
+
 # @pytest.mark.xfail(raises=ValueError)
 def test_designation_mismatch_request_response(client, jwt, app):
     # create JWT & setup header with a Bearer Token using the JWT
@@ -170,6 +464,7 @@ def test_designation_mismatch_request_response(client, jwt, app):
     print('\n' + 'request: ' + path + '\n')
     response = client.get(path, headers=headers)
     print(response)
+
 
 # @pytest.mark.xfail(raises=ValueError)
 def test_too_many_words_request_response(client, jwt, app):
@@ -190,6 +485,7 @@ def test_too_many_words_request_response(client, jwt, app):
     response = client.get(path, headers=headers)
     print(response)
 
+
 # @pytest.mark.xfail(raises=ValueError)
 def test_name_requires_consent_request_response(client, jwt, app):
     # create JWT & setup header with a Bearer Token using the JWT
@@ -209,6 +505,7 @@ def test_name_requires_consent_request_response(client, jwt, app):
     response = client.get(path, headers=headers)
     print(response)
 
+
 # @pytest.mark.xfail(raises=ValueError)
 def test_contains_unclassifiable_word_request_response(client, jwt, app):
     # create JWT & setup header with a Bearer Token using the JWT
@@ -227,6 +524,7 @@ def test_contains_unclassifiable_word_request_response(client, jwt, app):
     print('\n' + 'request: ' + path + '\n')
     response = client.get(path, headers=headers)
     print(response)
+
 
 # @pytest.mark.xfail(raises=ValueError)
 def test_corporate_name_conflict_request_response(client, jwt, app):
