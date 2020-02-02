ee7b83a5957ec4a81e2185bf07c6e95344cfee5a
Lucas <tech@firebrandwebsolutions.com>
2020-02-01 23:15:34 -0800

    Use jsonpickle to serialize response objects. Refactor a couple things in AnalysisResponseStrategy and add some meat to its variants.

--- api/namex/resources/auto_analyse/analysis_strategies.py
+++ api/namex/resources/auto_analyse/analysis_strategies.py
@@ -15,80 +15,200 @@ from .response_objects.descriptive_word import DescriptiveWord
 class ValidNameResponseStrategy(AnalysisResponseStrategy):
     issue_type = AnalysisResultCodes.VALID_NAME
     status_text = 'Approved'
+    issue = NameAnalysisIssue(
+        consentingBody=None,
+        designations=None,
+        descriptiveWords=None,
+        issueType=issue_type,
+        word=None,
+        wordIndex=None,
+        showExaminationButton=None,
+        conflicts=None,
+        options=None,
+        nameActions=None
+    )
 
-    def create_issue(self):
-        issue = super(ValidNameResponseStrategy, self).create_new_issue()
+    @classmethod
+    def create_issue(cls):
+        issue = cls.issue
         return issue
 
 
 class AddDistinctiveWordResponseStrategy(AnalysisResponseStrategy):
     issue_type = AnalysisResultCodes.ADD_DISTINCTIVE_WORD
     status_text = 'Further Action Required'
+    issue = NameAnalysisIssue(
+        consentingBody=None,
+        designations=None,
+        descriptiveWords=None,
+        issueType=issue_type,
+        word=None,
+        wordIndex=None,
+        showExaminationButton=None,
+        conflicts=None,
+        options=None,
+        nameActions=None
+    )
 
-    def create_issue(self):
-        issue = super(AddDistinctiveWordResponseStrategy, self).create_new_issue()
+    @classmethod
+    def create_issue(cls):
+        issue = cls.issue
         return issue
 
 
 class AddDescriptiveWordResponseStrategy(AnalysisResponseStrategy):
     issue_type = AnalysisResultCodes.ADD_DESCRIPTIVE_WORD
     status_text = 'Further Action Required'
+    issue = NameAnalysisIssue(
+        consentingBody=None,
+        designations=None,
+        descriptiveWords=None,
+        issueType=issue_type,
+        word=None,
+        wordIndex=None,
+        showExaminationButton=None,
+        conflicts=None,
+        options=None,
+        nameActions=None
+    )
 
-    def create_issue(self):
-        issue = super(AddDescriptiveWordResponseStrategy, self).create_new_issue()
+    @classmethod
+    def create_issue(cls):
+        issue = cls.issue
         return issue
 
 
 class ContainsWordsToAvoidResponseStrategy(AnalysisResponseStrategy):
     issue_type = AnalysisResultCodes.WORD_TO_AVOID
     status_text = 'Further Action Required'
+    issue = NameAnalysisIssue(
+        consentingBody=None,
+        designations=None,
+        descriptiveWords=None,
+        issueType=issue_type,
+        word=None,
+        wordIndex=None,
+        showExaminationButton=None,
+        conflicts=None,
+        options=None,
+        nameActions=None
+    )
 
-    def create_issue(self):
-        issue = super(ContainsWordsToAvoidResponseStrategy, self).create_new_issue()
+    @classmethod
+    def create_issue(cls):
+        issue = cls.issue
         return issue
 
 
 class DesignationMismatchResponseStrategy(AnalysisResponseStrategy):
     issue_type = AnalysisResultCodes.DESIGNATION_MISMATCH
     status_text = 'Further Action Required'
+    issue = NameAnalysisIssue(
+        consentingBody=None,
+        designations=None,
+        descriptiveWords=None,
+        issueType=issue_type,
+        word=None,
+        wordIndex=None,
+        showExaminationButton=None,
+        conflicts=None,
+        options=None,
+        nameActions=None
+    )
 
-    def create_issue(self):
-        issue = super(DesignationMismatchResponseStrategy, self).create_new_issue()
+    @classmethod
+    def create_issue(cls):
+        issue = cls.issue
         return issue
 
 
 class TooManyWordsResponseStrategy(AnalysisResponseStrategy):
     issue_type = AnalysisResultCodes.TOO_MANY_WORDS
     status_text = 'Further Action Required'
+    issue = NameAnalysisIssue(
+        consentingBody=None,
+        designations=None,
+        descriptiveWords=None,
+        issueType=issue_type,
+        word=None,
+        wordIndex=None,
+        showExaminationButton=None,
+        conflicts=None,
+        options=None,
+        nameActions=None
+    )
 
-    def create_issue(self):
-        issue = super(TooManyWordsResponseStrategy, self).create_new_issue()
+    @classmethod
+    def create_issue(cls):
+        issue = cls.issue
         return issue
 
 
 class NameRequiresConsentResponseStrategy(AnalysisResponseStrategy):
     issue_type = AnalysisResultCodes.NAME_REQUIRES_CONSENT
     status_text = 'May be Approved With Consent'
+    issue = NameAnalysisIssue(
+        consentingBody=ConsentingBody(
+            name='Test Body',
+            email='test@example.com'
+        ),
+        designations=None,
+        descriptiveWords=None,
+        issueType=issue_type,
+        word=None,
+        wordIndex=None,
+        showExaminationButton=None,
+        conflicts=None,
+        options=None,
+        nameActions=None
+    )
 
-    def create_issue(self):
-        issue = super(NameRequiresConsentResponseStrategy, self).create_new_issue()
+    @classmethod
+    def create_issue(cls):
+        issue = cls.issue
         return issue
 
 
 class ContainsUnclassifiableWordResponseStrategy(AnalysisResponseStrategy):
     issue_type = AnalysisResultCodes.CONTAINS_UNCLASSIFIABLE_WORD
     status_text = 'Further Action Required'
+    issue = NameAnalysisIssue(
+        consentingBody=None,
+        designations=None,
+        descriptiveWords=None,
+        issueType=issue_type,
+        word=None,
+        wordIndex=None,
+        showExaminationButton=None,
+        conflicts=None,
+        options=None,
+        nameActions=None
+    )
 
-    def create_issue(self):
-        issue = super(ContainsUnclassifiableWordResponseStrategy, self).create_new_issue()
+    @classmethod
+    def create_issue(cls):
+        issue = cls.issue
         return issue
 
 
 class CorporateNameConflictResponseStrategy(AnalysisResponseStrategy):
     issue_type = AnalysisResultCodes.CORPORATE_CONFLICT
     status_text = 'Further Action Required'
+    issue = NameAnalysisIssue(
+        consentingBody=None,
+        designations=None,
+        descriptiveWords=None,
+        issueType=issue_type,
+        word=None,
+        wordIndex=None,
+        showExaminationButton=None,
+        conflicts=None,
+        options=None,
+        nameActions=None
+    )
 
-    def create_issue(self):
-        issue = super(CorporateNameConflictResponseStrategy, self).create_new_issue()
+    @classmethod
+    def create_issue(cls):
+        issue = cls.issue
         return issue
 
