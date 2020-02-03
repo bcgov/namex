daf18cc54cf23451ae18257469b9d6e155d7a544
areyeslo <areyeslo@users.noreply.github.com>
2020-02-03 15:50:23 -0800

    getters for substitution_list, stop_words, prefixes, designations, stand-alone and test cases ran successfully.

--- api/tests/python/namex_services/name_request/test_name_request_utils.py
+++ api/tests/python/namex_services/name_request/test_name_request_utils.py
@@ -1,15 +1,17 @@
 from namex.services.name_request.auto_analyse.name_analysis_utils import \
-    data_frame_to_list, clean_name_words, regex_transform, remove_french, get_substitution_list
+    data_frame_to_list, clean_name_words, regex_transform, remove_french, get_substitution_list, get_stop_word_list, \
+    get_prefix_list, get_en_designation_any_list, get_en_designation_end_list, get_fr_designation_end_list, \
+    get_stand_alone_list, is_substitution_word
 
 from hamcrest import *
 import pandas as pd
 
 text = 'WM 4MULA VENTURES INC.'
-#text = 'W & M MOUNTAIN VENTURES INC.'
+# text = 'W & M MOUNTAIN VENTURES INC.'
 
 name = ['WM', 'MOUNTAIN', 'VENTURES']
 
-subs_word = 'MOUNTAIN'
+substitution_word = '4mula'
 list_dist = ['WM', '4BY4']
 list_desc = ['VENTURES']
 list_none = []
@@ -18,20 +20,24 @@ stop_words = ['an', 'and', 'are', 'as', 'at', 'be', 'but', 'by', 'for', \
               'if', 'in', 'into', 'is', 'it', 'no', 'not', 'of', 'on', 'or', \
               'such', 'that', 'the', 'their', 'then', 'there', 'these', \
               'they', 'this', 'to']
-dsg_any = ['association', 'assoc', 'assoc.', 'assn', 'assn.', 'society', 'soc', \
-           'soc.', 'foundation', 'co-operative', 'co-op', 'coop', 'cooperative']
+en_designation_any_list = ['association', 'assoc', 'assoc.', 'assn', 'assn.', 'society', 'soc', \
+                           'soc.', 'foundation', 'co-operative', 'co-op', 'coop', 'cooperative']
 
-dsg_end = ['llc', 'l.l.c', 'limited liability company', 'llp', 'limited liability partnership', \
-           'limited partnership', 'limited', 'ltd', 'ltd.', 'incorporated', 'inc', 'inc.', \
-           'corporation', 'corp', 'corp.', 'ulc.', 'ulc', 'unlimited liability company', \
-           'company', 'co', 'co.', 'liability']
+en_designation_end_list = ['llc', 'l.l.c', 'limited liability company', 'llp', 'limited liability partnership', \
+                           'limited partnership', 'limited', 'ltd', 'ltd.', 'incorporated', 'inc', 'inc.', \
+                           'corporation', 'corp', 'corp.', 'ulc.', 'ulc', 'unlimited liability company', \
+                           'company', 'co', 'co.', 'liability']
 
-subs_list = [['accelerate', ' xlr8', ' xlreight'], ['access', ' axis', ' axys'], \
-             ['cosi', ' cosy', ' cozi', ' cozy'], ['acqua', ' acwa', ' aqua'], \
-             ['aerial', ' arial', ' ariel']]
+fr_designation_end_list = ['ltee', 'ltee.', 'incorporee', ' societe a responsabilite limitee', \
+                           'societe en nom collectif a responsabilite limitee', 'slr', 'sencrl', \
+                           'limitee', 'incorporee']
 
-french_desig_list = ["societe a responsabilite limitee", "societe en nom collectif a responsabilite limitee", \
-                     "limitee", "ltee", "ltee.", "incorporee", "slr", "sencrl"]
+stand_alone_list = ['bc', 'holdings', 'ventures', 'solutions', 'enterprise', 'industries']
+
+prefix_list = ['un', 're', 'in', 'dis', 'en', 'non', 'in', 'over', 'mis', 'sub', 'pre', 'inter', 'fore', 'de', 'trans', \
+               'super', 'semi', 'anti', 'mid', 'under', 'ante', 'bene', 'circum', 'co', 'com', 'con', 'col', 'dia', \
+               'ex', 'homo', 'hyper', 'mal', 'micro', 'multi', 'para', 'poly', 'post', 'pro', 'retro', 'tele', \
+               'therm', 'trans', 'uni']
 
 data = [['WM', 'Distinctive', 12], ['079', 'Distinctive', 3], ['VENTURES', 'Descriptive', 1000]]
 df = pd.DataFrame(data, columns=['word', 'word_classification', 'frequency'])
@@ -45,16 +51,45 @@ def test_data_frame_to_list(client, jwt, app):
 
 
 def test_remove_french(client, jwt, app):
-    assert_that(remove_french(text, french_desig_list), 'W & M 074 VENTURES INC.')
+    assert_that(remove_french(text, fr_designation_end_list), 'W & M 074 VENTURES INC.')
 
 
 def test_get_substitution_list(client, jwt, app):
-    assert_that(get_substitution_list(subs_word), ['mount', 'mountain', 'mt', 'mtn'])
+    assert_that(get_substitution_list(substitution_word), ['mount', 'mountain', 'mt', 'mtn'])
+
+
+def test_is_substitution_word(client, jwt, app):
+    assert_that(is_substitution_word(substitution_word), True)
+
+
+def test_get_stop_word_list(client, jwt, app):
+    assert_that(get_stop_word_list(), stop_words)
+
+
+def test_get_prefix_list(client, jwt, app):
+    assert_that(get_prefix_list(), prefix_list)
+
+
+def test_get_en_designation_any_list(client, jwt, app):
+    assert_that(get_en_designation_any_list(), en_designation_any_list)
+
+
+def test_get_en_designation_end_list(client, jwt, app):
+    assert_that(get_en_designation_end_list(), en_designation_end_list)
+
+
+def test_get_fr_designation_end_list(client, jwt, app):
+    assert_that(get_fr_designation_end_list(), fr_designation_end_list)
+
+
+def test_get_stand_alone_list(client, jwt, app):
+    assert_that(get_stand_alone_list(), stand_alone_list)
 
 
 def test_regex_transform(client, jwt, app):
-    assert_that(regex_transform(text, stop_words, dsg_any, dsg_end, subs_list), 'WM 4MULA VENTURES')
+    assert_that(regex_transform(text, en_designation_any_list, en_designation_end_list, prefix_list), 'WM 4MULA VENTURES')
 
 
 def test_clean_name_words(client, jwt, app):
-    assert_that(clean_name_words(text, stop_words, dsg_any, dsg_end, subs_list), 'WM 074 VENTURES')
+    assert_that(clean_name_words(text, stop_words, en_designation_any_list, en_designation_end_list, fr_designation_end_list, \
+                                 prefix_list), 'WM 074 VENTURES')
