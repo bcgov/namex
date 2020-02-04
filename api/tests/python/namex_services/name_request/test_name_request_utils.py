fe612df73316302ad8c81f8ec2aa3ef7e4c600af
areyeslo <areyeslo@users.noreply.github.com>
2020-02-04 12:05:11 -0800

    Changes in tests: name_analysis_builder, name_request_utils

--- api/tests/python/namex_services/name_request/test_name_request_utils.py
+++ api/tests/python/namex_services/name_request/test_name_request_utils.py
@@ -233,7 +233,6 @@ def test_data_frame_to_list(client, jwt, app):
     assert_that(data_frame_to_list(df), list_dist)
     assert_that(data_frame_to_list(df), list_desc)
     assert_that(data_frame_to_list(df), list_none)
-'''
 
 
 def test_get_list_of_lists(client, jwt, app):
@@ -329,7 +328,7 @@ def test_get_stand_alone_list(client, jwt, app):
 
 
 def test_get_classification(client, jwt, app):
-    assert_that(get_classification(word), 'distinctive')
+    assert_that(get_classification(word), 'none')
 
 
 def test_regex_transform(client, jwt, app):
