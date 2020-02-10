1ade10724ac9122a214c7655651db9cb15262dcd
Lucas <tech@firebrandwebsolutions.com>
2020-02-10 14:01:57 -0800

    Refactor a bunch of stuff. Move name preprocessing out into its own service. Untested - just checking in my changes.

---
+++ api/namex/models/synonym.py
@@ -0,0 +1,44 @@
+from . import db, ma
+
+
+# The class that corresponds to the database table for synonyms.
+class Synonym(db.Model):
+    __tablename__ = 'synonym'
+    __bind_key__ = 'synonyms'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    category = db.Column(db.String(100))
+    synonyms_text = db.Column(db.String(1000), unique=True, nullable=False)
+    stems_text = db.Column(db.String(1000),nullable=False)
+    comment = db.Column(db.String(1000))
+    enabled = db.Column(db.Boolean(), default=True)
+
+    def json(self):
+        return {"id": self.id, "category": self.category, "synonymsText": self.synonyms_text,
+                "stemsText": self.stems_text, "comment":self.comment, "enabled": self.enabled}
+
+    @classmethod
+    def find(cls, term, col):
+        print('finding {} for {}'.format(col, term))
+        synonyms_list = []
+        term = term.lower()
+        if col == 'synonyms_text':
+            rows = cls.query.filter(Synonym.synonyms_text.ilike('%' + term + '%')).all()
+            for row in rows:
+                synonyms = [synonym.strip().lower() for synonym in row.synonyms_text.split(',')]
+                if term in synonyms:
+                    synonyms_list.append(row)
+        # col == stems_text
+        else:
+            rows = cls.query.filter(Synonym.stems_text.ilike('%' + term + '%')).all()
+            for row in rows:
+                synonyms = [synonym.strip().lower() for synonym in row.stems_text.split(',')]
+                if term in synonyms:
+                    synonyms_list.append(row)
+
+        return synonyms_list
+
+
+class SynonymSchema(ma.ModelSchema):
+    class Meta:
+        model = Synonym
--- api/namex/services/name_processing/name_processing.py
+++ api/namex/services/name_processing/name_processing.py
@@ -1,169 +1,71 @@
-import re
-import warnings
-
-from ..name_request.auto_analyse.name_analysis_utils import remove_french, remove_stop_words
-
-from namex.services.synonyms.synonym import SynonymService
-from namex.services.word_classification.word_classification import WordClassificationService
-
-from .mixins.get_synonym_lists import GetSynonymListsMixin
-
-'''
-Service for pre-processing of a user submitted name request name string.
-Setting the name using NameProcessingService.set_name will clean the name and set the following properties:
-@:prop name_as_submitted The original name string
-@:prop name_as_submitted_tokenized The original name tokenized handling designations as one token. 
-                                    For instance, if original name has limited liability company, this compound designation is made a token.
-@:prop name_original_tokens The original name tokenized without any special handling.
-@:prop processed_name The cleaned name
-@:prop name_tokens Word tokens generated from the cleaned name
-'''
+from ..name_request.auto_analyse.name_analysis_utils import clean_name_words, data_frame_to_list
 
 
-class NameProcessingService(GetSynonymListsMixin):
-    @property
-    def name_as_submitted(self):
-        return self._name_as_submitted
+class NameProcessingService:
+    _synonyms_api = None
 
-    @property
-    def name_as_submitted_tokenized(self):
-        return self._name_as_submitted_tokenized
+    _prefixes = []
+    _stop_words = []
+    _designated_end_words = []
+    _designated_any_words = []
 
-    @property
-    def name_original_tokens(self):
-        return self._name_original_tokens
-
-    @name_as_submitted.setter
-    def name_as_submitted(self, val):
-        self._name_as_submitted = val
-
-    @name_as_submitted_tokenized.setter
-    def name_as_submitted_tokenized(self, val):
-        self._name_as_submitted_tokenized = val
-
-    @name_original_tokens.setter
-    def name_original_tokens(self, val):
-        self._name_original_tokens = val
-
-    @property
-    def processed_name(self):
-        return self._processed_name
-
-    @processed_name.setter
-    def processed_name(self, val):
-        self._processed_name = val
-
-    @property
-    def name_tokens(self):
-        return self._name_tokens
-
-    @name_tokens.setter
-    def name_tokens(self, val):
-        self._name_tokens = val
-
-    @property
-    def word_classification_service(self):
-        return self._word_classification_service
-
-    @word_classification_service.setter
-    def word_classification_service(self, svc):
-        self._word_classification_service = svc
-
-    @property
-    def synonym_service(self):
-        return self._synonym_service
-
-    @synonym_service.setter
-    def synonym_service(self, svc):
-        self._synonym_service = svc
-
-    def __init__(self):
-        self.synonym_service = SynonymService()
-        self.word_classification_service = WordClassificationService()
-        self.name_as_submitted = None
-        self.name_as_submitted_tokenized = None
-        self.name_original_tokens = None
-        self.processed_name = None
-        self.name_tokens = None
-        self.distinctive_word_tokens = None
-        self.descriptive_word_tokens = None
-        self.unclassified_word_tokens = None
+    _name_as_submitted = ''
+    _preprocessed_name = ''
+    _list_name_words = []
+    _list_dist_words = []
+    _list_desc_words = []
+    _list_none_words = []
 
     '''
-    Set and process a submitted name string using the process_name class method.
+    Prepare any data required to process the name.
     '''
-    def set_name(self, name):
-        self.name_as_submitted = name  # Store the user's submitted name string
-        self.name_original_tokens = name.lower().split()
-        self._process_name()
-
-    def set_name_tokenized(self, name):
-        all_designations = self._designated_all_words
-        all_designations.sort(key=len, reverse=True)
-        designation_alternators = '|'.join(map(re.escape, all_designations))
-        regex = re.compile(r'(?<!\w)({}|[a-z-A-Z]+)(?!\w)'.format(designation_alternators))
-        self.name_as_submitted_tokenized = regex.findall(name.lower())
-
-    def _clean_name_words(self, text, stop_words=[], designation_any=[], designation_end=[], designation_all=[], fr_designation_end_list=[], prefix_list=[], number_list=[]):
-        if not text or not stop_words or not designation_any or not designation_end or not prefix_list and not number_list:
-            warnings.warn("Parameters in clean_name_words function are not set.", Warning)
-
-        syn_svc = self.synonym_service
-
-        words = text.lower()
-        words = remove_stop_words(words, stop_words)
-        words = remove_french(words)
-        exceptions_ws = syn_svc.exception_regex(words)
-        tokens = syn_svc.regex_transform(words, designation_all, prefix_list, number_list, exceptions_ws)
-        tokens = tokens.split()
-
-        return [x.lower() for x in tokens if x]
-
-    def _prepare_data(self):
-        syn_svc = self.synonym_service
-
+    def prepare_data(self):
         # Query database for word designations
-        # These properties are mixed in via GetSynonymListsMixin
-        # See the class constructor
-        self._stop_words = syn_svc.get_stop_words()
-        self._prefixes = syn_svc.get_prefixes()
-        self._number_words = syn_svc.get_number_words()
-        self._designated_end_words = syn_svc.get_designated_end_all_words()
-        self._designated_any_words = syn_svc.get_designated_any_all_words()
-        self._designated_all_words = list(set(self._designated_any_words +
-                                              self._designated_end_words))
-        self._designated_all_words.sort(key=len, reverse=True)
-        # TODO: Handle french designations
-        self._fr_designation_end_list = []
+        self._stop_words = self._synonyms_api.get_stop_words()
+        self._designated_end_words = self._synonyms_api.get_designated_end_words()
+        self._designated_any_words = self._synonyms_api.get_designated_any_words()
 
     '''
-    Split a name string into classifiable tokens. Called whenever set_name is invoked.
+    Split a name string into classifiable tokens. Called internally whenever set_name is invoked.
     @:param string:name
     '''
-    def _process_name(self):
-        try:
-            # Prepare any data that we need to pre-process the name
-            self._prepare_data()
+    def preprocess_name(self):
+        if not self._name_as_submitted:
+            return  # TODO: Should we throw an error or something?
 
-            # Clean the provided name and tokenize the string
-            self.name_tokens = self._clean_name_words(
-                self.name_as_submitted,
-                # These properties are mixed in via GetSynonymListsMixin
-                # See the class constructor
-                self._stop_words,
-                self._designated_any_words,
-                self._designated_end_words,
-                self._designated_all_words,
-                self._fr_designation_end_list,
-                self._prefixes,
-                self._number_words
-            )
+        # Clean the provided name and tokenize the string
+        # Identify stop words, any words and end words an store the lists to our director instance
+        self._list_name_words = clean_name_words(
+            self._name_as_submitted,
+            self._stop_words,
+            self._designated_any_words,
+            self._designated_end_words,
+            [],  # French words
+            self._prefixes
+        )
 
-            # Store clean, processed name to instance
-            clean_name = ' '.join(map(str, self.name_tokens))
-            self.processed_name = clean_name
-            print('Processed name: ' + self.processed_name)
+        # Store results to instance TODO: Don't delete this! Not sure how I am going to re-implement this yet...
+        # self._list_name_words, self._list_desc_words, self._list_none_words = data_frame_to_list(cf)
 
-        except Exception as error:
-            print('Pre-processing name failed: ' + repr(error))
-            raise
+        # Store clean, preprocessed name to instance
+        self.set_preprocessed_name(' '.join(map(str, self.get_list_name())))
+
+    def set_preprocessed_name(self, name):
+        # Process the name
+        self._preprocessed_name = name  # Store the user's submitted name string
+
+    # API for extending implementations
+    def get_preprocessed_name(self):
+        return self._preprocessed_name
+
+    def get_list_name(self):
+        return self._list_name_words
+
+    def get_list_dist(self):
+        return self._list_dist_words
+
+    def get_list_desc(self):
+        return self._list_desc_words
+
+    def get_list_none(self):
+        return self._list_none_words
