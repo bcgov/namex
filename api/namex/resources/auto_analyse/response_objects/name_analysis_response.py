793e5aefc875696cf08a3b34024f26068848caad
Lucas <tech@firebrandwebsolutions.com>
2020-02-13 12:02:19 -0800

    Tweak name request responses.

--- api/namex/resources/auto_analyse/response_objects/name_analysis_response.py
+++ api/namex/resources/auto_analyse/response_objects/name_analysis_response.py
@@ -3,5 +3,6 @@ from . import Serializable
 
 class NameAnalysisResponse(Serializable):
     def __init__(self, **kwargs):
+        self.header = kwargs['header'] or ''  # TODO: Can we use 'heading' instead?
         self.status = kwargs['status'] or ''
         self.issues = kwargs['issues'] or []
