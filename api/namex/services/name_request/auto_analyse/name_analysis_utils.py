66897a779ea525214ddff55d3f04e8ede84d99f7
areyeslo <areyeslo@users.noreply.github.com>
2020-02-14 12:07:11 -0800

    Include function to return all possible combinations when a name contains words with both classifications: words_distinctive_descriptive function

--- api/namex/services/name_request/auto_analyse/name_analysis_utils.py
+++ api/namex/services/name_request/auto_analyse/name_analysis_utils.py
@@ -5,7 +5,6 @@ import collections
 from sqlalchemy import create_engine
 from toolz import unique
 
-
 from namex.services.name_request.auto_analyse import DataFrameFields
 
 POSTGRES_ADDRESS = 'localhost'
@@ -13,7 +12,7 @@ POSTGRES_PORT = '5432'
 POSTGRES_USERNAME = 'postgres'
 POSTGRES_PASSWORD = ' '
 POSTGRES_DBNAME = 'namex-auto-analyse'
-#POSTGRES_DBNAME_WC = 'namex-local'
+# POSTGRES_DBNAME_WC = 'namex-local'
 
 postgres_str = ('postgresql://{username}:{password}@{ipaddress}:{port}/{dbname}'.format(username=POSTGRES_USERNAME,
                                                                                         password=POSTGRES_PASSWORD,
@@ -21,18 +20,19 @@ postgres_str = ('postgresql://{username}:{password}@{ipaddress}:{port}/{dbname}'
                                                                                         port=POSTGRES_PORT,
                                                                                         dbname=POSTGRES_DBNAME))
 
-#postgres_wc_str = ('postgresql://{username}:{password}@{ipaddress}:{port}/{dbname}'.format(username=POSTGRES_USERNAME,
+# postgres_wc_str = ('postgresql://{username}:{password}@{ipaddress}:{port}/{dbname}'.format(username=POSTGRES_USERNAME,
 #                                                                                           password=POSTGRES_PASSWORD,
 #                                                                                           ipaddress=POSTGRES_ADDRESS,
 #                                                                                           port=POSTGRES_PORT,
 #                                                                                           dbname=POSTGRES_DBNAME_WC))
 
 cnx = create_engine(postgres_str)
-#cnx_wc = create_engine(postgres_wc_str)
+
+
+# cnx_wc = create_engine(postgres_wc_str)
 
 # TODO: Fix caps and stuff...
 def data_frame_to_list(df):
-
     df_dist = df.loc[df.word_classification == DataFrameFields.DISTINCTIVE.value]
     df_desc = df.loc[df.word_classification == DataFrameFields.DESCRIPTIVE.value]
     df_none = df.loc[df.word_classification == DataFrameFields.UNCLASSIFIED.value]
@@ -44,7 +44,8 @@ def data_frame_to_list(df):
     return list_dist, list_desc, list_none
 
 
-def clean_name_words(text, stop_words=[], designation_any=[], designation_end=[], fr_designation_end_list=[], prefix_list=[]):
+def clean_name_words(text, stop_words=[], designation_any=[], designation_end=[], fr_designation_end_list=[],
+                     prefix_list=[]):
     # TODO: Warn or something if params aren't set!
     words = text.lower()
     words = ' '.join([word for x, word in enumerate(words.split(" ")) if x == 0 or word not in stop_words])
@@ -221,7 +222,9 @@ def get_designation_by_entity_type(entity_type):
     df = pd.read_sql_query(query, cnx)
 
     if not df.empty:
-        designation_value_list = {re.sub(r'.*(any).*|.*(end).*', r'\1\2', x[0], 0, re.IGNORECASE): ''.join(x[1:]).split(",") for x in df.itertuples(index=False)}
+        designation_value_list = {
+            re.sub(r'.*(any).*|.*(end).*', r'\1\2', x[0], 0, re.IGNORECASE): ''.join(x[1:]).split(",") for x in
+            df.itertuples(index=False)}
         return designation_value_list
 
     return None
@@ -237,9 +240,9 @@ def get_designation_end_in_name(name):
 
     # Getting list of lists where the first list contains designations of type "anywhere" and the second list contains designations of type "end".
     # [['association],['limited partnership']
-    designation_end_list=[list(elem) for elem in found_designation_end]
+    designation_end_list = [list(elem) for elem in found_designation_end]
     if any(isinstance(el, list) for el in designation_end_list):
-        designation_end_list=get_flat_list(designation_end_list)
+        designation_end_list = get_flat_list(designation_end_list)
     designation_end_list = list(filter(None, designation_end_list))
     designation_end_list = list(dict.fromkeys(designation_end_list))
 
@@ -249,7 +252,7 @@ def get_designation_end_in_name(name):
 def get_designation_any_in_name(name):
     en_designation_any_all_list = get_en_designation_any_all_list()
     designation_any_rgx = '(' + '|'.join(map(str, en_designation_any_all_list)) + ')'
-    designation_any_regex= r'\b' + designation_any_rgx + '(?=\s)'
+    designation_any_regex = r'\b' + designation_any_rgx + '(?=\s)'
 
     # Returns list of tuples
     found_designation_any = re.findall(designation_any_regex, name.lower())
@@ -280,19 +283,21 @@ def get_wrong_place_any_designations(name):
 def get_wrong_place_end_designations(name):
     en_designation_end_all_list = get_en_designation_end_all_list()
     designation_any_rgx = '(' + '|'.join(map(str, en_designation_end_all_list)) + ')'
-    designation_any_regex= r'\b' + designation_any_rgx + '(?=\s)'
+    designation_any_regex = r'\b' + designation_any_rgx + '(?=\s)'
 
     # Returns list of tuples
     wrong_designation_any_list = re.findall(designation_any_regex, name.lower())
 
     return wrong_designation_any_list
 
+
 def get_entity_type_end_designation(entity_end_designation_dict, all_designation_any_end_list):
     entity_type_end_designation_name = list()
     for designation_end in all_designation_any_end_list:
         entity_type_end_designation_name.extend(get_entity_type_by_value(entity_end_designation_dict, designation_end))
 
-    all_entity_types = [item for item, count in collections.Counter(entity_type_end_designation_name).items() if count > 1]
+    all_entity_types = [item for item, count in collections.Counter(entity_type_end_designation_name).items() if
+                        count > 1]
 
     if all_entity_types:
         return all_entity_types
@@ -530,44 +535,9 @@ def remove_french(text, fr_designation_end_list):
             compound.pop()
             text = ' '.join(map(str, compound))
     return text
-'''
-
-'''
-Previous behaviour: The section after slash considered french designations to imply the section was in French.
-Current behaviour: The section after slash is not longer considering french designation.
-Rules:  1) Before and after slash has to be at least two words to removed string after slash.
-           Eg.
-           ABC ENGINEERING/CENTRAL CARE  --> ABC ENGINEERING
-        2) In the case just having a word at the beginning then the string is kept with no changes.
-           Eg.
-           RE/MAX WALNUT DEVELOPMENT  --> RE/MAX WALNUT DEVELOPMENT
-        3) In the case just having a word at the end, this is kept removing the slash.
-           Eg.
-           ABC ENGINEERING 7/24 --> ABC ENGINEERING 7 24
-'''
 
 
-def remove_french(text):
-    text = re.sub(r'(^\w+(?:[^\w\n]+\w+)+[^\w\n]*)/(\w+(?:[^\w\n]+\w+)+[^\w\n]*$)?',
-                  r'\1 ',
-                  text,
-                  0,
-                  re.IGNORECASE)
-    return " ".join(text.split())
-
-
-def remove_stop_words(original_name, stop_words):
-    stop_words_rgx = '|'.join(stop_words)
-    regex = re.compile(r'\b({})\b'.format(stop_words_rgx))
-    found_stop_words = regex.findall(original_name.lower())
-
-    for word in found_stop_words:
-        original_name = original_name.replace(word, "")
-
-    return re.sub(' +', ' ', original_name)
-
-
-def list_distinctive_descriptive_same(name_list):
+def words_distinctive_descriptive(name_list):
     queue = collections.deque(name_list)
     dist_list = []
     desc_list = []
@@ -580,89 +550,11 @@ def list_distinctive_descriptive_same(name_list):
     for dist in dist_list:
         desc_list.append([i for i in name_list if i not in dist])
 
-    return dist_list, desc_list
-
-
-def validate_distinctive_descriptive_lists(list_name, list_dist, list_desc):
-    current_category = None
-    list_dist_tmp = []
-    list_desc_tmp = []
-    list_incorrect_classification = []
-    for idx, token_name, has_more in lookahead(list_name):
-        if idx == 0:
-            current_category = None
-        if ((token_name in list_dist and token_name in list_desc) or (
-                token_name not in list_dist and token_name not in list_desc)) and has_more and (
-                current_category is None):
-            list_dist_tmp.extend([token_name])
-            current_category = DataFrameFields.DISTINCTIVE.value
-        elif ((token_name in list_dist and token_name in list_desc) or (
-                token_name not in list_dist and token_name not in list_desc)) and has_more and (
-                current_category == DataFrameFields.DISTINCTIVE.value):
-            list_dist_tmp.extend([token_name])
-            list_desc_tmp.extend([token_name])
-            current_category = DataFrameFields.DISTINCTIVE.value
-        elif ((token_name in list_dist and token_name in list_desc) or (
-                token_name not in list_dist and token_name not in list_desc)) and has_more and (
-                current_category is None or current_category == DataFrameFields.DESCRIPTIVE.value):
-            list_desc_tmp.extend([token_name])
-            current_category = DataFrameFields.DESCRIPTIVE.value
-        elif token_name in list_dist and token_name not in list_desc and (
-                current_category is None or current_category == DataFrameFields.DISTINCTIVE.value) and has_more:
-            current_category = DataFrameFields.DISTINCTIVE.value
-            list_dist_tmp.extend([token_name])
-        elif token_name in list_desc and (
-                current_category == DataFrameFields.DISTINCTIVE.value or current_category == DataFrameFields.DESCRIPTIVE.value):
-            current_category = DataFrameFields.DESCRIPTIVE.value
-            list_desc_tmp.extend([token_name])
-        else:
-            list_incorrect_classification.append({token_name: idx})
-            # break
-
-    return list_dist_tmp, list_desc_tmp, list_incorrect_classification
-
-
-def list_distinctive_descriptive(name_list, dist_list, desc_list):
-    queue_dist = collections.deque(dist_list)
-
-    if dist_list == name_list:
-        queue_dist.pop()
-
-    dist_list_tmp, dist_list_all, desc_list_tmp, desc_list_all = [], [], [], []
-
-    dist_list_tmp.append(list(queue_dist))
-
-    while len(queue_dist) > 1:
-        queue_dist.pop()
-        dist_list_tmp.append(list(queue_dist))
-
-    dist_list_tmp.reverse()
-
-    for dist in dist_list_tmp:
-        desc_list_tmp.append([i for i in name_list if i not in dist and i in desc_list])
-
-    # Validate generation of list of lists of distinctives and descriptives with the correct combinations:
-    for idx, element in enumerate(dist_list_tmp):
-        if (dist_list_tmp[idx] + desc_list_tmp[idx]) == name_list:
-            dist_list_all.append(dist_list_tmp[idx])
-            desc_list_all.append(desc_list_tmp[idx])
-
-    return dist_list_all, desc_list_all
-
-
-def lookahead(iterable):
-    """Pass through all values from the given iterable, augmented by the
-    information if there are more values to come after the current one
-    (True), or if it is the last value (False).
-    """
-    # Get an iterator and pull the first value.
-    it = iter(iterable)
-    last = next(it)
-    # Run the iterator to exhaustion (starting from the second value).
     idx = 0
-    for idx, val in enumerate(it):
-        # Report the *previous* value (more to come).
-        yield idx, last, True
-        last = val
-    # Report the last value.
-    yield idx + 1, last, False
+    for dist, desc in zip(dist_list, desc_list):
+        if not dist + desc == name_list:
+            dist_list.pop(idx)
+            desc_list.pop(idx)
+        idx += 1
+
+    return dist_list, desc_list
