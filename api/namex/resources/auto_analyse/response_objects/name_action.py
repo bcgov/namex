ca9d5a59160d792b4089ce259f7b34b30548723c
Lucas <tech@firebrandwebsolutions.com>
2020-02-18 11:06:00 -0800

    Fix int check in NameAction response object.

--- api/namex/resources/auto_analyse/response_objects/name_action.py
+++ api/namex/resources/auto_analyse/response_objects/name_action.py
@@ -35,15 +35,18 @@ class NameAction(Serializable):
     def __init__(self, **kwargs):
         # Required!
         if not kwargs.get('type').value in NameActions.list():
-            raise TypeError('Invalid NameAction')
+            raise TypeError('Invalid NameAction, please supply a valid NameAction type')
 
         if kwargs.get('position') and kwargs['position'].value not in WordPositions.list():
-            raise TypeError('Invalid word position')
+            raise TypeError('Invalid word position, please supply a valid WordPositions type')
+
+        if kwargs.get('index') and (isinstance(kwargs['index'], int) is False or kwargs['index'] < 0):
+            raise TypeError('Invalid index, index must be a positive integer or zero')
 
         # TODO: Make sure index is valid! Add validation.
 
         self.type = kwargs['type'].value
-        self.index = kwargs['index'] if kwargs.get('index') else None
+        self.index = kwargs['index'] if kwargs.get('index') > -1 else None  # Ensure positive index
         self.word = kwargs['word'] if kwargs.get('word') else None
         self.position = kwargs['position'].value if kwargs.get('position') else None  # [start | end] (is there another?)
         self.message = kwargs['message'] if kwargs.get('message') else None  # <string> | None
