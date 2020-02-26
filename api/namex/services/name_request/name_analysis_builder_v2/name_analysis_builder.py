2cc96e748dc4aad093b634331dd37ffe95bd4a86
Lucas <tech@firebrandwebsolutions.com>
2020-02-26 11:10:39 -0800

    Fix some issues.

--- api/namex/services/name_request/name_analysis_builder_v2/name_analysis_builder.py
+++ api/namex/services/name_request/name_analysis_builder_v2/name_analysis_builder.py
@@ -54,6 +54,16 @@ class NameAnalysisBuilder(AbstractNameAnalysisBuilder):
         elif len(list_desc) < 1:
             result.is_valid = False
             result.result_code = AnalysisResultCodes.ADD_DESCRIPTIVE_WORD
+        # TODO: We need another check here but we also need unclassified words check before we run check name is well formed
+        elif len(list_none) > 0:
+            unclassified_words_list_response = []
+            for idx, token in enumerate(list_name):
+                if any(token in word for word in list_none):
+                    unclassified_words_list_response.append({idx: token})
+
+            result.is_valid = False
+            result.result_code = AnalysisResultCodes.CONTAINS_UNCLASSIFIABLE_WORD
+            result.values = unclassified_words_list_response
         elif len(list_name) > MAX_LIMIT:
             result.is_valid = False
             result.result_code = AnalysisResultCodes.TOO_MANY_WORDS
@@ -69,12 +79,13 @@ class NameAnalysisBuilder(AbstractNameAnalysisBuilder):
         result = ProcedureResult()
         result.is_valid = True
 
+        # TODO: Arturo plz check the word against the list, provide it as an input for get_virtual_word_condition_service().get_words_to_avoid()
         all_words_to_avoid_list = self.get_virtual_word_condition_service().get_words_to_avoid()
         words_to_avoid_list = []
 
         for words_to_avoid in all_words_to_avoid_list:
             if words_to_avoid.lower() in name.lower():
-                words_to_avoid_list.append(words_to_avoid)
+                words_to_avoid_list.append(words_to_avoid.lower())
 
         words_to_avoid_list_response = []
 
@@ -147,7 +158,7 @@ class NameAnalysisBuilder(AbstractNameAnalysisBuilder):
 
         for words_consent in all_words_consent_list:
             if words_consent.lower() in name.lower():
-                words_consent_list.append(words_consent)
+                words_consent_list.append(words_consent.lower())
 
         words_consent_list_response = []
 
@@ -157,7 +168,7 @@ class NameAnalysisBuilder(AbstractNameAnalysisBuilder):
 
         if words_consent_list_response:
             result.is_valid = False
-            result.result_code = AnalysisResultCodes.WORD_TO_AVOID
+            result.result_code = AnalysisResultCodes.NAME_REQUIRES_CONSENT
             result.values = words_consent_list_response
 
         return result
