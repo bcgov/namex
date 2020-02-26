c42c038e4aa38f19edbc57b5e392b8cf57bf0b35
Lucas <tech@firebrandwebsolutions.com>
2020-02-26 14:01:18 -0800

    Prune out more stuff.

--- api/namex/services/synonyms/synonym.py
+++ api/namex/services/synonyms/synonym.py
@@ -140,7 +140,6 @@ class SynonymService(SynonymDesignationMixin, SynonymModelMixin):
         flattened = list(map(str.strip, (list(filter(None, self.flatten_synonyms_text(results))))))
         return flattened
 
-    # TODO: Move this out of utils, it uses a model utils shouldn't use class methods
     '''
     Rules for Regex Transform (from bottom to top):
     1.- Replace with non-space 
