4d703b5b755727835c27cafd9ef3baea632ce190
Lucas <tech@firebrandwebsolutions.com>
2020-02-26 12:37:51 -0800

    Delete entity_type_end_designation methods that were querying data using pandas. They have been replaced with a single get_designations method that uses the Synonym model.

--- api/namex/services/name_request/auto_analyse/name_analysis_director.py
+++ api/namex/services/name_request/auto_analyse/name_analysis_director.py
@@ -302,7 +302,7 @@ class NameAnalysisDirector(GetSynonymsListsMixin, GetWordClassificationListsMixi
 
     def set_entity_type_any_designation(self, entity_any_designation_dict, designation_any_list):
         self._entity_type_any_designation = self.get_synonym_service().get_entity_type_any_designation(
-            self.get_synonym_service().get_all_end_designations,
+            self.get_synonym_service().get_all_end_designations(),
             designation_any_list)
 
     def get_entity_type_any_designation(self):
@@ -310,7 +310,7 @@ class NameAnalysisDirector(GetSynonymsListsMixin, GetWordClassificationListsMixi
 
     def set_entity_type_end_designation(self, entity_end_designation_dict, designation_end_list):
         self._entity_type_end_designation = self.get_synonym_service().get_entity_type_end_designation(
-            self.get_synonym_service().get_all_any_designations,
+            self.get_synonym_service().get_all_any_designations(),
             designation_end_list)
 
     def get_entity_type_end_designation(self):
