e17e870e9a40613c1a3b86ce65b7c785ddd60182
Lucas <tech@firebrandwebsolutions.com>
2020-02-26 14:22:51 -0800

    Minor changes to SynonymService, a couple things broke while re-factoring.

--- api/namex/services/synonyms/synonym.py
+++ api/namex/services/synonyms/synonym.py
@@ -40,7 +40,7 @@ class SynonymService(SynonymDesignationMixin, SynonymModelMixin):
 
         criteria = SynonymQueryCriteria(
             word=word,
-            fields=[self._model.synonyms_text],
+            fields=[model.synonyms_text],
             filters=filters
         )
 
@@ -101,16 +101,14 @@ class SynonymService(SynonymDesignationMixin, SynonymModelMixin):
         filters = []
 
         if entity_type_code is not None:
-            filters += func.lower(model.category).op('~')(r'\y{}[-_]+valid\y'.format(entity_type_code.value.lower()))
+            filters.append(func.lower(model.category).op('~')(r'\y{}[-_]+valid\y'.format(entity_type_code.value.lower())))
 
         if position_code is not None:
-            filters += func.lower(model.category).op('~')(r'\y{}\y'.format('designation[s]?[_-]+' + position_code.value.lower()))
+            filters.append(func.lower(model.category).op('~')(r'\y{}\y'.format('designation[s]?[_-]+' + position_code.value.lower())))
         else:
-            filters += func.lower(model.category).op('~')(r'\y{}\y'.format('designation[s]?[_-]'))
+            filters.append(func.lower(model.category).op('~')(r'\y{}\y'.format('designation[s]?[_-]')))
 
-        filters += [
-            func.lower(model.category).op('~')(r'\y{}\y'.format(lang.lower()))
-        ]
+        filters.append(func.lower(model.category).op('~')(r'\y{}\y'.format(lang.lower())))
 
         results = self.find_word_synonyms(None, filters)
         flattened = self._flatten_synonyms_text(results)
