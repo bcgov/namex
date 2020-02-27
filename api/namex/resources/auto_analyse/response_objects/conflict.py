49389cb21f529f97da8d0a94aa80072b59960e3c
Lucas <tech@firebrandwebsolutions.com>
2020-02-27 12:27:28 -0800

    Clean up kwargs in response_objects.

--- api/namex/resources/auto_analyse/response_objects/conflict.py
+++ api/namex/resources/auto_analyse/response_objects/conflict.py
@@ -3,5 +3,5 @@ from . import Serializable
 
 class Conflict(Serializable):
     def __init__(self, **kwargs):
-        self.name = kwargs['name'] if kwargs.get('name') else ''  # Required! <string>
-        self.date = kwargs['date'] if kwargs.get('date') else None  # <date> | None
+        self.name = kwargs.get('name', '')
+        self.date = kwargs.get('date', None)
