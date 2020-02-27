3231762e4e5ba48343fea0425507085cafdeaf3d
Lucas <tech@firebrandwebsolutions.com>
2020-02-27 13:33:37 -0800

    Merge in new search_conflicts stuff.

--- api/namex/services/synonyms/mixins/model.py
+++ api/namex/services/synonyms/mixins/model.py
@@ -22,35 +22,3 @@ class SynonymModelMixin(SynonymServiceMixin):
 
         # Return {'mountain': ['mount', 'mountain', 'mt', 'mtn']} based on list_d
         return dict_subs
-
-    # TODO: Replace this method... use the models!
-    @classmethod
-    def build_query_distinctive(cls, dist_all_permutations, length):
-        query = "select n.name " + \
-                "from requests r, names n " + \
-                "where r.id = n.nr_id and " + \
-                "r.state_cd IN ('APPROVED','CONDITIONAL') and " + \
-                "r.request_type_cd IN ('PA','CR','CP','FI','SO', 'UL','CUL','CCR','CFI','CCP','CSO','CCC','CC') and " + \
-                "n.state IN ('APPROVED','CONDITION') and " + \
-                "lower(n.name) similar to " + "'"
-        st = ''
-        for s in range(length):
-            st += '%s '
-
-        permutations = "|".join(st % tup for tup in dist_all_permutations)
-        query += "(" + permutations + ")%%" + "'"
-
-        return query
-
-    # TODO: Replace this method... use the models!
-    @classmethod
-    def build_query_descriptive(cls, desc_substitution_list, query):
-        for element in desc_substitution_list:
-            query += " and lower(n.name) similar to "
-            substitutions = ' ?| '.join(map(str, element))
-            query += "'" + "%%( " + substitutions + " ?)%%" + "'"
-
-        return query
-
-
-
