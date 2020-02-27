3d8783e9c3c779a33085fa2087b5eceb9de14f33
Lucas <tech@firebrandwebsolutions.com>
2020-02-27 12:27:28 -0800

    Clean up kwargs in response_objects.

--- api/namex/resources/auto_analyse/response_objects/setup.py
+++ api/namex/resources/auto_analyse/response_objects/setup.py
@@ -4,9 +4,9 @@ from . import Serializable
 class Setup(Serializable):
     def __init__(self, **kwargs):
         # TODO: Type the list of buttons!
-        self.button = kwargs['button'] if kwargs.get('button') else ''
-        self.checkbox = kwargs['checkbox'] if kwargs.get('checkbox') else ''
+        self.button = kwargs.get('button', '')
+        self.checkbox = kwargs.get('checkbox', '')
         # TODO: Can we get Scott to change 'header' to header?
-        self.header = kwargs['header'] if kwargs.get('header') else ''
-        self.line1 = kwargs['line1'] if kwargs.get('line1') else ''
-        self.line2 = kwargs['line2'] if kwargs.get('line2') else ''
+        self.header = kwargs.get('header', '')
+        self.line1 = kwargs.get('line1', '')
+        self.line2 = kwargs.get('line2', '')
