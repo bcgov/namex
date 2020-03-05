4201ffdca19a32df5c50d336a386a23afbefa0a0
Lucas <tech@firebrandwebsolutions.com>
2020-03-04 19:07:25 -0800

    Clean up issues, options and setups for responses.

--- api/namex/resources/auto_analyse/analysis_options.py
+++ api/namex/resources/auto_analyse/analysis_options.py
@@ -19,9 +19,9 @@ class AddDistinctiveSetup(Setup):
 
 add_distinctive_setup = AddDistinctiveSetup(
     type="add_distinctive",
-    header="",
-    line1="Add Distinctive",
-    line2=""
+    header="Add Distinctive",
+    line1="Add some words to the beginning of your name that sets your name apart. For example, an individual's name or initials; a geographic location; a colour; a coined, made-up word; or an acronym.",
+    line2="Example: Change 'Renovations Ltd.' → 'Joe's Renovations Ltd."
 )
 
 
@@ -31,9 +31,9 @@ class AddDescriptiveSetup(Setup):
 
 add_descriptive_setup = AddDistinctiveSetup(
     type="add_descriptive",
-    header="",
-    line1="Add Descriptive",
-    line2=""
+    header="Add Descriptive",
+    line1="Add some additional words that help describe what your business does. For example, the product or service you provide.",
+    line2="Example: Change 'Joe's Ltd.' → 'Joe's Renovations Ltd."
 )
 
 
@@ -43,8 +43,20 @@ class TooManyWordsSetup(Setup):
 
 too_many_words_setup = TooManyWordsSetup(
     type="too_many_words",
-    header="",
-    line1="Too Many Words",
+    header="Too Many Words",
+    line1="Please remove one or more words and try your search again, or you can choose to submit the name above for examination.",
+    line2="Note: Designations at the end of your name such as 'Limited', 'Inc', 'Cooperative' will not be counted."
+)
+
+
+class RemoveSetup(Setup):
+    pass
+
+
+remove_setup = RemoveSetup(
+    type="remove",
+    header="Remove",
+    line1="Please remove or replace the word from your search and try again.",
     line2=""
 )
 
@@ -55,9 +67,33 @@ class RemoveOrReplaceSetup(Setup):
 
 remove_or_replace_setup = RemoveOrReplaceSetup(
     type="remove_or_replace",
-    header="",
-    line1="Remove or Replace",
+    header="Remove or Replace",
+    line1="Please remove or replace the highlighted words and try your search again.",
     line2=""
+)
+
+
+class RemoveOrSubmitSetup(Setup):
+    pass
+
+
+remove_or_submit_setup = RemoveOrReplaceSetup(
+    type="remove_or_submit",
+    header="Remove or Submit",
+    line1="Please remove or replace the highlighted words and try your search again, or you can choose to submit the name above for examination.",
+    line2=""
+)
+
+
+class ResolveConflictSetup(Setup):
+    pass
+
+
+resolve_conflict_setup = ResolveConflictSetup(
+    type="resolve_conflict",
+    header="Resolve Conflict",
+    line1="Add a word to the beginning of the name that sets it apart like a person’s name or initials.",
+    line2="Or remove the word(s) {words} and replace them with different ones."
 )
 
 
@@ -67,10 +103,10 @@ class SendToExaminerSetup(Setup):
 
 send_to_examiner_setup = SendToExaminerSetup(
     type="send_to_examiner",
-    header="",
-    line1="Send to Examiner",
+    header="Send to Examiner",
+    line1="You can choose to submit this name for examination. Please check wait times at the top of the screen.",
     line2="",
-    label=""
+    action="I want my name examined."
 )
 
 
@@ -80,10 +116,10 @@ class ObtainConsentSetup(Setup):
 
 obtain_consent_setup = ObtainConsentSetup(
     type="obtain_consent",
-    header="",
-    line1="Obtain Consent",
+    header="Obtain Consent",
+    line1="This name can be auto-approved, but you will be required to send written consent to the BC Business Registry.",
     line2="",
-    label="I am able to obtain and send written consent."
+    action="I am able to obtain and send written consent."
 )
 
 
@@ -91,12 +127,12 @@ class SelfConsentSetup(Setup):
     pass
 
 
-self_consent_setup = SelfConsentSetup(
-    type="self_consent",
-    header="",
-    line1="Self Consent",
+conflict_self_consent_setup = SelfConsentSetup(
+    type="conflict_self_consent",
+    header="Conflict Self Consent",
+    line1="This name can be auto-approved if you are the registered owner of the conflicting name, but you are required to send written consent to the BC Business Registry.",
     line2="",
-    label="I have authority over the conflicting name."
+    action="I am the registered owner of the conflicting name. I will send written consent."
 )
 
 
@@ -106,10 +142,9 @@ class ReplaceDesignationSetup(Setup):
 
 replace_designation_setup = ReplaceDesignationSetup(
     type="replace_designation",
-    header="",
+    header="Replace Designation",
     line1="Replace Designation",
-    line2="",
-    label=""
+    line2=""
 )
 
 
@@ -119,8 +154,7 @@ class ChangeEntityTypeSetup(Setup):
 
 change_entity_type_setup = ChangeEntityTypeSetup(
     type="change_entity_type",
-    header="",
+    header="Change Entity Type",
     line1="Change Entity Type",
-    line2="",
-    label=""
+    line2=""
 )
