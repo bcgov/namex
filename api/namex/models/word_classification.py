07d8e2236766dfe2bf20d6820bd94db567edcc42
Lucas <tech@firebrandwebsolutions.com>
2020-03-17 16:45:15 -0600

    Get rid of the wrong / outdated find_word_classification methods in word_classification model.

--- api/namex/models/word_classification.py
+++ api/namex/models/word_classification.py
@@ -37,18 +37,8 @@ class WordClassification(db.Model):
                 "approvedBy": self.approved_by, "startDate": self.start_dt,
                 "lastUpdatedBy": self.last_updated_by, "lastUpdatedDate": self.last_updated_dt}
 
-    '''
-    # TODO: Fix this it's not working...
     @classmethod
     def find_word_classification(cls, word):
-        # print(cls.query.filter(func.lower(WordClassification.word) == word.lower()))
-        word_property = WordClassification.word
-        lower_func_1 = func.lower(word_property)
-        lower_func_2 = func.lower(word)
-        return cls.query.filter(lower_func_1 == lower_func_2).all()
-        --- OR ---
-        # TODO: Can we return more than one result?
-        # results = cls.query.filter(func.lower(WordClassification.word) == func.lower(word))
         results = db.session.query(cls.word, cls.classification) \
             .filter(func.lower(cls.word) == func.lower(word)) \
             .filter(cls.end_dt == None) \
@@ -57,19 +47,6 @@ class WordClassification(db.Model):
         print(word)
         print(list(map(lambda x: x.classification, results)))
         return results
-    '''
-
-    # TODO: Do we still need to update this method?
-    '''
-    Note: we convert to lower case as word text in the DB will be in all caps.
-    '''
-    @classmethod
-    def find_word_classification(cls, word):
-        # TODO: Can we return more than one result?
-        results = cls.query.filter(func.lower(WordClassification.word) == func.lower(word))
-        print(word)
-        print(list(map(lambda x: x.classification, results)))
-        return cls.query.filter(func.lower(WordClassification.word) == func.lower(word)).all()
 
     def save_to_db(self):
         db.session.add(self)
