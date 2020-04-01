339c7a62ffbd6c79d7d3bb907642af868f9e2776
Trent <Lorna.Trent@gov.bc.ca>
2020-03-31 20:12:37 -0700

    added save for last NR # and handled whnethere is no NR to begin with

--- api/namex/models/nr_number.py
+++ api/namex/models/nr_number.py
@@ -11,40 +11,33 @@ class NRNumber(db.Model):
     nrNum = db.Column('nr_num', db.String(10), unique=True)
     lastUpdate = db.Column('last_update', db.DateTime(timezone=True), default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    @classmethod
-    def find_last_nr_num(cls):
-        return cls.query(cls.nrNum).filter(cls.source == 'NAMEREQUEST').one_or_none()
 
     @classmethod
-    def get_next_nr_num(cls,last_nr_num):
-        last_nr_header = last_nr_num[0:4]
-        last_number = last_nr_num[4:10]
+    def get_next_nr_num(cls, last_nr):
+        last_nr_header = last_nr[0:4]
+        last_number = last_nr[4:10]
         if(last_number == '999999'):
             #next_nr_header = #next letter in the alphabet starting at a specific letter
             next_number = '000000'
         else:
             next_nr_header = last_nr_header
-
             next_number = str((int(last_number) + 1)).zfill(6)
 
-        next_nrNum = next_nr_header + next_number
+        next_nr_num = next_nr_header + next_number
 
-        return(next_nrNum)
+        return(next_nr_num)
 
-        def as_dict(self):
-            return {
-                'id': self.id,
+    def json(self):
+        return {'id': self.id,
                 'nrNum': self.nrNum
-            }
+               }
 
-        def save_to_db(self):
-            db.session.add(self)
-            db.session.commit()
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
 
-        def delete_from_db(self):
-            pass
+
 
 class NRNumberSchema(ma.ModelSchema):
     class Meta:
         model = NRNumber
-        fields = ('id', 'nrNum')
