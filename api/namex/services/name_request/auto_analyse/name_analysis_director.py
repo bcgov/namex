769acfe0aff6a06a4c45a0c51f6a033ac3947bcd
areyeslo <areyeslo@users.noreply.github.com>
2020-04-15 15:45:07 -0700

    SpecialRestrictedWords come after unclassified and before consents

--- api/namex/services/name_request/auto_analyse/name_analysis_director.py
+++ api/namex/services/name_request/auto_analyse/name_analysis_director.py
@@ -300,8 +300,8 @@ class NameAnalysisDirector(GetSynonymsListsMixin, GetDesignationsListsMixin, Get
                 AnalysisIssueCodes.ADD_DISTINCTIVE_WORD,
                 AnalysisIssueCodes.ADD_DESCRIPTIVE_WORD,
                 AnalysisIssueCodes.CONTAINS_UNCLASSIFIABLE_WORD,
-                AnalysisIssueCodes.NAME_REQUIRES_CONSENT,
                 AnalysisIssueCodes.WORD_SPECIAL_USE,
+                AnalysisIssueCodes.NAME_REQUIRES_CONSENT,
                 AnalysisIssueCodes.CORPORATE_CONFLICT,
                 AnalysisIssueCodes.DESIGNATION_MISMATCH,
                 AnalysisIssueCodes.DESIGNATION_MISPLACED
