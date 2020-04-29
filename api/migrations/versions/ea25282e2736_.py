eab7289a4893c3d6d904ce95ccec65547efa0d7b
LJTrent <lorna.trent@gov.bc.ca>
2020-04-29 10:47:45 -0700

    updates

---
+++ api/migrations/versions/ea25282e2736_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: ea25282e2736
+Revises: 5b30946cd550
+Create Date: 2020-04-29 09:40:52.221336
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ea25282e2736'
+down_revision = '5b30946cd550'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
