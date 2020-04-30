f4fe5d9407bad7f39e0e4845721bc647ea97e5cd
areyeslo <areyeslo@users.noreply.github.com>
2020-04-30 12:06:36 -0700

    All variables related to designations have to go in GetDesignationsListsMixin.

--- api/namex/services/name_processing/mixins/get_synonym_lists.py
+++ api/namex/services/name_processing/mixins/get_synonym_lists.py
@@ -5,15 +5,15 @@ class GetSynonymListsMixin(object):
     _stop_words = []
     # TODO: Arturo _number_words is declared in multiple files
     _number_words = []
-    _designated_end_words = []
-    _designated_any_words = []
-    _designated_all_words = []
+    #_designated_end_words = []
+    #_designated_any_words = []
+    #_designated_all_words = []
 
-    _eng_designated_end_words = []
-    _eng_designated_end_words = []
+    #_eng_designated_end_words = []
+    #_eng_designated_end_words = []
 
-    _fr_designated_any_words = []
-    _fr_designated_any_words = []
+    #_fr_designated_any_words = []
+    #_fr_designated_any_words = []
 
     def get_prefixes(self):
         return self._prefixes
@@ -30,25 +30,25 @@ class GetSynonymListsMixin(object):
     def get_number_words(self):
         return self._number_words
 
-    def get_designated_end_words(self):
-        return self._designated_end_words
-
-    def get_designated_any_words(self):
-        return self._designated_any_words
-
-    def get_designated_all_words(self):
-        return self._designated_all_words
-
-    def get_eng_designated_end_words(self):
-        return self._eng_designated_end_words
-
-    def get_eng_designated_any_words(self):
-        return self._eng_designated_any_words
-
-    def get_fr_designated_end_words(self):
-        return self._fr_designated_end_words
-
-    def get_fr_designated_any_words(self):
-        return self._fr_designated_any_words
+    # def get_designated_end_words(self):
+    #     return self._designated_end_words
+    #
+    # def get_designated_any_words(self):
+    #     return self._designated_any_words
+    #
+    # def get_designated_all_words(self):
+    #     return self._designated_all_words
+    #
+    # def get_eng_designated_end_words(self):
+    #     return self._eng_designated_end_words
+    #
+    # def get_eng_designated_any_words(self):
+    #     return self._eng_designated_any_words
+    #
+    # def get_fr_designated_end_words(self):
+    #     return self._fr_designated_end_words
+    #
+    # def get_fr_designated_any_words(self):
+    #     return self._fr_designated_any_words
 
 
