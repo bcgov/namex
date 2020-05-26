0b37b04bd6fc80ea8fca06ed00aaef694d8d1f16
areyeslo <areyeslo@users.noreply.github.com>
2020-05-26 16:04:46 -0700

    Added comment for designation mismatch to work later.

--- api/tests/python/end_points/auto_analyse/protected_names/test_auto_analyse_invalid_np_bc.py
+++ api/tests/python/end_points/auto_analyse/protected_names/test_auto_analyse_invalid_np_bc.py
@@ -878,7 +878,7 @@ def test_designation_existence_incomplete_designation_request_response(client, j
             assert_issues_count_is_gt(0, payload.get('issues'))
             assert_has_issue_type(AnalysisIssueCodes.TOO_MANY_WORDS, payload.get('issues'))
 
-
+# #TODO: COOP is also a special word. What if coop is typed in CR entity type. Do we show mismatch designation and special word use?
 @pytest.mark.xfail(raises=ValueError)
 def test_designation_mismatch_one_word_request_response(client, jwt, app):
     words_list_classification = [{'word': 'ARMSTRONG', 'classification': 'DIST'},
