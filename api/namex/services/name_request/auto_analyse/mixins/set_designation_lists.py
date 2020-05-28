e50d84c1404e3660069b38312438fbe605e946b7
areyeslo <areyeslo@users.noreply.github.com>
2020-05-28 12:50:35 -0700

    Removed old comments, updated comments.

--- api/namex/services/name_request/auto_analyse/mixins/set_designation_lists.py
+++ api/namex/services/name_request/auto_analyse/mixins/set_designation_lists.py
@@ -8,12 +8,11 @@ from namex.utils.common import parse_dict_of_lists
 class SetDesignationsListsMixin(object):
     def _set_designations(self):
         # Set available designations for entity type selected by user (by default designations related to 'CR' entity type)
-        # _designation_any_list_user and _designation_end_list_user contain the only correct designations
+        # _designation_any_list_correct and_designation_end_list_correct contain only correct designations in English and French
         self._set_designations_by_entity_type_user()
 
-        # Set _designation_any_list and _designation_end_list based on company name typed by user
-        # Set _all_designations (general list) based on company name typed by user
-        # All previous set designations have correct position, but may belong to wrong entity type
+        # Set _designation_any_list and _designation_end_list are only correct designations based on company name typed by user
+        # Set _all_designations contains all found designations in company name typed by user
         self._set_designations_by_input_name()
 
         # Set _misplaced_designation_end_list which contains <end> designations in other part of the name
@@ -25,14 +24,8 @@ class SetDesignationsListsMixin(object):
         # Set _entity_type_end_designation for designations found on company name typed by user
         self._set_entity_type_end_designation()
 
-        # Set _misplaced_designation_all based on company name typed by user
-        # self._set_misplaced_designation_in_input_name()
-
         # Set all designations based on entity type typed by user,'CR' by default
         self._all_designations_user = self._eng_designation_all_list_correct + self._fr_designation_all_list_correct
-
-        # self._all_designations_user_no_periods = remove_periods_designation(self._all_designations_user)
-        # self._all_designations_user_no_periods.sort(key=len, reverse=True)
 
     '''
     Set the corresponding entity type for designations <any> found in name
@@ -55,7 +48,6 @@ class SetDesignationsListsMixin(object):
 
     def _set_entity_type_end_designation(self):
         syn_svc = self.synonym_service
-        # entity_end_designation_dict = self._entity_end_designation_dict
         designation_end_list = self._designation_end_list
 
         all_end_designations = syn_svc.get_all_end_designations().data
@@ -113,16 +105,13 @@ class SetDesignationsListsMixin(object):
     designation_end_list: Retrieves all designations properly placed at the end in the name regardless the entity type
                            <Entity type>-Valid English Designations_end Stop
                            English Designations_end Stop.
-   all_designations: Retrieves misplaced and correctly placed designations.
-     Note: The previous lists contain designations that are in the correct position. For instance, designations with <end> position
-           found anywhere are not counted here, they are counted in _set_designations_incorrect_position_by_input_name.
+    all_designations: Retrieves all designations in company name.
     '''
 
     def _set_designations_by_input_name(self):
         syn_svc = self.synonym_service
         np_svc = self.name_processing_service
 
-        # original_name = self.get_original_name()
         # Get the first section of name when exists slash. For instance, ARMSTRONG PLUMBING LTD./ ARMSTRONG PLUMBING LIMITEE
         # Just take ARMSTRONG PLUMBING LTD. and perform analysis of designations.
         name_first_part = np_svc.name_first_part
@@ -139,7 +128,6 @@ class SetDesignationsListsMixin(object):
     def _set_designations_incorrect_position_by_input_name(self):
         syn_svc = self.synonym_service
         tokenized_name = self.get_original_name_tokenized()
-        #correct_designation_end_list = remove_periods_designation(self._designation_end_list_correct)
         correct_designation_end_list = self._designation_end_list_correct
 
         designation_end_misplaced_list = syn_svc.get_incorrect_designation_end_in_name(tokenized_name=tokenized_name,
