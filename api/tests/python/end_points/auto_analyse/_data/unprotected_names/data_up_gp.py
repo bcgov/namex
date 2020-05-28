48bf40029b5751c2846c8ee8ec7356325aa42d62
Lucas <tech@firebrandwebsolutions.com>
2020-05-27 21:18:56 -0700

    Clean up auto analyse test data files.

--- api/tests/python/end_points/auto_analyse/_data/unprotected_names/data_up_gp.py
+++ api/tests/python/end_points/auto_analyse/_data/unprotected_names/data_up_gp.py
@@ -1,37 +1,5 @@
-from namex.models import User
-
-token_header = {
-    "alg": "RS256",
-    "typ": "JWT",
-    "kid": "flask-jwt-oidc-test-client"
-}
-
-claims = {
-    "iss": "https://sso-dev.pathfinder.gov.bc.ca/auth/realms/sbc",
-    "sub": "43e6a245-0bf7-4ccf-9bd0-e7fb85fd18cc",
-    "aud": "NameX-Dev",
-    "exp": 31531718745,
-    "iat": 1531718745,
-    "jti": "flask-jwt-oidc-test-support",
-    "typ": "Bearer",
-    "username": "test-user",
-    "realm_access": {
-        "roles": [
-            "{}".format(User.EDITOR),
-            "{}".format(User.APPROVER),
-            "viewer",
-            "user"
-        ]
-    }
-}
-
-API_BASE_URI = '/api/v1/'
-ENDPOINT_PATH = API_BASE_URI + 'name-analysis'
-
-
-# @pytest.mark.xfail(raises=ValueError)
-def test_contains_unclassifiable_word_request_response(client, jwt, app):
-    test_params = {
+def data_for_contains_unclassifiable_word_request_test():
+    return {
         'name': 'MOUNTAIN VIEW FOOD BLOGGINS',
         'location': 'BC',
         'entity_type': 'GP',
@@ -39,9 +7,8 @@ def test_contains_unclassifiable_word_request_response(client, jwt, app):
     }
 
 
-# @pytest.mark.xfail(raises=ValueError)
-def test_too_many_words_request_response(client, jwt, app):
-    test_params = {
+def data_for_too_many_words_request_test():
+    return {
         'name': 'MOUNTAIN VIEW FOOD GROWERS',
         'location': 'BC',
         'entity_type': 'GP',
@@ -49,9 +16,8 @@ def test_too_many_words_request_response(client, jwt, app):
     }
 
 
-# @pytest.mark.xfail(raises=ValueError)
-def test_contains_words_to_avoid_request_response(client, jwt, app):
-    test_params = {
+def data_for_contains_words_to_avoid_request_test():
+    return {
         'name': 'MOUNTAIN VIEW FOOD GROWERS',
         'location': 'BC',
         'entity_type': 'GP',
@@ -59,9 +25,8 @@ def test_contains_words_to_avoid_request_response(client, jwt, app):
     }
 
 
-# @pytest.mark.xfail(raises=ValueError)
-def test_designation_mismatch_request_response(client, jwt, app):
-    test_params = {
+def data_for_designation_mismatch_request_test():
+    return {
         'name': 'MOUNTAIN VIEW FOOD GROWERS',
         'location': 'BC',
         'entity_type': 'GP',
@@ -69,9 +34,8 @@ def test_designation_mismatch_request_response(client, jwt, app):
     }
 
 
-# @pytest.mark.xfail(raises=ValueError)
-def test_name_requires_consent_request_response(client, jwt, app):
-    test_params = {
+def data_for_name_requires_consent_request_test():
+    return {
         'name': 'MOUNTAIN VIEW FOOD GROWERS',
         'location': 'BC',
         'entity_type': 'GP',
@@ -80,11 +44,11 @@ def test_name_requires_consent_request_response(client, jwt, app):
 
 
 # TODO: Pytest uses an empty DB so create a CONFLICTING NAME first before / as part of running this test!!!
-# @pytest.mark.xfail(raises=ValueError)
-def test_corporate_name_conflict_request_response(client, jwt, app):
+
+def data_for_corporate_name_conflict_request_test():
     # TODO: Insert 'name': 'MOUNTAIN VIEW FOOD INC.' as the 1st test!
     # TODO: Insert 'name': 'MOUNTAIN VIEW GROWERS INC.' as the 2nd test!
-    test_params = {
+    return {
         'name': 'MOUNTAIN VIEW FOOD GROWERS',
         'location': 'BC',
         'entity_type': 'GP',
