b27f84fec106b2d659d767e52379005e6a5633dd
areyeslo <areyeslo@users.noreply.github.com>
2020-05-28 12:22:48 -0700

    Updated test cases.

---
+++ api/tests/python/end_points/auto_analyse/protected_names/test_end_designation_more_than_once.py
@@ -0,0 +1,94 @@
+import pytest
+import jsonpickle
+
+from urllib.parse import quote_plus
+
+from namex.services.name_request.auto_analyse import AnalysisIssueCodes
+
+from ..common import assert_issues_count_is_gt, assert_has_word_upper, assert_has_designations_upper, \
+    save_words_list_classification, assert_has_issue_type, assert_has_no_issue_type
+from ..common import ENDPOINT_PATH
+from ..common import token_header, claims
+
+
+@pytest.mark.xfail(raises=ValueError)
+def test_end_designation_more_than_once_request_response(client, jwt, app):
+    words_list_classification = [{'word': 'ARMSTRONG', 'classification': 'DIST'},
+                                 {'word': 'ARMSTRONG', 'classification': 'DESC'},
+                                 {'word': 'PLUMBING', 'classification': 'DIST'},
+                                 {'word': 'PLUMBING', 'classification': 'DESC'}]
+    save_words_list_classification(words_list_classification)
+
+    # create JWT & setup header with a Bearer Token using the JWT
+    token = jwt.create_jwt(claims, token_header)
+    headers = {'Authorization': 'Bearer ' + token, 'content-type': 'application/json'}
+
+    test_params = [
+        {
+            'name': 'ARMSTRONG PLUMBING COOP',
+            'name': 'LTD. ARMSTRONG CORPORATION PLUMBING  INC.',
+            'location': 'BC',
+            'entity_type': 'CR',
+            'request_action': 'NEW'
+        },
+        {
+            'name': 'ARMSTRONG LTD. CORPORATION PLUMBING  INC.',
+            'location': 'BC',
+            'entity_type': 'CR',
+            'request_action': 'NEW'
+        },
+        {
+            'name': 'ARMSTRONG LTD. CORPORATION INC. PLUMBING',
+            'location': 'BC',
+            'entity_type': 'CR',
+            'request_action': 'NEW'
+        },
+        {
+            'name': 'LTD. COOP ARMSTRONG L.L.C. CORPORATION PLUMBING CCC INC.',
+            'location': 'BC',
+            'entity_type': 'CR',
+            'request_action': 'NEW'
+        },
+        {
+            'name': 'ARMSTRONG LTD. COOP L.L.C. CORPORATION LLP INC. PLUMBING  ',
+            'location': 'BC',
+            'entity_type': 'CR',
+            'request_action': 'NEW'
+        },
+        {
+            'name': 'ARMSTRONG LTD. COOP L.L.C. CORPORATION LLP PLUMBING INC.',
+            'location': 'BC',
+            'entity_type': 'CR',
+            'request_action': 'NEW'
+        },
+        {
+            'name': 'ARMSTRONG PLUMBING LTD. INC.',
+            'location': 'BC',
+            'entity_type': 'CR',
+            'request_action': 'NEW'
+        },
+        {
+            'name': 'ARMSTRONG PLUMBING LTD. INC. CORPORATION',
+            'location': 'BC',
+            'entity_type': 'CR',
+            'request_action': 'NEW'
+        },
+        {
+            'name': 'ARMSTRONG LIMITED LIABILITY COMPANY PLUMBING COOP LIMITED INC. CORPORATION',
+            'location': 'BC',
+            'entity_type': 'CR',
+            'request_action': 'NEW'
+        },
+
+    ]
+    for entry in test_params:
+        query = '&'.join("{!s}={}".format(k, quote_plus(v)) for (k, v) in entry.items())
+        path = ENDPOINT_PATH + '?' + query
+        print('\n' + 'request: ' + path + '\n')
+        response = client.get(path, headers=headers)
+        payload = jsonpickle.decode(response.data)
+        print("Assert that the payload contains issues")
+        if isinstance(payload.get('issues'), list):
+            assert_issues_count_is_gt(0, payload.get('issues'))
+            assert_has_issue_type(AnalysisIssueCodes.END_DESIGNATION_MORE_THAN_ONCE, payload.get('issues'))
+            assert_has_no_issue_type(AnalysisIssueCodes.DESIGNATION_MISPLACED, payload.get('issues'))
