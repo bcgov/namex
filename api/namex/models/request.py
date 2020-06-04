48c4d778c0ed91b06562daf5aad53adeec866a52
areyeslo <areyeslo@users.noreply.github.com>
2020-06-03 18:42:07 -0700

    Removed comments.

--- api/namex/models/request.py
+++ api/namex/models/request.py
@@ -357,7 +357,7 @@ class Request(db.Model):
             else:
                 substitutions = '|'.join(map(str, descriptive_element))
                 e.filters[0].append(func.lower(Name.name).op('~')(r'^\s*\W*({})\y\W*\s*'.format(substitutions)))
-                # return criteria
+
         if distinctive:
             return criteria
         results = Request.find_by_criteria(criteria)
