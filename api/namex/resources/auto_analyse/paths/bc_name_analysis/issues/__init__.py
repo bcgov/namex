144190382f7b5fe6db9a25054779d44a72fab4f1
areyeslo <areyeslo@users.noreply.github.com>
2020-07-20 14:17:43 -0700

    Added issue code for queue conflict.

--- api/namex/resources/auto_analyse/paths/bc_name_analysis/issues/__init__.py
+++ api/namex/resources/auto_analyse/paths/bc_name_analysis/issues/__init__.py
@@ -5,6 +5,7 @@ from .check_is_valid import BcCheckIsValidIssue as CheckIsValidIssue
 from .contains_unclassifiable_word import BcContainsUnclassifiableWordIssue
 from .contains_words_to_avoid import BcContainsWordsToAvoidIssue
 from .corporate_name_conflict import BcCorporateNameConflictIssue
+from .queue_name_conflict import BcQueueNameConflictIssue
 from .designation_mismatch import BcDesignationMismatchIssue
 from .designation_misplaced import BcDesignationMisplacedIssue
 from .designation_non_existent import BcDesignationNonExistentIssue
