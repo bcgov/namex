e782ae153c326c621c11828f77bbab1b52d768f6
Lucas <tech@firebrandwebsolutions.com>
2020-07-22 16:50:36 -0700

    Move apply_nr_state_change methods out of the main apply_nr_state_change function.

--- api/namex/services/name_request/name_request_state.py
+++ api/namex/services/name_request/name_request_state.py
@@ -1,6 +1,64 @@
 from namex.models import State
 
 
+def to_draft(resource, nr, on_success_cb=None):
+    if nr.stateCd in [State.DRAFT]:
+        resource.nr_state_code = State.DRAFT
+        nr.stateCd = State.DRAFT
+
+        if on_success_cb:
+            nr = on_success_cb(nr, resource)
+        return nr
+
+
+def to_cond_reserved(resource, nr, on_success_cb):
+    if nr.stateCd in [State.DRAFT, State.COND_RESERVE]:
+        resource.nr_state_code = State.COND_RESERVE
+        nr.stateCd = State.COND_RESERVE
+        if on_success_cb:
+            nr = on_success_cb(nr, resource)
+        return nr
+
+
+def to_reserved(resource, nr, on_success_cb):
+    if nr.stateCd in [State.DRAFT, State.RESERVED]:
+        resource.nr_state_code = State.RESERVED
+        nr.stateCd = State.RESERVED
+        if on_success_cb:
+            nr = on_success_cb(nr, resource)
+        return nr
+
+
+def to_conditional(resource, nr, on_success_cb):
+    if nr.stateCd != State.COND_RESERVE:
+        raise Exception('Invalid state transition')
+
+    # Check for payment
+    if nr.payment_token is None:
+        raise Exception('Transition error, payment token is not defined')
+
+    resource.next_state_code = State.CONDITIONAL
+    nr.stateCd = State.CONDITIONAL
+    if on_success_cb:
+        nr = on_success_cb(nr, resource)
+    return nr
+
+
+def to_approved(resource, nr, on_success_cb):
+    if nr.stateCd != State.RESERVED:
+        raise Exception('Invalid state transition')
+
+    # Check for payment
+    if nr.payment_token is None:
+        raise Exception('Transition error, payment token is not defined')
+
+    resource.next_state_code = State.APPROVED
+    nr.stateCd = State.APPROVED
+    if on_success_cb:
+        nr = on_success_cb(nr, resource)
+    return nr
+
+
 def apply_nr_state_change(self, name_request, next_state, on_success=None):
     """
     This is where we handle entity state changes.
@@ -12,58 +70,6 @@ def apply_nr_state_change(self, name_request, next_state, on_success=None):
     :param on_success:
     :return:
     """
-    def to_draft(resource, nr, on_success_cb=None):
-        if nr.stateCd in [State.DRAFT]:
-            resource.nr_state_code = State.DRAFT
-            nr.stateCd = State.DRAFT
-
-            if on_success_cb:
-                nr = on_success_cb(nr, resource)
-            return nr
-
-    def to_cond_reserved(resource, nr, on_success_cb):
-        if nr.stateCd in [State.DRAFT, State.COND_RESERVE]:
-            resource.nr_state_code = State.COND_RESERVE
-            nr.stateCd = State.COND_RESERVE
-            if on_success_cb:
-                nr = on_success_cb(nr, resource)
-            return nr
-
-    def to_reserved(resource, nr, on_success_cb):
-        if nr.stateCd in [State.DRAFT, State.RESERVED]:
-            resource.nr_state_code = State.RESERVED
-            nr.stateCd = State.RESERVED
-            if on_success_cb:
-                nr = on_success_cb(nr, resource)
-            return nr
-
-    def to_conditional(resource, nr, on_success_cb):
-        if nr.stateCd != State.COND_RESERVE:
-            raise Exception('Invalid state transition')
-
-        # Check for payment
-        if nr.payment_token is None:
-            raise Exception('Transition error, payment token is not defined')
-
-        resource.next_state_code = State.CONDITIONAL
-        nr.stateCd = State.CONDITIONAL
-        if on_success_cb:
-            nr = on_success_cb(nr, resource)
-        return nr
-
-    def to_approved(resource, nr, on_success_cb):
-        if nr.stateCd != State.RESERVED:
-            raise Exception('Invalid state transition')
-
-        # Check for payment
-        if nr.payment_token is None:
-            raise Exception('Transition error, payment token is not defined')
-
-        resource.next_state_code = State.APPROVED
-        nr.stateCd = State.APPROVED
-        if on_success_cb:
-            nr = on_success_cb(nr, resource)
-        return nr
 
     return {
         State.DRAFT: to_draft,
