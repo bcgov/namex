0eecd256b4b385509eb6aa6bb937f9f0b5fe4b4c
areyeslo <areyeslo@users.noreply.github.com>
2020-08-17 09:46:05 -0700

    Check not null for proceding with the operation.

--- api/namex/utils/service_utils.py
+++ api/namex/utils/service_utils.py
@@ -67,6 +67,6 @@ def get_entity_type_description(entity_type_str):
 
 
 def get_waiting_time(examination_time_secs, queue_requests):
-    waiting_time = round(examination_time_secs * queue_requests / 86400)
+    waiting_time = 0.0 if (examination_time_secs is None or queue_requests is None) else round(examination_time_secs * queue_requests / 86400)
 
     return waiting_time
