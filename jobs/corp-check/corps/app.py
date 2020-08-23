4815565579b7728d5edae20372985f12bdbee885
LJTrent <lorna.trent@gov.bc.ca>
2020-08-22 20:00:16 -0700

    matched all active corps for correct consumed date

--- jobs/corp-check/corps/app.py
+++ jobs/corp-check/corps/app.py
@@ -2,11 +2,11 @@ from flask import Flask, g, current_app
 from config import Config
 
 from namex import db
-from namex.services import EventRecorder
 from namex.services.nro import NROServices
 from namex.services.nro.utils import ora_row_to_dict
-from namex.models import Request, Event, State
 from corps.utils.logging import setup_logging
+
+
 
 
 
@@ -44,18 +44,20 @@ def job_result_set(ora_con, max_rows):
 
     result_set = ora_cursor.execute("""
     SELECT id, name, start_date from namex.solr_dataimport_conflicts_vw@colin_readonly 
-    WHERE TRUNC(start_date)  <  to_date('20190510','YYYYMMDD')
-    AND ROWNUM <= :max_rows ORDER BY id
+    WHERE TRUNC(start_date)  <  to_date('20190510','YYYYMMDD') and id > 'S0033034'
+     ORDER BY id
     """
-                                    , max_rows=max_rows
+
                                     )
+
+
 
     col_names = [row[0] for row in ora_cursor.description]
 
     return result_set, col_names
 
 def find_corp_count_in_namex(id, corp_name):
-    sql = "select count(*) as name_count" \
+    sql = "select count(*) as name_count " \
           "from names n " \
           "where n.corp_num = '{id}' and n.name = '{corp_name}' and n.state in ('APPROVED','CONDITION') ".format(id=id, corp_name=corp_name)
 
@@ -82,8 +84,8 @@ def update_consumption_date(name_id,start_date):
 
     #need to deal with utc consumption_date check extractor
 
-    update_sql = "update names" \
-          "set consumption_date =  {start_date}" \
+    update_sql = "update names " \
+          "set consumption_date =  '{start_date}' " \
           "where id =  {name_id}".format(name_id=name_id, start_date=start_date)
 
     print(update_sql)
@@ -91,10 +93,10 @@ def update_consumption_date(name_id,start_date):
     return results
 
 
-def insert_corps_list(id,nr_id, name_id):
-    insert_sql = "insert into corps_list" \
-          "(corp_num, nr_id, name_id )"\
-          "values('{id}', {nr_id}, {name_id})".format(id=id, nr_id=nr_id, name_id=name_id)
+def insert_missing_corps_list(corp_num,corp_name):
+    insert_sql = "insert into missing_corps" \
+          "(corp_num, corp_name )"\
+          "values('{corp_num}', '{corp_name}')".format(corp_num=corp_num, corp_name=corp_name)
 
     print(insert_sql)
 
@@ -102,11 +104,19 @@ def insert_corps_list(id,nr_id, name_id):
     return results
 
 
+def insert_active_corps_list(corp_num, corp_name, nr_id, name_id):
+    insert_sql = "insert into active_corps" \
+          "(corp_num, corp_name, nr_id, name_id )"\
+          "values('{corp_num}', '{corp_name}', {nr_id}, {name_id})".format(corp_num=corp_num, corp_name=corp_name, nr_id=nr_id, name_id=name_id)
 
-def job(app, namex_db, nro_connection, max_rows=100):
+    print(insert_sql)
+
+    results = db.session.execute(insert_sql)
+    return results
+
+def job(app, db, nro_connection, max_rows=100):
 
     row_count = 0
-    datafix_status = None
 
     try:
         ora_con = nro_connection
@@ -115,27 +125,42 @@ def job(app, namex_db, nro_connection, max_rows=100):
         for r in result:
 
             row_count += 1
+            if row_count > max_rows:
+                return row_count
+
             row = ora_row_to_dict(col_names, r)
 
             corp_num = row['id']
-            corp_name = row['name']
+            corp_name =row['name']
+
+
+            corp_name=corp_name.replace('\'', "''")
             start_date = row['start_date']
 
             name_count = find_corp_count_in_namex(corp_num, corp_name)
             if name_count == 0:
-                insert_corps_list(corp_num)
+                #these are the ones that need to be datafixed in namesp to geta valid consumed NR into Namex
+                insert_missing_corps_list(corp_num,corp_name)
             else:
 
                 name_results = find_corp_in_namex(corp_num, corp_name)
                 for name in name_results:
+                    if name.consumption_date:
+                        test_date = name.consumption_date
+                        test_date = test_date.date()
+                    else:
+                        test_date = None
 
-                    if name.consumption_date != start_date:
-                        update_consumption_date(name.id)
+                    test_start_date = start_date.date()
 
-                        #only the ones that needed to be fixed.
-                        insert_corps_list(corp_num, name.nr_id,name.id )
+                    if test_date != test_start_date:
+                        update_consumption_date(name.id,start_date)
 
+                    #these are the active corps in namex and will be used to find the cnsumed names that shoudl be set to historical
+                    #any names that dont match to this list shoudl be set to historical at the request level.
+                    insert_active_corps_list(corp_num, corp_name, name.nr_id,name.id )
 
+            db.session.commit()
         return row_count
 
     except Exception as err:
