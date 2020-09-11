bf06618a0fb1ae0187c96785bff52521aaa954af
Lorna Trent <35615843+LJTrent@users.noreply.github.com>
2020-09-10 18:45:44 -0700

    Extractor fixes (#804)
    * needed a default for the extractor
    
    * not required

--- api/namex/resources/name_requests/configuration.py
+++ api/namex/resources/name_requests/configuration.py
@@ -2,5 +2,5 @@ import os
 
 SOLR_CORE = 'possible.conflicts'
 
-SOLR_URL = os.getenv('SOLR_BASE_URL')
+SOLR_URL = os.getenv('SOLR_BASE_URL', 'http://localhost:8983')
 SOLR_API_URL = SOLR_URL + '/solr/'
