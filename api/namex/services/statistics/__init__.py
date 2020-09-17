591cdf56fa26b93e43e729c29b818d376f204a60
areyeslo <areyeslo@users.noreply.github.com>
2020-09-17 12:10:10 -0700

    Fix query for waiting time and return hours for priority wait time and days for regular wait time.

--- api/namex/services/statistics/__init__.py
+++ api/namex/services/statistics/__init__.py
@@ -1,1 +1,9 @@
+from namex.constants import AbstractEnum
+
 response_keys = ['auto_approved_count', 'priority_wait_time', 'regular_wait_time']
+
+
+class UnitTime(AbstractEnum):
+    DAY = 'days'
+    HR = 'hours'
+    MIN = 'minutes'
