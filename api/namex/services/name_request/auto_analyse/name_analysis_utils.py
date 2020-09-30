420b1cdd34803cf0df6ed69cf400cdf3f674476c
areyeslo <areyeslo@users.noreply.github.com>
2020-09-29 21:50:18 -0700

    Remove double letters from list of distinctive.4

--- api/namex/services/name_request/auto_analyse/name_analysis_utils.py
+++ api/namex/services/name_request/auto_analyse/name_analysis_utils.py
@@ -237,6 +237,10 @@ def get_classification(service, stand_alone_words, syn_svc, match, wc_svc, token
     service._list_dist_words = remove_misplaced_distinctive(service.get_list_dist(), service.get_list_desc(),
                                                             service.name_tokens_search_conflict)
 
+    service._list_dist_words, updated_name_tokens = remove_double_letters_list_dist_words(service.get_list_dist(),
+                                                                                          service.name_tokens_search_conflict)
+    service.set_name_tokens_search_conflict(updated_name_tokens)
+
     service._list_desc_words = remove_descriptive_same_category(dict_desc)
 
     service.set_name_tokens_search_conflict(update_token_list(service.get_list_dist() + service.get_list_desc(),
@@ -339,9 +343,11 @@ def remove_descriptive_same_category(dict_desc):
     return desc_list
 
 
-def remove_doble_letters(list_dist):
-    list_dist_final=[]
+def remove_double_letters_list_dist_words(list_dist, name_tokens):
+    list_dist_final = []
     for item in list_dist:
-        list_dist_final.append(remove_double_letters(item))
+        not_double_letters_item = remove_double_letters(item)
+        list_dist_final.append(not_double_letters_item)
+        name_tokens = list(map(lambda x: str.replace(x, item, not_double_letters_item), name_tokens))
 
-    return list_dist_final
+    return list_dist_final, name_tokens
