9fe5224ebffac2b684085ecdd191e0e19ff79b66
kialj876 <kialj876@gmail.com>
2020-10-16 13:28:12 -0700

    sentry intitialization for namex-api (#820)
    * sentry intitialization and config cleanup
    
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>
    
    * removed flake8 setup + rebase
    
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- api/namex/__init__.py
+++ api/namex/__init__.py
@@ -1,37 +1,44 @@
 # -*- coding: utf-8 -*-
-"""NAMEX API
+"""NAMEX API.
 
 This module is the API for the Names Examination system
 
 TODO: Fill in a larger description once the API is defined for V1
 """
-import config
-from namex.utils.logging import setup_logging
-setup_logging() ## important to do this first
-
 import os
 
-from .VERSION import __version__
+import sentry_sdk  # noqa: I001; pylint: disable=ungrouped-imports; conflicts with Flake8
 from flask import Flask
-from namex.utils.run_version import get_run_version
 from flask_jwt_oidc import JwtManager
+from sentry_sdk.integrations.flask import FlaskIntegration  # noqa: I001
 
-jwt = JwtManager()
-
-from namex.services.nro import NROServices
-nro = NROServices()
-
+import config
+from namex import models
 from namex.models import db, ma
 from namex.resources import api
-from namex import models
+from namex.services.nro import NROServices
+from namex.utils.logging import setup_logging
+from namex.utils.run_version import get_run_version
+# noqa: I003; dont know what flake8 wants here
+from .VERSION import __version__  # noqa: F401; imported from here
 
+setup_logging()  # important to do this first
+jwt = JwtManager()
+nro = NROServices()
 run_version = get_run_version()
 
 
 def create_app(run_mode=os.getenv('FLASK_ENV', 'production')):
-
+    """Create app."""
     app = Flask(__name__)
     app.config.from_object(config.CONFIGURATION[run_mode])
+
+    # Configure Sentry
+    if app.config.get('SENTRY_DSN', None):
+        sentry_sdk.init(
+            dsn=app.config.get('SENTRY_DSN'),
+            integrations=[FlaskIntegration()]
+        )
 
     db.init_app(app)
     ma.init_app(app)
@@ -44,7 +51,7 @@ def create_app(run_mode=os.getenv('FLASK_ENV', 'production')):
     @app.after_request
     def add_version(response):
         os.getenv('OPENSHIFT_BUILD_COMMIT', '')
-        response.headers["API"] = 'NameX/{ver}'.format(ver=run_version)
+        response.headers['API'] = 'NameX/{ver}'.format(ver=run_version)
         return response
 
     register_shellcontext(app)
@@ -53,6 +60,7 @@ def create_app(run_mode=os.getenv('FLASK_ENV', 'production')):
 
 
 def setup_jwt_manager(app, jwt):
+    """Initialize jwt config."""
     def get_roles(a_dict):
         return a_dict['realm_access']['roles']
     app.config['JWT_ROLE_CALLBACK'] = get_roles
