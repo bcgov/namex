c04705fab98994a4d3cf6732b8d2d0683cea5b8f
kialj876 <kialj876@gmail.com>
2020-10-19 16:46:00 -0700

    accuracy enhancement and bug fix (#883)
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- jobs/auto-analyser-uat/run_auto_analyser_uat.py
+++ jobs/auto-analyser-uat/run_auto_analyser_uat.py
@@ -86,17 +86,13 @@ def get_names_from_namex(uat_job: UatJobResult, app: Flask, excl_names: List, pr
                 """
                  and names.state='REJECTED'
                  and requests.state_cd in ('APPROVED', 'CONDITIONAL', 'REJECTED')
+                 order by requests.submitted_date desc nulls last
                 """
             )
         else:  # uat_job.uat_type == uat_accuracy
-            sql += "and requests.state_cd in ('DRAFT')"
+            sql += "and requests.state_cd in ('DRAFT') order by requests.submitted_date asc nulls last"
 
-    sql += (
-        f"""
-        order by requests.submitted_date desc nulls last
-        limit {app.config['MAX_ROWS']}
-        """
-    )
+    sql += (f"limit {app.config['MAX_ROWS']}")
     sql = sql.replace('[', '(').replace(']', ')').replace('"', "'")
     new_names = db.get_engine(app, 'namex').execute(text(sql))
     return new_names.fetchall()
