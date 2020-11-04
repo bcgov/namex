c783bab40dc540df06718a4dbfcfc1b7f4a5132b
kialj876 <kialj876@gmail.com>
2020-11-03 16:00:49 -0800

    notebook update, uat updater bug fix (#905)
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- jobs/auto-analyser-uat/run_auto_analyser_uat.py
+++ jobs/auto-analyser-uat/run_auto_analyser_uat.py
@@ -93,7 +93,8 @@ def get_names_from_namex(uat_job: UatJobResult, app: Flask, excl_names: List, pr
             sql += " and requests.state_cd in ('DRAFT') order by requests.submitted_date asc nulls last"
 
     sql += (f" limit {app.config['MAX_ROWS']}")
-    sql = sql.replace('[', '(').replace(']', ')').replace(', "', ", '").replace('",', "',")
+    sql = sql.replace('[', '(').replace(']', ')').replace(', "', ", '").replace('",', "',").replace('("', "('")\
+        .replace('")', "')")
     new_names = db.get_engine(app, 'namex').execute(text(sql))
     return new_names.fetchall()
 
