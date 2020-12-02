6e69d4b98ee12fb4b5a4ee052b25caa26bed6c1c
areyeslo <areyeslo@users.noreply.github.com>
2020-12-02 10:03:33 -0800

    Adding optional s and word boundary.

--- api/namex/models/request.py
+++ api/namex/models/request.py
@@ -104,8 +104,8 @@ class Request(db.Model):
 
     # Check-In / Check-Out (for INPROGRESS)
     # A UUID granted to the user that checks out the Name Request
-    checkedOutBy = db.Column('checked_out_by', db.String(64),index=True)
-    checkedOutDt = db.Column('checked_out_dt', db.DateTime(timezone=True),index=True)
+    checkedOutBy = db.Column('checked_out_by', db.String(64), index=True)
+    checkedOutDt = db.Column('checked_out_dt', db.DateTime(timezone=True), index=True)
 
     # MRAS fields
     homeJurisNum = db.Column('home_juris_num', db.String(40))
@@ -463,7 +463,8 @@ class Request(db.Model):
             elif word in list_desc:
                 name.extend(Request.set_special_characters_descriptive([word]))
             else:
-                raise Exception('Invalid classification for the word {0}. Cannot be included in exact match query.'.format(word))
+                raise Exception(
+                    'Invalid classification for the word {0}. Cannot be included in exact match query.'.format(word))
 
         criteria = cls.get_designations_in_name(criteria, name, any_designation_list, end_designation_list, stop_words)
 
@@ -496,9 +497,9 @@ class Request(db.Model):
         special_characters_dist = Request.set_special_characters_distinctive(dist)
         substitutions = '|'.join(map(str, special_characters_dist))
         if not check_name_is_well_formed:
-            dist_criteria = r'(no.?)*\s*\d*\s*\W*({0})?\W*\y({1}s?)\W*\s*\y'.format(stop_words, substitutions)
+            dist_criteria = r'(no.?)*\s*\d*\s*\W*({0})?\W*\y({1})\W*\s*\y'.format(stop_words, substitutions)
         else:
-            dist_criteria = r'\s*\W*({0})?\W*\y*({1}s?)\W*\s*\y'.format(stop_words, substitutions)
+            dist_criteria = r'\s*\W*({0})?\W*\y*({1})\W*\s*\y'.format(stop_words, substitutions)
 
         return dist_criteria
 
@@ -506,7 +507,7 @@ class Request(db.Model):
     def get_descriptive_query(cls, desc, criteria, name_criteria):
         special_characters_descriptive = Request.set_special_characters_descriptive(desc)
         for e in criteria:
-            substitutions = ' ?| '.join(map(str, special_characters_descriptive)) + ' ?'
+            substitutions = r's\W* ?| '.join(map(str, special_characters_descriptive)) + 's\W* ?'
             name_criteria += r'.*({})\y'.format(substitutions)
             e.filters.insert(len(e.filters), [func.lower(Name.name).op('~')(name_criteria)])
 
@@ -555,7 +556,7 @@ class Request(db.Model):
         list_special_characters = []
         for element in list_d:
             list_special_characters.append(
-                r'\W*'.join(element[i:i + 1] + element[i:i + 1] + '?' for i in range(0, len(element), 1)))
+                r'\W*'.join(element[i:i + 1] + element[i:i + 1] + '?' for i in range(0, len(element), 1))+ r'\W*s?')
 
         return list_special_characters
 
@@ -563,7 +564,7 @@ class Request(db.Model):
     def set_special_characters_descriptive(cls, list_d):
         list_special_characters = []
         for element in list_d:
-            list_special_characters.append(r'\W*'.join(element[i:i + 1] for i in range(0, len(element), 1)))
+            list_special_characters.append(r'\W*'.join(element[i:i + 1] for i in range(0, len(element), 1)) + r'\W*s?')
 
         return list_special_characters
 
