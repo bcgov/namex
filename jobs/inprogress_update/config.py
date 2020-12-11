e4f6ccac3721d069f86a55c39129cdd711cb896c
Kial Jinnah <kialj876@gmail.com>
2020-12-11 09:02:28 -0800

    inprogress-updater update
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- jobs/inprogress_update/config.py
+++ jobs/inprogress_update/config.py
@@ -1,14 +1,19 @@
+"""Config setup for inprogress updater job."""
 import os
-from dotenv import load_dotenv, find_dotenv
 
-#this will load all the envars from a .env file
+from dotenv import find_dotenv, load_dotenv
+
+
+# this will load all the envars from a .env file
 load_dotenv(find_dotenv())
 
 
 class BaseConfig(object):
+    """Base config."""
+
     PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-    MAX_ROW_LIMIT = os.getenv('MAX_ROWS','100')
+    MAX_ROW_LIMIT = os.getenv('MAX_ROWS', '100')
     MIN_DELAY_SECONDS = os.getenv('MIN_DELAY_SECONDS', '600')
     SECRET_KEY = 'a secret'
 
@@ -22,10 +27,10 @@ class BaseConfig(object):
 
     # POSTGRESQL
     DB_USER = os.getenv('DATABASE_USERNAME', '')
-    DB_PASSWORD = os.getenv('DATABASE_PASSWORD','')
-    DB_NAME = os.getenv('DATABASE_NAME','')
-    DB_HOST = os.getenv('DATABASE_HOST','')
-    DB_PORT = os.getenv('DATABASE_PORT','5432')
+    DB_PASSWORD = os.getenv('DATABASE_PASSWORD', '')
+    DB_NAME = os.getenv('DATABASE_NAME', '')
+    DB_HOST = os.getenv('DATABASE_HOST', '')
+    DB_PORT = os.getenv('DATABASE_PORT', '5432')
     SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{password}@{host}:{port}/{name}'.format(
         user=DB_USER,
         password=DB_PASSWORD,
@@ -34,22 +39,32 @@ class BaseConfig(object):
         name=DB_NAME,
     )
 
+    NRO_USER = os.getenv('NRO_USER', '')
+    NRO_PASSWORD = os.getenv('NRO_PASSWORD', '')
+    NRO_DB_NAME = os.getenv('NRO_DB_NAME', '')
+    NRO_HOST = os.getenv('NRO_HOST', '')
+    NRO_PORT = int(os.getenv('NRO_PORT', '1521'))
+
 
 class Config(BaseConfig):
+    """Normal config."""
+
     DEBUG = False
     TESTING = False
 
 
 class TestConfig(BaseConfig):
+    """Test config."""
+
     DEBUG = True
     TESTING = True
 
     # POSTGRESQL
     DB_USER = os.getenv('DATABASE_USERNAME', '')
-    DB_PASSWORD = os.getenv('DATABASE_PASSWORD','')
+    DB_PASSWORD = os.getenv('DATABASE_PASSWORD', '')
     DB_NAME = os.getenv('DATABASE_NAME_TEST', '')
-    DB_HOST = os.getenv('DATABASE_HOST','')
-    DB_PORT = os.getenv('DATABASE_PORT','5432')
+    DB_HOST = os.getenv('DATABASE_HOST', '')
+    DB_PORT = os.getenv('DATABASE_PORT', '5432')
     SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{password}@{host}:{port}/{name}'.format(
          user=DB_USER,
          password=DB_PASSWORD,
--- jobs/inprogress_update/inprogress_update.py
+++ jobs/inprogress_update/inprogress_update.py
@@ -1,21 +1,21 @@
-import sys
+"""Script used to regularly update INPROGRESS NRs."""
+from datetime import datetime
 
-from sqlalchemy import text
-from datetime import datetime, timedelta
-
-from flask import Flask, g, current_app
-
-from namex import db
-from namex.models import Request, State, User, Event
+from flask import Flask, current_app
+from namex import db, nro
+from namex.models import Event, Request, State, User
 from namex.services import EventRecorder
 from namex.utils.logging import setup_logging
+from sqlalchemy import text
 
 from config import Config
 
-setup_logging() ## important to do this first
+
+setup_logging()  # important to do this first
 
 
 def create_app(config=Config):
+    """Create instance of app."""
     app = Flask(__name__)
     app.config.from_object(config)
     db.init_app(app)
@@ -24,48 +24,75 @@ def create_app(config=Config):
 
     return app
 
+
 def get_ops_params():
-    try:
-        delay = int(current_app.config.get('MIN_DELAY_SECONDS', 1800))
-    except:
-        delay = 1800
-    try:
-        max_rows = int(current_app.config.get('MAX_ROW_LIMIT', 100))
-    except:
-        max_rows = 100
+    """Get params for job."""
+    client_delay = int(current_app.config.get('MIN_CLIENT_DELAY_SECONDS', 900))
+    examine_delay = int(current_app.config.get('MIN_EXAMINE_DELAY_SECONDS', 1800))
+    max_rows = int(current_app.config.get('MAX_ROW_LIMIT', 100))
 
-    return delay, max_rows
+    return client_delay, examine_delay, max_rows
 
 
-def job(user: User, max_rows: int, delay: int) -> (int, bool):
+def inprogress_update(user: User, max_rows: int, client_delay: int, examine_delay: int) -> (int, bool):
+    """Update inprogress nrs."""
     row_count = 0
 
     try:
-
-        reqs = db.session.query(Request). \
+        # for nrs edited by clients
+        client_edit_reqs = db.session.query(Request). \
             filter(Request.stateCd == State.INPROGRESS). \
-            filter(Request.lastUpdate <= text('(now() at time zone \'utc\') - INTERVAL \'{delay} SECONDS\''.format(delay=delay))). \
+            filter(Request.lastUpdate <= text(f'(now() at time zone \'utc\') - INTERVAL \'{client_delay} SECONDS\'')). \
+            filter(Request.checkedOutBy != None). \
+            order_by(Request.lastUpdate.asc()). \
+            limit(max_rows). \
+            with_for_update().all()
+        for r in client_edit_reqs:
+            row_count += 1
+
+            current_app.logger.debug(f'processing: {r.nrNum}')
+            current_app.logger.debug(f'nr {r.nrNum}, state: {r.stateCd} last_update:{r.lastUpdate}')
+
+            r.stateCd = State.DRAFT
+            r.checkedOutBy = None
+            db.session.add(r)
+            errors = nro.checkin_checkout_nr(r, 'UNLOCK')
+            if errors:
+                raise Exception('Failed to update nro.')
+            # commit here to keep this entry in sync with NRO (in case errors happen later)
+            db.session.commit()
+
+            EventRecorder.record(user, Event.SET_TO_DRAFT, r, {}, save_to_session=True)
+
+        # for nrs edited by examiners
+        examine_reqs = db.session.query(Request). \
+            filter(Request.stateCd == State.INPROGRESS). \
+            filter(Request.lastUpdate <= text(f"(now() at time zone 'utc') - INTERVAL '{examine_delay} SECONDS'")). \
+            filter(Request.checkedOutBy == None). \
             order_by(Request.lastUpdate.asc()). \
             limit(max_rows). \
             with_for_update().all()
 
-        for r in reqs:
+        for r in examine_reqs:
             row_count += 1
 
-            current_app.logger.debug('processing: {}'.format(r.nrNum))
+            current_app.logger.debug(f'processing: {r.nrNum}')
+            current_app.logger.debug(f'nr {r.nrNum}, state: {r.stateCd} last_update:{r.lastUpdate}')
 
-            print ('nr {}, state: {} last_update:{}'.format(r.nrNum, r.stateCd, r.lastUpdate))
-
-            # if this NR was previously in DRAFT, reset it to that state (ie: the user walked away from an open edit window)
+            # if this NR was previously in DRAFT, reset it to that state
+            # (ie: the user walked away from an open edit window)
+            event = None
             if r.previousStateCd == State.DRAFT:
                 r.stateCd = State.DRAFT
                 r.previousStateCd = None
+                event = Event.SET_TO_DRAFT
             # otherwise put it on hold
             else:
                 r.stateCd = State.HOLD
+                event = Event.MARKED_ON_HOLD
 
             db.session.add(r)
-            EventRecorder.record(user, Event.MARKED_ON_HOLD, r, {}, save_to_session=True)
+            EventRecorder.record(user, event, r, {}, save_to_session=True)
 
         db.session.commit()
         return row_count, True
@@ -76,27 +103,25 @@ def job(user: User, max_rows: int, delay: int) -> (int, bool):
         return -1, False
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     NRO_SERVICE_ACCOUNT = 'NRO_SERVICE_ACCOUNT'
     app = create_app(Config)
-    delay, max_rows = get_ops_params()
+    client_delay, examine_delay, max_rows = get_ops_params()
 
     start_time = datetime.utcnow()
 
     user = User.find_by_username(current_app.config[NRO_SERVICE_ACCOUNT])
     if not user:
-        print("job: unable to load {0}, current config value={1}"
-              .format(NRO_SERVICE_ACCOUNT, current_app.config[NRO_SERVICE_ACCOUNT]))
-        exit(0)
+        current_app.logger.error(f'Setup error: unable to load {NRO_SERVICE_ACCOUNT}.')
+        exit(1)
 
-    row_count, success = job(user, max_rows, delay)
+    row_count, success = inprogress_update(user, max_rows, client_delay, examine_delay)
 
     app.do_teardown_appcontext()
     end_time = datetime.utcnow()
 
     if success:
-        print("job - requests processed: {0} completed in:{1}".format(row_count, end_time-start_time))
-        exit(0)
-
-    print('Failed to move timed out INPROGRESS NRs to HOLD: ', err, err.with_traceback(None), file=sys.stderr)
-    exit(1)
+        current_app.logger.debug(f'Requests processed: {row_count} completed in:{end_time-start_time}')
+    else:
+        current_app.logger.error('Failed to move timed out INPROGRESS NRs')
+        exit(1)
