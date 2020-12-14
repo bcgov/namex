8947dd58fd9c186ebbfdfb28b9f78be344aa6c02
Kial Jinnah <kialj876@gmail.com>
2020-12-14 10:44:39 -0800

    oracle connection fixes
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- jobs/inprogress_update/inprogress_update.py
+++ jobs/inprogress_update/inprogress_update.py
@@ -19,6 +19,7 @@ def create_app(config=Config):
     app = Flask(__name__)
     app.config.from_object(config)
     db.init_app(app)
+    nro.init_app(app)
     app.app_context().push()
     current_app.logger.debug('created the Flask App and pushed the App Context')
 
