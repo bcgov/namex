31cfa4b13658e88a81b5af6a3a3fd76ab70dc89b
Steph Rabbani <jordiwes@gmail.com>
2021-02-23 16:19:54 -0800

    6335 send queue position and time estimate - one more fix (#1037)
    * initial send days estimates back from request
    * in case get oldest queue date returns nothing

--- api/namex/resources/name_requests/name_request.py
+++ api/namex/resources/name_requests/name_request.py
@@ -52,12 +52,16 @@ class NameRequestResource(BaseNameRequestResource):
 
             # If draft, get the wait time and oldest queued request
             if nr_model.stateCd == 'DRAFT':
-                oldest_draft = Request.get_oldest_draft().submittedDate
-                if oldest_draft is None:
-                    oldest_draft = datetime.today()
-                response_data['oldest_draft'] = oldest_draft.isoformat()
-                response_data['waiting_time'] = Request.get_waiting_time_regular_queue(unit=UnitTime.DAY.value).examinationTime
 
+                oldest_draft = Request.get_oldest_draft()
+                if oldest_draft is None:
+                    oldest_draft_date = datetime.now().astimezone()
+                else:
+                    oldest_draft_date = oldest_draft.submittedDate
+
+                delta = datetime.now().astimezone() - oldest_draft_date
+                response_data['oldest_draft'] = oldest_draft_date.isoformat()
+                response_data['waiting_time'] = delta.days
 
             # Add the list of valid Name Request actions for the given state to the response
             response_data['actions'] = get_nr_state_actions(nr_model.stateCd, nr_model)
