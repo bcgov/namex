d950245e040741f35df02f27e59079fc72d3dbd4
Omid Zamani <ozamani9@gmail.com>
2021-11-30 15:06:54 -0800

    fix lint and flake8 errors

--- services/solr-names-updater/src/solr_names_updater/worker.py
+++ services/solr-names-updater/src/solr_names_updater/worker.py
@@ -69,8 +69,8 @@ def is_processable(msg: dict):
     """Determine if message is processable using message type of msg."""
     with FLASK_APP.app_context():
         if msg and is_names_event_msg_type(msg) \
-           and (nr_num := msg.get('data',{}) \
-                             .get('request', {}) \
+           and (nr_num := msg.get('data', {})
+                             .get('request', {})
                              .get('nrNum', None)) \
            and (nr := RequestDAO.find_by_nr(nr_num)) \
            and nr.entity_type_cd not in ('FR', 'GP'):
