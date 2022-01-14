048e33bce21d7082cc0672b5a7ca86527aa5ed55
Vysakh Menon <vysakh.menon@aot-technologies.com>
2022-01-13 22:05:50 -0800

    9499 get legal_type based on nr_type and entity_type_cd (#1207)

--- api/namex/models/request.py
+++ api/namex/models/request.py
@@ -212,7 +212,7 @@ class Request(db.Model):
             'notifiedBeforeExpiry': self.notifiedBeforeExpiry,
             'notifiedExpiry': self.notifiedExpiry
         }
-        if nr_actions := nr_filing_actions.get_actions(self.requestTypeCd):
+        if nr_actions := nr_filing_actions.get_actions(self.requestTypeCd, self.entity_type_cd):
             nr_json['legalType'] = nr_actions.get('legalType')
             nr_json['target'] = nr_actions.get('target')
             nr_json['actions'] = nr_actions.get('actions')
