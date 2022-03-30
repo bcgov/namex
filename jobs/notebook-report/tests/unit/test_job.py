06c59e396315a953398e20f987e17d96e4723a34
Chen <Steven.Chen@gov.bc.ca>
2022-03-30 09:16:27 -0700

    Stop producing the Weekly restoration expiry job #11651
    Signed-off-by: Chen <Steven.Chen@gov.bc.ca>

--- jobs/notebook-report/tests/unit/test_job.py
+++ jobs/notebook-report/tests/unit/test_job.py
@@ -1,11 +1,13 @@
-from datetime import datetime
+"""test job for the notebook."""
 import os
+
 import psycopg2
-import pytest
-import ast
+
 from notebookreport import processnotebooks
 
+
 def test_connection_failed():
+    """Test connection failed for the notebook."""
     status = False
     try:
         connection = psycopg2.connect(user=os.getenv('FAKE_PG_USER', ''),
@@ -14,15 +16,16 @@ def test_connection_failed():
                                       port=os.getenv('FAKE_PG_PORT', '5432'),
                                       database=os.getenv('FAKE_PG_DB_NAME', ''))
 
-        cursor = connection.cursor()
+        connection.cursor()
         status = True
-    except Exception:
+    except Exception:  # noqa: B902
         status = False
     finally:
-        assert status == False
+        assert status is False
 
 
 def test_connection_succeed():
+    """Test connection succeed for the notebook report."""
     status = False
     try:
         connection = psycopg2.connect(user=os.getenv('PG_USER', ''),
@@ -30,23 +33,23 @@ def test_connection_succeed():
                                       host=os.getenv('PG_HOST', ''),
                                       port=os.getenv('PG_PORT', '5432'),
                                       database=os.getenv('PG_DB_NAME', ''))
-        cursor = connection.cursor()
+        connection.cursor()
         status = True
-    except Exception:
+    except Exception:  # noqa: B902
         status = False
     finally:
-        assert status == True
+        assert status is True
 
 
 def test_daily_notebook_report():
-    status = processnotebooks("daily")
+    """Test daily notebook report."""
+    status = processnotebooks('daily')
 
-    assert status == True
-
-
-def test_weekly_notebook_report():    
-    status = processnotebooks("weekly")
-
-    assert status == True
+    assert status is True
 
 
+def test_weekly_notebook_report():
+    """Test weekly notebook report."""
+    status = processnotebooks('weekly')
+
+    assert status is True
