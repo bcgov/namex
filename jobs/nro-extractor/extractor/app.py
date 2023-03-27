6b7243baef2748eb32c72eb165308d1e330ede4a
eve-git <eve.deng@gov.bc.ca>
2023-03-27 16:26:49 -0700

    nro-extractor consider no fee

--- jobs/nro-extractor/extractor/app.py
+++ jobs/nro-extractor/extractor/app.py
@@ -181,12 +181,12 @@ def job(app, namex_db, nro_connection, user, max_rows=100):
                     completed_payments = []
                     paymentExists = False
                     if nr:
-                        paymentExists = nr.payments.first() is None
+                        paymentExists = nr.payments.first() is not None
                         completed_payments = [x for x in nr.payments.all() if x.payment_status_code in [PaymentStatusCode.APPROVED.value, PaymentStatusCode.COMPLETED.value]]
                     # ignore if:
                     # - NR does not exist and NR originated in namex (handles racetime condition for when it is still in the process of saving)
                     # - NR has a pending update from namex (pending payment)
-                    if (not nr and nr_submitter and nr_submitter.get('submitter', '') == 'namex') or (not paymentExists and len(completed_payments) == 0):
+                    if (not nr and nr_submitter and nr_submitter.get('submitter', '') == 'namex') or (paymentExists and len(completed_payments) == 0):
                         success = update_feeder_row(
                             ora_con,
                             row_id=row['id'],
