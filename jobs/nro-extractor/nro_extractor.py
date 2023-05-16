af8e2d3dd2ceee39cf5b3944a63c630864e28c03
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2023-05-16 09:06:11 -0700

    add mutex file to job

--- jobs/nro-extractor/nro_extractor.py
+++ jobs/nro-extractor/nro_extractor.py
@@ -9,6 +9,7 @@ from namex.models import User
 
 from config import Config
 from extractor.app import create_app, db, nro, job
+import os.path
 
 setup_logging() # important to do this first
 
@@ -25,29 +26,38 @@ def get_ops_params():
 
 if __name__ == "__main__":
     start_time = datetime.utcnow()
-    print('nro-extractor: starting job: {}'.format(start_time))
 
-    # setup Flask, push a context, initialize db & nro connections
-    app = create_app(Config)
+    if not os.path.isfile('/mutex/file'):
+        open('/mutex/file', 'w').close()
+        print('nro-extractor: starting job: {}'.format(start_time))
 
-    # get the service account user to save Requests
-    user = User.find_by_username(current_app.config['NRO_SERVICE_ACCOUNT'])
-    max_rows = get_ops_params()
-    processed = 0
+        # setup Flask, push a context, initialize db & nro connections
+        app = create_app(Config)
 
-    # run the job
-    processed = job(app, db, nro.connection, user, max_rows)
+        # get the service account user to save Requests
+        user = User.find_by_username(current_app.config['NRO_SERVICE_ACCOUNT'])
+        max_rows = get_ops_params()
+        processed = 0
 
-    # clean up
-    app.do_teardown_appcontext()
-    end_time = datetime.utcnow()
+        # run the job
+        processed = job(app, db, nro.connection, user, max_rows)
 
-    # report out
-    if processed < 0:
-        print("nro-extractor: errored out: no rows process; completed in:{}".format(end_time - start_time)
-              ,file=sys.stderr)
-        sys.exit(1)
+        # clean up
+        app.do_teardown_appcontext()
+        end_time = datetime.utcnow()
 
-    print("nro-extractor: finished - requests processed: {0} completed in:{1}".format(processed, end_time-start_time),
-          file=sys.stderr)
-    sys.exit(0)
+        os.remove('/mutex/file')
+
+        # report out
+        if processed < 0:
+            print("nro-extractor: errored out: no rows process; completed in:{}".format(end_time - start_time)
+                ,file=sys.stderr)
+            sys.exit(1)
+
+        print("nro-extractor: finished - requests processed: {0} completed in:{1}".format(processed, end_time-start_time),
+            file=sys.stderr)
+        sys.exit(0)
+
+    else:
+        print('previous job still running')
+        sys.exit(0)
