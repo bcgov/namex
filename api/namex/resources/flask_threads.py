e4da1dbe08fc090e0702cc3dbc75fc0ea3fe45ec
Andrew Bolyachevets <Andriy.Bolyachevets@gov.bc.ca>
2023-05-29 16:14:03 -0700

    fire-and-forget emails (#1386)

---
+++ api/namex/resources/flask_threads.py
@@ -0,0 +1,11 @@
+from threading import Thread
+from flask import current_app
+
+class FlaskThread(Thread):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.app = current_app._get_current_object()
+
+    def run(self):
+        with self.app.app_context():
+            super().run()
