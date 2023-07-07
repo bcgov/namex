5e4f12ec3aa25f0c1c63344539edaddd76d4868e
Kial Jinnah <kialjinnah@gmail.com>
2023-07-07 13:30:10 -0700

    ignore unneeded parties
    Signed-off-by: Kial Jinnah <kialjinnah@gmail.com>

--- nro-legacy/sql/object/registry/colin_mgr/trigger/namex_corp_party_qmsg.sql
+++ nro-legacy/sql/object/registry/colin_mgr/trigger/namex_corp_party_qmsg.sql
@@ -4,7 +4,9 @@ DROP TRIGGER NAMEX_CORP_PARTY_QMSG;
 
 CREATE OR REPLACE TRIGGER namex_corp_party_qmsg AFTER INSERT or UPDATE or DELETE ON CORP_PARTY FOR EACH ROW
 BEGIN
-    namex_trigger_handler.enqueue_corporation(:new.corp_num);
+    IF :new.party_typ_cd not in ('PAS','PDI','PSA','RAD','RAF','RAO','RAS','TAP','TAA','TSP') THEN
+        namex_trigger_handler.enqueue_corporation(:new.corp_num);
+    END IF;
 
     EXCEPTION
         WHEN OTHERS THEN
