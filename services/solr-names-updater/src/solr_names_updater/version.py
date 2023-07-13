c40e0f85670818f738964ece3e126200353f05ad
eve-git <54647458+eve-git@users.noreply.github.com>
2023-07-13 11:58:15 -0700

    remove expired name from solr (#1394)

--- services/solr-names-updater/src/solr_names_updater/version.py
+++ services/solr-names-updater/src/solr_names_updater/version.py
@@ -22,4 +22,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '0.3.0'  # pylint: disable=invalid-name
+__version__ = '0.3.1'  # pylint: disable=invalid-name
