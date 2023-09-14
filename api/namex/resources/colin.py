0cf379096e6daf8cac377b77fedeb1288a76c898
Kevin Zhang <kevinzhang940420@gmail.com>
2023-09-14 08:47:06 -0700

    refactor namx-api proxy

--- api/namex/resources/colin.py
+++ api/namex/resources/colin.py
@@ -47,48 +47,8 @@ class ColinServiceException(Exception):
 colin_api = Namespace('colin', description='COLIN API')
 
 
-@cors_preflight('POST')
+@cors_preflight('POST, GET')
 @colin_api.route('/<string:corp_num>', strict_slashes=False, methods=['POST', 'OPTIONS'])
-@colin_api.doc(params={
-    'corp_num': 'Incorporation Number - This field is required'
-})
-class ColinApi(Resource):
-    @cors.crossdomain(origin='*')
-    def post(self, corp_num):
-        try:
-            SBC_SVC_AUTH_URL = current_app.config.get('SBC_SVC_AUTH_URL', '')
-            SBC_SVC_AUTH_CLIENT_ID = current_app.config.get('SBC_SVC_AUTH_CLIENT_ID', '')
-            SBC_SVC_CLIENT_SECRET = current_app.config.get('SBC_SVC_CLIENT_SECRET', '')
-            authenticated, token = get_client_credentials(SBC_SVC_AUTH_URL, SBC_SVC_AUTH_CLIENT_ID, SBC_SVC_CLIENT_SECRET)
-            if not authenticated:
-                raise ColinServiceException(message=MSG_CLIENT_CREDENTIALS_REQ_FAILED)
-
-            # Get the profile
-            print(f'\nCalling COLIN API using [corp_num: {corp_num}]')
-            colin_url = f'{current_app.config.get("COLIN_SVC_URL")}/corporations/{corp_num}'
-            headers = {
-                # 'x-api-key': COLIN_SVC_API_KEY,
-                # 'Accept': 'application/xml'
-                'Authorization': 'Bearer ' + token
-            }
-
-            print(colin_url)
-            print(repr(headers))
-            response = requests.get(
-                colin_url,
-                headers=headers
-            )
-
-            content = json.loads(response.text)
-            if response.status_code != 200:
-                return jsonify(content.get('message')), response.status_code
-            return jsonify(content), response.status_code
-        except ColinServiceException as err:
-            return handle_exception(err, err.message, err.status_code)
-        except Exception as err:
-            return handle_exception(err, 'Internal Server Error', 500)
-
-@cors_preflight('GET')
 @colin_api.route('/<string:corp_num>', strict_slashes=False, methods=['GET', 'OPTIONS'])
 @colin_api.doc(params={
     'corp_num': 'Incorporation Number - This field is required'
@@ -96,35 +56,44 @@ class ColinApi(Resource):
 class ColinApi(Resource):
     @cors.crossdomain(origin='*')
     def post(self, corp_num):
-        try:
-            SBC_SVC_AUTH_URL = current_app.config.get('SBC_SVC_AUTH_URL', '')
-            SBC_SVC_AUTH_CLIENT_ID = current_app.config.get('SBC_SVC_AUTH_CLIENT_ID', '')
-            SBC_SVC_CLIENT_SECRET = current_app.config.get('SBC_SVC_CLIENT_SECRET', '')
-            authenticated, token = get_client_credentials(SBC_SVC_AUTH_URL, SBC_SVC_AUTH_CLIENT_ID, SBC_SVC_CLIENT_SECRET)
-            if not authenticated:
-                raise ColinServiceException(message=MSG_CLIENT_CREDENTIALS_REQ_FAILED)
+        colin_url = f'{current_app.config.get("COLIN_SVC_URL")}/corporations/{corp_num}'
+        response, status_code = _init(colin_url, corp_num)
+        return response, status_code
 
-            # Get the profile
-            print(f'\nCalling COLIN API using [corp_num: {corp_num}]')
-            colin_url = f'{current_app.config.get("COLIN_SVC_URL")}/corporations/business/{corp_num}'
-            headers = {
-                # 'x-api-key': COLIN_SVC_API_KEY,
-                # 'Accept': 'application/xml'
-                'Authorization': 'Bearer ' + token
-            }
+    def get(self, corp_num):
+        colin_url = f'{current_app.config.get("COLIN_SVC_URL")}/corporations/business/{corp_num}'
+        response, status_code = _init(colin_url, corp_num)
+        return response, status_code
 
-            print(colin_url)
-            print(repr(headers))
-            response = requests.get(
-                colin_url,
-                headers=headers
-            )
+def _init(colin_url, corp_num):
+    try:
+        SBC_SVC_AUTH_URL = current_app.config.get('SBC_SVC_AUTH_URL', '')
+        SBC_SVC_AUTH_CLIENT_ID = current_app.config.get('SBC_SVC_AUTH_CLIENT_ID', '')
+        SBC_SVC_CLIENT_SECRET = current_app.config.get('SBC_SVC_CLIENT_SECRET', '')
+        authenticated, token = get_client_credentials(SBC_SVC_AUTH_URL, SBC_SVC_AUTH_CLIENT_ID, SBC_SVC_CLIENT_SECRET)
+        if not authenticated:
+            raise ColinServiceException(message=MSG_CLIENT_CREDENTIALS_REQ_FAILED)
 
-            content = json.loads(response.text)
-            if response.status_code != 200:
-                return jsonify(content.get('message')), response.status_code
-            return jsonify(content), response.status_code
-        except ColinServiceException as err:
-            return handle_exception(err, err.message, err.status_code)
-        except Exception as err:
-            return handle_exception(err, 'Internal Server Error', 500)
+        # Get the profile
+        print(f'\nCalling COLIN API using [corp_num: {corp_num}]')
+        headers = {
+            # 'x-api-key': COLIN_SVC_API_KEY,
+            # 'Accept': 'application/xml'
+            'Authorization': 'Bearer ' + token
+        }
+
+        print(colin_url)
+        print(repr(headers))
+        response = requests.get(
+            colin_url,
+            headers=headers
+        )
+
+        content = json.loads(response.text)
+        if response.status_code != 200:
+            return jsonify(content.get('message')), response.status_code
+        return jsonify(content), response.status_code
+    except ColinServiceException as err:
+        return handle_exception(err, err.message, err.status_code)
+    except Exception as err:
+        return handle_exception(err, 'Internal Server Error', 500)
