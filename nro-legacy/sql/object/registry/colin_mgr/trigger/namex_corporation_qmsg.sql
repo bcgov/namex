039d0def8981f8c4de6becbad258c2ac19c42218
Kial <kialj876@gmail.com>
2023-09-21 16:11:09 -0700

    Oracle trigger: documenting changes being applied by david r (#1423)
    Signed-off-by: Kial Jinnah <kialjinnah@gmail.com>

--- nro-legacy/sql/object/registry/colin_mgr/trigger/namex_corporation_qmsg.sql
+++ nro-legacy/sql/object/registry/colin_mgr/trigger/namex_corporation_qmsg.sql
@@ -4,7 +4,13 @@ DROP TRIGGER NAMEX_CORPORATION_QMSG;
 
 CREATE OR REPLACE TRIGGER namex_corporation_qmsg AFTER INSERT or UPDATE ON CORPORATION FOR EACH ROW
 BEGIN
-    namex_trigger_handler.enqueue_corporation(:new.corp_num);
+    if (nvl(:old.bn_9            ,'^') <> nvl(:new.bn_9             ,'^')
+     or nvl(:old.bn_15           ,'^') <> nvl(:new.bn_15            ,'^')
+     or nvl(:old.corp_typ_cd     ,'^') <> nvl(:new.corp_typ_cd      ,'^')
+     or nvl(:old.last_ar_filed_dt,'^') <> nvl(:new.last_ar_filed_dt ,'^')
+     or nvl(:old.transition_dt   ,'^') <> nvl(:new.transition_dt    ,'^')) then
+        namex_trigger_handler.enqueue_corporation(:new.corp_num);
+    end if;
 
     EXCEPTION
         WHEN OTHERS THEN
