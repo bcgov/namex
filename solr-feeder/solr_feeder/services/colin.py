b4bc011da56b0c6ceb331ae2a415c35a814ecee5
Kial <kialj876@gmail.com>
2023-09-22 07:48:24 -0700

    Solr-feeder: nds update to ignore garbage data (#1425)
    * Solr-feeder: nds update to ignore garbage data
    
    Signed-off-by: Kial Jinnah <kialjinnah@gmail.com>
    
    * fixed lint
    
    Signed-off-by: Kial Jinnah <kialjinnah@gmail.com>
    
    ---------
    
    Signed-off-by: Kial Jinnah <kialjinnah@gmail.com>

--- solr-feeder/solr_feeder/services/colin.py
+++ solr-feeder/solr_feeder/services/colin.py
@@ -139,7 +139,24 @@ def get_parties(legal_type: str, identifier: str, token: str) -> tuple[list[dict
             return None, {'message': all_parties.json(), 'status_code': all_parties.status_code}
 
         for party in (all_parties.json()).get('parties', []):
-            parties.append(_parse_party(party, legal_type))
+            # David R said to ignore these (they are also ignored in the importer job):
+            #   - Data for these is garbage and won't be brought over when they are modernized.
+            #   - party_typ_cds: ('PAS','PDI','PSA','RAD','RAF','RAO','RAS','TAP','TAA','TSP')
+            ignored_types = ['Partner ATT SK', 'Partner DIRECTOR', 'Partner Sign Auth',
+                             'Partner Reinstatement Applicant - Director of foreign entity',
+                             'Partner Reinstatement Applicant - Foreign Entity Reinstated',
+                             'Partner Reinstatement Applicant - Office of foreign entity',
+                             'Partner Reinstatement Applicant - Shareholder of foreign entity',
+                             'TILMA Alternate Attorney', 'TILMA Primary Attorney', 'TILMA Submitting Party']
+            roles = []
+            for role in party['roles']:
+                # only add valid roles
+                if role['roleType'] not in ignored_types:
+                    roles.append(role)
+            if roles:
+                # has valid roles so add party to update
+                party['roles'] = roles
+                parties.append(_parse_party(party, legal_type))
 
         return parties, None
 
