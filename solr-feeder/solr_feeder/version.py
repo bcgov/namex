82ba91437a481327af7987ed6f55d996796f8aaf
Kial Jinnah <kialj876@gmail.com>
2024-01-19 09:29:38 -0800

    ops bug fix for businesses with parties with data issues
    Signed-off-by: Kial Jinnah <kialj876@gmail.com>

--- solr-feeder/solr_feeder/version.py
+++ solr-feeder/solr_feeder/version.py
@@ -21,4 +21,4 @@ Post-release segment: .postN
 Development release segment: .devN
 """
 
-__version__ = '2.0.7'  # pylint: disable=invalid-name
+__version__ = '2.0.8'  # pylint: disable=invalid-name
