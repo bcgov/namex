38dd09c7cc0d2e47638d088defcd55e9c7916298
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2024-02-24 00:08:03 -0800

    remove old queue tests and add gcp queue tests from business pay

--- services/namex-pay/tests/unit/services/__init__.py
+++ services/namex-pay/tests/unit/services/__init__.py
@@ -11,4 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Test suite for the integrations to NATS Queue."""
+"""Test queue based on gcp pubsub"""
