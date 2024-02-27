ec0bc93641935cb55579d29dddef47a8226b51db
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2024-02-26 19:08:41 -0800

    disable old queue

--- api/namex/utils/queue_util.py
+++ api/namex/utils/queue_util.py
@@ -6,7 +6,7 @@ from datetime import datetime, timezone
 from flask import current_app
 
 from namex.services import queue
-from queue_common.messages import create_cloud_event_msg
+# from queue_common.messages import create_cloud_event_msg
 
 
 def publish_email_notification(nr_num: str, option: str, refund_value=None):
@@ -21,14 +21,15 @@ def publish_email_notification(nr_num: str, option: str, refund_value=None):
     if refund_value:
         data['request']['refundValue'] = refund_value
 
-    payload = create_cloud_event_msg(
-        msg_id=str(uuid.uuid4()),
-        msg_type='bc.registry.names.request',
-        source=f'/requests/{nr_num}',
-        time=datetime.utcfromtimestamp(time.time()).replace(tzinfo=timezone.utc).isoformat(),
-        identifier=nr_num,
-        json_data_body=data
-    )
+    # payload = create_cloud_event_msg(
+    #     msg_id=str(uuid.uuid4()),
+    #     msg_type='bc.registry.names.request',
+    #     source=f'/requests/{nr_num}',
+    #     time=datetime.utcfromtimestamp(time.time()).replace(tzinfo=timezone.utc).isoformat(),
+    #     identifier=nr_num,
+    #     json_data_body=data
+    # )
+    payload = {}
 
     email_subject = current_app.config.get('NATS_EMAILER_SUBJECT')
     current_app.logger.debug('About to publish email for %s nrNum=%s', option, nr_num)
@@ -37,21 +38,22 @@ def publish_email_notification(nr_num: str, option: str, refund_value=None):
 
 def create_name_request_state_msg(nr_num, state_cd, old_state_cd):
     """Builds a name request state message."""
-    return create_cloud_event_msg(msg_id=str(uuid.uuid4()),
-                                            msg_type='bc.registry.names.events',
-                                            source=f'/requests/{nr_num}',
-                                            time=datetime.
-                                            utcfromtimestamp(time.time()).
-                                            replace(tzinfo=timezone.utc).
-                                            isoformat(),
-                                            identifier=nr_num,
-                                            json_data_body={
-                                                'request': {
-                                                    'nrNum': nr_num,
-                                                    'newState': state_cd,
-                                                    'previousState': old_state_cd
-                                                }}
-                                            )
+    return {}
+    # return create_cloud_event_msg(msg_id=str(uuid.uuid4()),
+    #                                         msg_type='bc.registry.names.events',
+    #                                         source=f'/requests/{nr_num}',
+    #                                         time=datetime.
+    #                                         utcfromtimestamp(time.time()).
+    #                                         replace(tzinfo=timezone.utc).
+    #                                         isoformat(),
+    #                                         identifier=nr_num,
+    #                                         json_data_body={
+    #                                             'request': {
+    #                                                 'nrNum': nr_num,
+    #                                                 'newState': state_cd,
+    #                                                 'previousState': old_state_cd
+    #                                             }}
+    #                                         )
 
 def send_name_request_state_msg(nr_num, state_cd, old_state_cd):
     """Publish name request state message to NATS nr state subject."""
@@ -65,23 +67,23 @@ def send_name_request_state_msg(nr_num, state_cd, old_state_cd):
 
 def create_name_state_msg(nr_num, name_id, state_cd, old_state_cd):
     """Builds a name state message."""
-    return create_cloud_event_msg(msg_id=str(uuid.uuid4()),
-                                  msg_type='bc.registry.names.events',
-                                  source=f'/request/{nr_num}/name/{name_id}',
-                                  time=datetime.
-                                    utcfromtimestamp(time.time()).
-                                    replace(tzinfo=timezone.utc).
-                                    isoformat(),
-                                  identifier=name_id,
-                                  json_data_body={
-                                      'name': {
-                                          'nameId': name_id,
-                                          'nrNum': nr_num,
-                                          'newState': state_cd,
-                                          'previousState': old_state_cd
-                                      }}
-                                  )
-
+    # return create_cloud_event_msg(msg_id=str(uuid.uuid4()),
+    #                               msg_type='bc.registry.names.events',
+    #                               source=f'/request/{nr_num}/name/{name_id}',
+    #                               time=datetime.
+    #                                 utcfromtimestamp(time.time()).
+    #                                 replace(tzinfo=timezone.utc).
+    #                                 isoformat(),
+    #                               identifier=name_id,
+    #                               json_data_body={
+    #                                   'name': {
+    #                                       'nameId': name_id,
+    #                                       'nrNum': nr_num,
+    #                                       'newState': state_cd,
+    #                                       'previousState': old_state_cd
+    #                                   }}
+    #                               )
+    return {}
 
 def send_name_state_msg(nr_num, name_id, state_cd, old_state_cd):
     """Publish name state message to NATS nr state subject."""
