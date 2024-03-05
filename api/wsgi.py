c4401ee6f2f155cf944b88b0c14090ba9f4a33cd
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2024-03-05 02:15:12 -0800

    remove migration script

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,8 +1,10 @@
 # from app import application
-from namex import create_app
-import sys
+from flask_migrate import Migrate
+
+from namex import create_app, db
 
 app = create_app()
+migrate = Migrate(app, db)
 
 if __name__ == "__main__":
     app.run()
