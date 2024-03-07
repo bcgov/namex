1b41106640a9d7c7203b4c0989a9d269b3648e95
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2024-03-07 01:03:42 -0800

    add auth

---
+++ services/pubsub/src/gcp_auth.py
@@ -0,0 +1,36 @@
+import functools
+from http import HTTPStatus
+
+import google.oauth2.id_token as id_token
+from cachecontrol import CacheControl
+from flask import abort, current_app, request
+from google.auth.transport.requests import Request
+from requests.sessions import Session
+
+
+def verify_jwt(session):
+    """Verify token is valid."""
+    msg = ''
+    try:
+        # Get the Cloud Pub/Sub-generated JWT in the "Authorization" header.
+        id_token.verify_oauth2_token(
+            request.headers.get("Authorization").split()[1],
+            Request(session=session),
+            audience=current_app.config.get("PAY_SUB_AUDIENCE")
+        )
+    except Exception as e:  # TODO fix
+        msg = f"Invalid token: {e}\n"
+    finally:
+        return msg
+
+
+def ensure_authorized_queue_user(f):
+    """Ensures the user is authorized to use the queue."""
+    @functools.wraps(f)
+    def decorated_function(*args, **kwargs):
+        # Use CacheControl to avoid re-fetching certificates for every request.
+        if message := verify_jwt(CacheControl(Session())):
+            print(message)
+            abort(HTTPStatus.UNAUTHORIZED)
+        return f(*args, **kwargs)
+    return decorated_function
