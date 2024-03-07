29c90231a85226c764087966a36474e7685b18a3
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2024-03-07 02:00:27 -0800

    rename

---
+++ services/pubsub/gcp_queue/__init__.py
@@ -0,0 +1,3 @@
+from .gcp_queue import GcpQueue
+from .logging import structured_log
+from .gcp_auth import ensure_authorized_queue_user
--- services/pubsub/src/__init__.py
+++
@@ -1,3 +0,0 @@
-from gcp_queue import GcpQueue
-from logging import structured_log
-from gcp_auth import ensure_authorized_queue_user
