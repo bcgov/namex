327a67f9fe7c6cc641f4ac47fb25a7f64fd0cbd1
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2024-06-15 20:04:25 -0700

    remove unused var

--- services/solr-names-updater/config.py
+++ services/solr-names-updater/config.py
@@ -100,7 +100,6 @@ class Config():  # pylint: disable=too-few-public-methods
     JWT_OIDC_JWKS_URI = os.getenv('JWT_OIDC_JWKS_URI')
     JWT_OIDC_ISSUER = os.getenv('JWT_OIDC_ISSUER')
     JWT_OIDC_AUDIENCE = os.getenv('JWT_OIDC_AUDIENCE')
-    JWT_OIDC_CLIENT_SECRET = os.getenv('JWT_OIDC_CLIENT_SECRET')
     JWT_OIDC_CACHING_ENABLED = os.getenv('JWT_OIDC_CACHING_ENABLED')
 
     JWT_OIDC_JWKS_CACHE_TIMEOUT = int(os.getenv('JWT_OIDC_JWKS_CACHE_TIMEOUT', '300'))
