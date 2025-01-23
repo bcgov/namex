398c2576dd0fe64dd4dd00c432ad9d52c01bc61a
EPortman <Ethan.Portman@gov.bc.ca>
2025-01-23 15:32:47 -0800

    solr-feeder gcp build

--- solr-feeder/wsgi.py
+++ solr-feeder/wsgi.py
@@ -9,6 +9,6 @@ format='%(asctime)s - %(name)s - %(levelname)s in %(module)s:%(filename)s:%(line
 logging.basicConfig(level=logging.DEBUG, format=format)
 
 # Listen on all interfaces, and the catalog Python container expects the application to be on 8080.
-application = solr_feeder.create_application()
+app = solr_feeder.create_application()
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
