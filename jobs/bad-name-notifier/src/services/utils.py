7a7a0f54982c1343f98a2c0ec7cfc520d3b57c5a
EPortman <Ethan.Portman@gov.bc.ca>
2025-02-06 12:46:07 -0800

    migrate bad-name-notifier job

--- jobs/bad-name-notifier/src/services/utils.py
+++ jobs/bad-name-notifier/src/services/utils.py
@@ -1,10 +1,13 @@
 from datetime import datetime, timedelta
 import pytz
+import requests
+from cachetools import TTLCache, cached
+from flask import current_app
 
 def get_yesterday_str():
         # Calculate yesterday's date
     yesterday = datetime.now() - timedelta(days=1)
-    
+
     # Format the date as yyyy-mm-dd
     return yesterday.strftime('%Y-%m-%d')
 
@@ -20,3 +23,24 @@ def get_yesterday_utc_range():
     start_of_yesterday_utc = start_of_yesterday_pacific.astimezone(pytz.utc).strftime('%Y-%m-%d %H:%M:%S')
 
     return start_of_yesterday_utc, start_of_today_utc
+
+@staticmethod
+@cached(cache=TTLCache(maxsize=1, ttl=180))
+def get_bearer_token():
+    """Get a valid Bearer token for the service to use."""
+    token_url = current_app.config.get("ACCOUNT_SVC_AUTH_URL")
+    client_id = current_app.config.get("ACCOUNT_SVC_CLIENT_ID")
+    client_secret = current_app.config.get("ACCOUNT_SVC_CLIENT_SECRET")
+
+    # get service account token
+    res = requests.post(
+        url=token_url,
+        data="grant_type=client_credentials",
+        headers={"content-type": "application/x-www-form-urlencoded"},
+        auth=(client_id, client_secret),
+    )
+
+    try:
+        return res.json().get("access_token")
+    except Exception:
+        return None
