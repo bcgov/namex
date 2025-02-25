43a53dce905f6577b15c7764060ece626db87dab
EPortman <Ethan.Portman@gov.bc.ca>
2025-02-25 09:36:54 -0800

    migrate solr synonyms api

--- solr-synonyms-api/manage.py
+++ solr-synonyms-api/manage.py
@@ -1,18 +1,13 @@
 """Manage the database and some other items required to run the API
 """
-from flask_script import Manager # class for handling a set of commands
-from flask_migrate import Migrate, MigrateCommand
-from synonyms import create_application
+from synonyms import create_app
 from synonyms.models import db
-from synonyms import models
+from flask_migrate import Migrate
 import logging
 
-app = create_application()
+app = create_app()
 migrate = Migrate(app, db)
-manager = Manager(app)
-
-manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    logging.log(logging.INFO, 'Running the Manager')
-    manager.run()
+    logging.info('Running the Manager')
+    app.run()
--- solr-synonyms-api/wsgi.py
+++ solr-synonyms-api/wsgi.py
@@ -1,7 +1,7 @@
 """Create and run an instance of this service."""
 from synonyms import create_app
 
-application = create_app()
+app = create_app()
 
 if __name__ == '__main__':
-    application.run()
+    app.run()
