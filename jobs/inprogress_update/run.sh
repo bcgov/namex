ccfbf56223cbfead059c7c1243aa9e1a84500cc1
EPortman <169073732+EPortman@users.noreply.github.com>
2025-02-26 12:14:27 -0800

    migrate inprogress updater on gcp branch (#1714)

---
+++ jobs/inprogress_update/run.sh
@@ -0,0 +1,3 @@
+cd /opt/app-root
+echo 'run inprogress-update'
+python src/inprogress_update/inprogress_update.py
---
+++ jobs/inprogress_update/tests/unit/__init__.py
@@ -0,0 +1,15 @@
+"""
+Expose the custom decorators used skip tests unless their environment variables are set
+
+The presence any of the following env vars will let those tests run
+set :
+    ORACLE_NAMESDB_TESTS to run integration_oracle_namesdb
+    FDW_NAMEX_TESTS to run integration_fdw_namex
+    SOLR_TESTS to run integration_solr
+    NRO_EXTRACTOR_TESTS to run integration_nro_extractor
+
+"""
+import datetime
+
+EPOCH_DATETIME = datetime.datetime.utcfromtimestamp(0)
+FROZEN_DATETIME = datetime.datetime(2001, 8, 5, 7, 7, 58, 272362)
