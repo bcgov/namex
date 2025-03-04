4fb0e21c9790064739f342919d56f19f9cd457fd
EPortman <169073732+EPortman@users.noreply.github.com>
2025-03-04 08:37:54 -0800

    feature: namex pay

--- services/namex-pay/config.py
+++ services/namex-pay/config.py
@@ -72,14 +72,10 @@ class Config():  # pylint: disable=too-few-public-methods
     DB_NAME = os.getenv('NAMEX_DATABASE_NAME', '')
     DB_HOST = os.getenv('NAMEX_DATABASE_HOST', '')
     DB_PORT = os.getenv('NAMEX_DATABASE_PORT', '5432')
-    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{int(DB_PORT)}/{DB_NAME}'
-
-    # ORACLE - LEGACY NRO NAMESDB
-    NRO_USER = os.getenv('NRO_USER', '')
-    NRO_PASSWORD = os.getenv('NRO_PASSWORD', '')
-    NRO_DB_NAME = os.getenv('NRO_DB_NAME', '')
-    NRO_HOST = os.getenv('ORACLE_HOST', '')
-    NRO_PORT = int(os.getenv('ORACLE_PORT', '1521'))
+    if DB_UNIX_SOCKET := os.getenv('NAMEX_DATABASE_UNIX_SOCKET', None):
+        SQLALCHEMY_DATABASE_URI = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}?host={DB_UNIX_SOCKET}'
+    else:
+        SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{int(DB_PORT)}/{DB_NAME}'
 
     GCP_AUTH_KEY = os.getenv('BUSINESS_GCP_AUTH_KEY', None)
     EMAILER_TOPIC = os.getenv('NAMEX_MAILER_TOPIC', '')
