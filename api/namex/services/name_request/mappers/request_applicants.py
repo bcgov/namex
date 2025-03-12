23b5b73bd5de79a318ec7295c3fe933d5eca11c5
eve-git <eve.deng@gov.bc.ca>
2025-03-12 14:29:19 -0700

    update map request applicant to allow optional field be removed

--- api/namex/services/name_request/mappers/request_applicants.py
+++ api/namex/services/name_request/mappers/request_applicants.py
@@ -5,26 +5,19 @@ from namex.models import Applicant
 def map_request_applicant(applicant: Applicant, request_applicant: dict):
     applicant.lastName = convert_to_ascii(request_applicant.get('lastName'))
     applicant.firstName = convert_to_ascii(request_applicant.get('firstName'))
-    if request_applicant.get('middleName'):
-        applicant.middleName = convert_to_ascii(request_applicant.get('middleName'))
-    applicant.contact = convert_to_ascii(request_applicant.get('contact'))
+    applicant.middleName = convert_to_ascii(request_applicant.get('middleName')) if request_applicant.get('middleName') else None
+    applicant.contact = convert_to_ascii(request_applicant.get('contact')) if request_applicant.get('contact') else None
 
-    if request_applicant.get('clientFirstName'):
-        applicant.clientFirstName = convert_to_ascii(request_applicant.get('clientFirstName'))
-    if request_applicant.get('clientLastName'):
-        applicant.clientLastName = convert_to_ascii(request_applicant.get('clientLastName'))
+    applicant.clientFirstName = convert_to_ascii(request_applicant.get('clientFirstName')) if request_applicant.get('clientFirstName') else None
+    applicant.clientLastName = convert_to_ascii(request_applicant.get('clientLastName')) if request_applicant.get('clientLastName') else None
 
-    if request_applicant.get('phoneNumber'):
-        applicant.phoneNumber = convert_to_ascii(request_applicant.get('phoneNumber'))
-    if request_applicant.get('faxNumber'):
-        applicant.faxNumber = convert_to_ascii(request_applicant.get('faxNumber'))
+    applicant.phoneNumber = convert_to_ascii(request_applicant.get('phoneNumber'))
+    applicant.faxNumber = convert_to_ascii(request_applicant.get('faxNumber')) if request_applicant.get('faxNumber') else None
     applicant.emailAddress = convert_to_ascii(request_applicant.get('emailAddress'))
 
     applicant.addrLine1 = convert_to_ascii(request_applicant.get('addrLine1'))
-    if request_applicant.get('addrLine2'):
-        applicant.addrLine2 = convert_to_ascii(request_applicant.get('addrLine2'))
-    if request_applicant.get('addrLine3'):
-        applicant.addrLine3 = convert_to_ascii(request_applicant.get('addrLine3'))
+    applicant.addrLine2 = convert_to_ascii(request_applicant.get('addrLine2')) if request_applicant.get('addrLine2') else None
+    applicant.addrLine3 = convert_to_ascii(request_applicant.get('addrLine3')) if request_applicant.get('addrLine3') else None
     applicant.city = convert_to_ascii(request_applicant.get('city'))
     applicant.stateProvinceCd = request_applicant.get('stateProvinceCd')
     applicant.postalCd = convert_to_ascii(request_applicant.get('postalCd'))
