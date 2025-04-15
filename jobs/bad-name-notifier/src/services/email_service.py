bfdc8370094af04635a324c7f1a254a50322c86f
EPortman <Ethan.Portman@gov.bc.ca>
2025-04-15 15:13:31 -0700

    clean up email logs in bad name notifier job

--- jobs/bad-name-notifier/src/services/email_service.py
+++ jobs/bad-name-notifier/src/services/email_service.py
@@ -36,22 +36,21 @@ def send_email_notification(bad_names):
 
     # Send email via Notify API
     token = get_bearer_token()
-    for recipient in recipients:
-        email_data = {
-            "recipients": recipient,
-            "content": {
-                "subject": "Bad characters in names",
-                "body": body,
-                "attachments": [],
-            },
-        }
-        resp = send_email(email_data, token)
-        if resp.status_code == HTTPStatus.OK:
-            current_app.logger.info(f"Email sent successfully to: {recipient}")
-        else:
-            current_app.logger.error(
-                f"Failed to send email. Status Code: {resp.status_code}, Response: {resp.text}"
-            )
+    email_data = {
+        "recipients": ",".join(recipients),
+        "content": {
+            "subject": "Bad characters in names",
+            "body": body,
+            "attachments": [],
+        },
+    }
+    resp = send_email(email_data, token)
+    if resp.status_code == HTTPStatus.OK:
+        current_app.logger.info(f"Email sent successfully to: {recipients}")
+    else:
+        current_app.logger.error(
+            f"Failed to send email. Status Code: {resp.status_code}, Response: {resp.text}"
+        )
 
 def generate_report_title():
     """Generates an email title with yesterday's date."""
