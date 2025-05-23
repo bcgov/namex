ac9c1d418f67d61713c715d7b44fe6d53bdf5535
EPortman <Ethan.Portman@gov.bc.ca>
2025-05-23 12:20:51 -0700

    upgrade solr admin app to flask3

--- solr-admin-app/solr_admin/views/secured_view.py
+++ solr-admin-app/solr_admin/views/secured_view.py
@@ -1,33 +1,15 @@
-from flask_admin.contrib import sqla
-from flask import request
-from solr_admin import keycloak
+from flask_admin.contrib.sqla import ModelView
+from flask import request, redirect
+from solr_admin.keycloak import Keycloak
 
-
-class SecuredView(sqla.ModelView):
-
-    def __init__(self, model, session,
-                 name=None, category=None, endpoint=None, url=None, static_folder=None,
-                 menu_class_name=None, menu_icon_type=None, menu_icon_value=None):
-        super().__init__(model, session,
-                 name, category, endpoint, url, static_folder,
-                 menu_class_name, menu_icon_type, menu_icon_value)
-        self.connected = False
+class SecuredView(ModelView):
 
     # At runtime determine whether or not the user has access to functionality of the view.
     def is_accessible(self):
-        kc = keycloak.Keycloak(None)
-
-        if not kc.is_logged_in():
-            self.connected = False
-            return False
-
-        return kc.has_access()
+        # Returns true only if the user has a valid token and the right role
+        return Keycloak().has_access()
 
     # At runtime determine what to do if the view is not accessible.
     def inaccessible_callback(self, name, **kwargs):
-        if not self.connected:
-            self.connected = True
-            kc = keycloak.Keycloak(None)
-            return kc.get_redirect_url(request.url)
-        else:
-            return 'not authorized'
+        # Redirect the browser into Keycloak’s OIDC flow when not logged in
+        return redirect(Keycloak().get_redirect_url(request.url))
