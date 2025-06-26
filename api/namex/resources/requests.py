01b93f4e6da0c42dccb72597b3d9f70e9e3ed8e0
Rajandeep <rajandeep.kaur@gov.bc.ca>
2025-06-26 09:11:47 -0700

    28974 - Search Pagination NR

--- api/namex/resources/requests.py
+++ api/namex/resources/requests.py
@@ -644,10 +644,12 @@ class RequestSearch(Resource):
             and search_details.limit is not None
             and search_details.limit > 0
         ):
-            q = q.offset((search_details.page - 1) * search_details.limit).limit(search_details.limit)
+            offset_value = (search_details.page - 1) * search_details.limit
+            q = q.offset(offset_value).limit(search_details.limit + 1)
+        
+        q_results = q.all()
 
-        q = q.offset((search_details.page - 1) * search_details.limit).limit(search_details.limit)
-        requests = request_auth_search_schemas.dump(q.all())
+        requests = request_auth_search_schemas.dump(q_results[:search_details.limit])
         actions_array = [
             nr_filing_actions.get_actions(r['requestTypeCd'], r['entity_type_cd'], r['request_action_cd'])
             for r in requests
@@ -655,9 +657,8 @@ class RequestSearch(Resource):
         for r, additional_fields in zip(requests, actions_array):
             if additional_fields:
                 r.update(additional_fields)
-
-        return jsonify(requests)
-
+        has_more = len(q_results) > search_details.limit
+        return jsonify({'requests': requests, 'hasMore': has_more})
 
 # noinspection PyUnresolvedReferences
 @cors_preflight('GET, PATCH, PUT, DELETE')
