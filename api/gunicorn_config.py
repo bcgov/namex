6b07b75d4fdaee287ca1714d4b41da15e9194d40
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2025-09-10 18:56:40 -0700

    update gunicorn configs

--- api/gunicorn_config.py
+++ api/gunicorn_config.py
@@ -1,7 +1,9 @@
 import os
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
-threads = int(os.environ.get('GUNICORN_THREADS', '1'))
+workers = int(os.environ.get("GUNICORN_PROCESSES", "1"))  # pylint: disable=invalid-name
+threads = int(os.environ.get("GUNICORN_THREADS", "8"))  # pylint: disable=invalid-name
+timeout = int(os.environ.get("GUNICORN_TIMEOUT", "0"))  # pylint: disable=invalid-name
+
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = {'X-Forwarded-Proto': 'https'}
