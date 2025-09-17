48fa8e910e8d1ef4a599f10ea3e1c435f045ff87
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2025-09-16 19:53:04 -0700

    revert

--- api/config.py
+++ api/config.py
@@ -142,7 +142,7 @@ class TestConfig(Config):  # pylint: disable=too-few-public-methods
     DB_PASSWORD = os.getenv('DATABASE_TEST_PASSWORD', 'postgres')
     DB_NAME = os.getenv('DATABASE_TEST_NAME', 'unittesting')
     DB_HOST = os.getenv('DATABASE_TEST_HOST', 'localhost')
-    DB_PORT = os.getenv('DATABASE_TEST_PORT', '5432')
+    DB_PORT = os.getenv('DATABASE_TEST_PORT', '54345')
     SQLALCHEMY_DATABASE_URI = f'postgresql+pg8000://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{int(DB_PORT)}/{DB_NAME}'
 
     # Ensure SQLAlchemy is properly configured for Flask-Marshmallow compatibility
