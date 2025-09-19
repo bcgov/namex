55c43a5da6eabc910d517ffc4d5c050eb24ba030
Andrew Bolyachevets <Andriy.Bolyachevets@gov.bc.ca>
2025-09-19 12:15:42 -0700

    Inporgress job (#1906)
    * init
    
    * add some unit tests for CI
    
    * test config
    
    * tweak git repo source
    
    * db/queue changes
    
    * add migration to test
    
    * copy missing file
    
    * try refactoring
    
    * fix
    
    * missing vars

--- jobs/inprogress_update/tests/unit/conftest.py
+++ jobs/inprogress_update/tests/unit/conftest.py
@@ -1,17 +1,65 @@
-"""This is test config."""
+# Copyright © 2025 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Common test configuration and fixtures."""
+
 import datetime
 import logging
+import os
+import random
+from contextlib import contextmanager
+from unittest.mock import patch
+
 import pytest
-from inprogress_update.config import TestConfig
+from flask_migrate import Migrate, upgrade
 from sqlalchemy import event, text
-from inprogress_update.inprogress_update import create_app, db as _db
+
+from config import TestConfig
+from inprogress_update import create_app
+from inprogress_update import db as _db
+
 from . import FROZEN_DATETIME
 
+# ----------------------------
+# Context managers / helpers
+# ----------------------------
 
-# fixture to freeze utcnow to a fixed date-time
+
+@contextmanager
+def not_raises(exception):
+    """Assures that an exception is NOT thrown."""
+    try:
+        yield
+    except exception:
+        raise pytest.fail(f'DID RAISE {exception}')
+
+
+# ----------------------------
+# Fixtures
+# ----------------------------
+
+# Mock Pub/Sub queue publish for all tests
+@pytest.fixture(autouse=True)
+def mock_pubsub_publish():
+    with patch('gcp_queue.gcp_queue.GcpQueue.publish') as mock_publish:
+        mock_publish.return_value = None
+        yield mock_publish
+
+
+# Fixture to freeze utcnow to a fixed datetime
 @pytest.fixture
-def patch_datetime_utcnow(monkeypatch):
-    """Return the FROZEN_DATETIME."""
+def freeze_datetime_utcnow(monkeypatch):
+    """Return a static time for datetime.utcnow()."""
     class _Datetime:
         @classmethod
         def utcnow(cls):
@@ -21,55 +69,86 @@ def patch_datetime_utcnow(monkeypatch):
 
 
 @pytest.fixture(scope='session')
-def app(request):
-    """Returns session-wide application."""
-    logging.log(logging.INFO, TestConfig().SQLALCHEMY_DATABASE_URI)
-    app = create_app(TestConfig())
-
-    return app
+def app():
+    """Return a session-wide Flask app configured in TEST mode."""
+    logging.info(f'Using test DB: {TestConfig().SQLALCHEMY_DATABASE_URI}')
+    _app = create_app(TestConfig())
+    return _app
 
 
 @pytest.fixture
+def config(app):
+    """Return the app config."""
+    return app.config
+
+
+@pytest.fixture(scope='session')
 def client(app):
-    """Returns client."""
-    client = app.test_client()
-
-    return client
+    """Return a session-wide test client."""
+    return app.test_client()
 
 
-@pytest.fixture(scope="function")
-def session(app, request):
-    """
-    Returns function-scoped session.
-    """
+@pytest.fixture(scope='session')
+def client_ctx(app):
+    """Return a session-wide test client context manager."""
+    with app.test_client() as _client:
+        yield _client
+
+
+@pytest.fixture(scope='function')
+def client_id():
+    """Return a unique client ID for testing purposes."""
+    _id = random.SystemRandom().getrandbits(0x58)
+    return f'client-{_id}'
+
+
+@pytest.fixture(scope='function', autouse=True)
+def session(app, db):
+    """Provide a function-scoped rollbacked session for tests."""
     with app.app_context():
-        conn = _db.engine.connect()
-        txn = conn.begin()
+        connection = db.engine.connect()
+        transaction = connection.begin()
 
-        options = dict(bind=conn, binds={})
-        sess = _db.create_scoped_session(options=options)
+        options = dict(bind=connection, binds={})
+        session_ = db._make_scoped_session(options=options)
+        db.session = session_
 
-        # establish  a SAVEPOINT just before beginning the test
-        # (http://docs.sqlalchemy.org/en/latest/orm/session_transaction.html#using-savepoint)
-        sess.begin_nested()
+        # Begin nested transaction / savepoint
+        session_.begin_nested()
 
-        @event.listens_for(sess(), 'after_transaction_end')
+        @event.listens_for(session_(), 'after_transaction_end')
         def restart_savepoint(sess2, trans):
-            # Detecting whether this is indeed the nested transaction of the test
             if trans.nested and not trans._parent.nested:
-                # Handle where test DOESN'T session.commit(),
                 sess2.expire_all()
-                sess.begin_nested()
+                sess2.begin_nested()
 
-        _db.session = sess
+        # Simple query to initialize session
+        session_.execute(text('SELECT 1'))
 
-        sql = text('select 1')
-        sess.execute(sql)
+        yield session_
 
-        yield sess
+        # Rollback and cleanup
+        transaction.rollback()
+        connection.close()
+        session_.remove()
 
-        # Cleanup
-        sess.remove()
-        # This instruction rollsback any commit that were executed in the tests.
-        txn.rollback()
-        conn.close()
+
+@pytest.fixture(scope='function')
+def db(app):
+    """Return a session-wide initialized database with all migrations applied."""
+    with app.app_context():
+        # Drop and recreate public schema
+        with _db.engine.connect() as conn:
+            conn.execute(text('DROP SCHEMA public CASCADE;'))
+            conn.execute(text('CREATE SCHEMA public;'))
+            conn.execute(text('GRANT ALL ON SCHEMA public TO postgres;'))
+            conn.execute(text('GRANT ALL ON SCHEMA public TO public;'))
+            conn.commit()
+
+        # Run Alembic migrations
+        api_dir = os.path.abspath('..').replace('jobs', 'api')
+        migrations_dir = os.path.join(api_dir, 'migrations')
+        Migrate(app, _db, directory=migrations_dir)
+        upgrade()
+
+        return _db
