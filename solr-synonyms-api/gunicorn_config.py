1d624d95f624c65bf1a2152d970f61b7b65a9e0e
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2025-11-10 18:37:31 -0800

    put back gunicorn config

--- solr-synonyms-api/gunicorn_config.py
+++ solr-synonyms-api/gunicorn_config.py
@@ -3,6 +3,7 @@ import os
 workers = int(os.environ.get("GUNICORN_PROCESSES", "1"))  # pylint: disable=invalid-name
 threads = int(os.environ.get("GUNICORN_THREADS", "8"))  # pylint: disable=invalid-name
 timeout = int(os.environ.get("GUNICORN_TIMEOUT", "0"))  # pylint: disable=invalid-name
+limit_request_line = int(os.environ.get("GUNICORN_MAX_REQUEST_LINE", "0"))
 
 
 forwarded_allow_ips = "*"
