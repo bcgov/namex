04e4ff03ffe54f229718d648a4b6b5bf7424c702
eve-git <54647458+eve-git@users.noreply.github.com>
2025-11-14 10:57:38 -0800

    29879 Update according to new Solr-API highlighting output (#1925)
    * Update according to new Solr-API highlighting output
    
    * Lint error fix

--- api/namex/services/solr/solr_helpers.py
+++ api/namex/services/solr/solr_helpers.py
@@ -1,13 +1,7 @@
 import string
 
 from namex.constants import Designations
-from namex.services.solr import (
-    first_consonants,
-    first_vowels,
-    has_leading_vowel,
-    replace_special_leading_sounds,
-    words_to_filter_from_name,
-)
+from namex.services.solr import words_to_filter_from_name
 from namex.services.solr.solr_client import SolrClient
 
 
@@ -55,7 +49,7 @@ class SolrHlpers:
         return processed_name.strip()
 
     @classmethod
-    def _conflicts_post_process(cls, q_data, synonyms, query_name):
+    def _conflicts_post_process(cls, q_data, query_name):
         """
         Processes Solr search results to filter candidates based on phonetic matching and designation exclusion.
 
@@ -78,14 +72,25 @@ class SolrHlpers:
                 if rcd.get('name_state') in ('CORP', 'A'):
                     histories.append(rcd)
             else:
+                highlighting = rcd.get('highlighting', {})
+                exact = cls.normalize_words(highlighting.get('exact', []))
+                stems = cls.normalize_words(highlighting.get('stems', []))
+                synonyms = cls.normalize_words(highlighting.get('synonyms', []))
+
                 rcd['type'] = 'similar'
-                stems = cls._find_stems(nm, query_name, synonyms)
-                rcd['stems'] = stems
+                rcd['highlighting'] = { 'exact': list(exact), 'stems': list(stems), 'synonyms': list(synonyms) }
                 similar_matches.append(rcd)
+
         return {
             'names': similar_matches,
             'exactNames': exact_matches,
             'histories': histories}
+
+    def normalize_words(word):
+        """
+        Normalize a list of words by removing punctuation and converting to uppercase.
+        """
+        return [w.upper().translate(str.maketrans('', '', string.punctuation)) for w in word]
 
     @classmethod
     def _find_stems(cls, name, query_name, synonyms):
@@ -133,74 +138,10 @@ class SolrHlpers:
         return ' '.join(filtered_words)
 
     @classmethod
-    def _phonetic_match(cls, word, query):
-        # NOTE: The previous Solr search supported phonetic matching.
-        # The current Solr service only matches exact words.
-        # Business requirements for phonetic matching are unclear, and it is unknown if this feature will return.
-        # This function is retained for possible future use if phonetic matching is reintroduced.
-        word = replace_special_leading_sounds(word)
-        query = replace_special_leading_sounds(query)
-
-        word_has_leading_vowel = has_leading_vowel(word)
-        query_has_leading_vowel = has_leading_vowel(query)
-
-        word_first_consonant = first_consonants(word)
-        query_first_consonant = first_consonants(query)
-
-        query_first_vowels = first_vowels(query, query_has_leading_vowel)
-        word_first_vowels = first_vowels(word, word_has_leading_vowel)
-
-        if query_has_leading_vowel:
-            query_sound = query_first_vowels + query_first_consonant
-        else:
-            query_sound = query_first_consonant + query_first_vowels
-
-        if word_has_leading_vowel:
-            word_sound = word_first_vowels + word_first_consonant
-        else:
-            word_sound = word_first_consonant + word_first_vowels
-
-        if word_sound == query_sound:
-            return True
-
-        return False
-
-    @classmethod
-    def _keep_candidate(cls, candidate, name, names):
-        if len([doc['id'] for doc in names if doc['id'] == candidate['id']]) == 0:
-            names.append(
-                {
-                    'name': name,
-                    'id': candidate['id'],
-                    'source': candidate['source'],
-                    'jurisdiction': candidate.get('jurisdiction', ''),
-                    'start_date': candidate.get('start_date', ''),
-                }
-            )
-
-    @classmethod
     def get_possible_conflicts(cls, name, start=0, rows=100):
         q_name = cls._name_pre_processing(name)
         q_name = cls._get_name_without_designation(q_name)
 
         candidates = SolrClient.get_possible_conflicts(q_name, start, rows)
+        return cls._conflicts_post_process(candidates, q_name)
 
-        name_ngrams = cls._get_name_ngrams(q_name)
-        synonyms = SolrClient.get_synonyms(name_ngrams)
-
-        return cls._conflicts_post_process(candidates, synonyms, q_name)
-
-
-    @classmethod
-    def _get_name_ngrams(cls, name):
-        """
-        Given a name string, returns all contiguous word combinations (n-grams).
-        Example: 'test name 123' -> ['test', 'test name', 'test name 123', 'name', 'name 123', '123']
-        """
-        words = name.split()
-        ngrams = []
-        for i in range(len(words)):
-            for j in range(i + 1, len(words) + 1):
-                ngram = ' '.join(words[i:j])
-                ngrams.append(ngram)
-        return ngrams
