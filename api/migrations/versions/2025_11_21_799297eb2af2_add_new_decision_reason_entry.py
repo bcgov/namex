6c01dc62838439efb927ea5e0de99fb3aa28d3b4
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2025-11-21 11:10:35 -0800

    chore - Add migration stub for new decision reason entry

---
+++ api/migrations/versions/2025_11_21_799297eb2af2_add_new_decision_reason_entry.py
@@ -0,0 +1,24 @@
+"""add new decision reason entry
+
+Revision ID: 799297eb2af2
+Revises: e5e4e389f21d
+Create Date: 2025-11-21 10:59:02.782992
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '799297eb2af2'
+down_revision = 'e5e4e389f21d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
