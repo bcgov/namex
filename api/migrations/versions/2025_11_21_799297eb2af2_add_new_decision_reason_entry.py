70aebf47bd7b12e5f79b61b87ac88a47acd810f2
Hrvoje Fekete <hrvoje.fekete@gmail.com>
2025-11-21 11:13:09 -0800

    chore - Add migration to insert new decision reason entry

--- api/migrations/versions/2025_11_21_799297eb2af2_add_new_decision_reason_entry.py
+++ api/migrations/versions/2025_11_21_799297eb2af2_add_new_decision_reason_entry.py
@@ -17,8 +17,22 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    # Get reference to the decision_reason table
+    decision_reason_table = sa.table('decision_reason',
+                                     sa.column('id', sa.Integer),
+                                     sa.column('name', sa.String),
+                                     sa.column('reason', sa.String)
+                                     )
+
+    # Insert the new entry
+    op.bulk_insert(
+        decision_reason_table,
+        [
+            {'id': 52, 'name': 'Outside Canada LP', 'reason': 'A Current Certificate Of Status from the Home Jurisdiction must be submitted to the Corporate Registry by email to bcregistries@gov.bc.ca You must include the Name Reservation Number on your correspondence.'},
+        ]
+    )
 
 
 def downgrade():
-    pass
+    # Remove the entry if rolling back
+    op.execute("DELETE FROM decision_reason WHERE id = 52")
