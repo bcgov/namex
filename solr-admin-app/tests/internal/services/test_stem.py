3e1fb0387d8a17a6b4877a18fd24237e5f440abb
Ali Hamood <ali.hamood@gov.bc.ca>
2025-12-16 09:10:31 -0800

    Removed the skip test directive. SOLR_URL must be set or CI should fail.

--- solr-admin-app/tests/internal/services/test_stem.py
+++ solr-admin-app/tests/internal/services/test_stem.py
@@ -6,12 +6,10 @@ from solr_admin.services.get_stems import get_stems, get_stems_url
 from urllib import request
 
 
-@pytest.mark.skipif(not os.getenv('SOLR_URL'), reason="SOLR_URL environment variable not set")
 def test_solr_configuration():
     assert_that(os.getenv('SOLR_URL'), not_none())
 
 
-@pytest.mark.skipif(not os.getenv('SOLR_URL'), reason="SOLR_URL environment variable not set")
 def test_solr_available():
     solr_url = os.getenv('SOLR_URL')
     url = solr_url + '/solr/possible.conflicts/admin/ping'
@@ -20,7 +18,6 @@ def test_solr_available():
     assert r.code == 200
 
 
-@pytest.mark.skipif(not os.getenv('SOLR_URL'), reason="SOLR_URL environment variable not set")
 def test_explore_stem_url():
     solr_url = os.getenv('SOLR_URL')
     synonym_list = 'construction, constructing, development'
@@ -29,7 +26,6 @@ def test_explore_stem_url():
     assert_that(url, equal_to(solr_url + '/solr/possible.conflicts/analysis/field?analysis.fieldvalue=construction,%20constructing,%20development&analysis.fieldname=name&wt=json&indent=true'))
 
 
-@pytest.mark.skipif(not os.getenv('SOLR_URL'), reason="SOLR_URL environment variable not set")
 def test_stem_several_worlds():
     synonym_list = 'construction, constructing, development'
     stems = get_stems(synonym_list)
